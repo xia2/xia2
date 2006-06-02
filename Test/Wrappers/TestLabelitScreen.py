21a58f61cc1dcbddf974d2b5a2f753c2edcb47fd
Graeme Winter <graeme.winter@gmail.com>
2006-06-02 13:39:35 +0000

    A unit test for Wrappers/Labelit/LabelitScreen.py

---
+++ Test/Wrappers/TestLabelitScreen.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# TestLabelitScreen.py
+# Maintained by G.Winter
+# 2nd June 2006
+# 
+# Unit tests to ensure that the LabelitScreen wrapper is behaving
+# properly.
+# 
+# 
+
+import os, sys
+
+if not os.environ.has_key('DPA_ROOT'):
+    raise RuntimeError, 'DPA_ROOT not defined'
+
+sys.path.append(os.path.join(os.environ['DPA_ROOT'],
+                             'Wrappers',
+                             'Labelit'))
+
+from LabelitScreen import LabelitScreen
+import unittest
+
+# this should be placed in a gwmath module or something...
+
+def nint(a):
+    b = int(a)
+    if a - b > 0.5:
+        b += 1
+    return b
+
+class TestLabelitScreen(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def testtetragonal(self):
+        '''A test to see if the indexing works when it should.'''
+
+        ls = LabelitScreen()
+
+        directory = os.path.join(os.environ['DPA_ROOT'],
+                                 'Data', 'Test', 'Images')
+
+        ls.addImage(os.path.join(directory, '12287_1_E1_001.img'))
+        ls.addImage(os.path.join(directory, '12287_1_E1_090.img'))
+        
+        ls.index()
+
+        # check the number of solutions
+        solutions = ls.getSolutions()
+        keys = solutions.keys()
+
+        self.assertEqual(max(keys), 9)
+        self.assertEqual(min(keys), 1)
+
+        # test the refined parameters
+        self.assertEqual(nint(ls.getDistance()), 170)
+
+        beam = ls.getBeam()
+        
+        self.assertEqual(nint(beam[0]), 109)
+        self.assertEqual(nint(beam[1]), 105)
+
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+    
+
+
