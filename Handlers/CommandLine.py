4457712524e5c4df52d357dc241d275b416abc79
Graeme Winter <graeme.winter@gmail.com>
2006-06-12 09:07:12 +0000

    Added readme to schema, decision to use schema to impl docs and header to commandline

--- Handlers/CommandLine.py
+++ Handlers/CommandLine.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python
+# CommandLine.py
+# Maintained by G.Winter
+# 12th June 2006
+# 
+# A handler for all of the information which may be passed in on the command
+# line. This singleton object should be able to handle the input, structure
+# it and make it available in a useful fashion.
+# 
+# This is a hook into a global data repository.
+#
+
+
 
---
+++ Schema/readme.txt
@@ -0,0 +1,6 @@
+Schema
+------
+
+This directory will contain XML Schema documents used to describe the internal
+and external data structures for this application. 
+
