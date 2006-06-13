8e68acf5f303ec8b4522c7e533d549f0ead08c08
Graeme Winter <graeme.winter@gmail.com>
2006-06-13 08:36:21 +0000

    Added beam centre handling.

--- Handlers/CommandLine.py
+++ Handlers/CommandLine.py
@@ -10,3 +10,73 @@
 # This is a hook into a global data repository.
 #
 
+import sys
+import os
+
+if not os.environ.has_key('DPA_ROOT'):
+    raise RuntimeError, 'DPA_ROOT not defined'
+if not os.environ.has_key('XIA2CORE_ROOT'):
+    raise RuntimeError, 'XIA2CORE_ROOT not defined'
+
+sys.path.append(os.path.join(os.environ['DPA_ROOT'], 'Schema'))
+
+from Object import Object
+
+class _CommandLine(Object):
+    '''A class to represent the command line input.'''
+
+    def __init__(self):
+        '''Initialise all of the information from the command line.'''
+
+        Object.__init__(self)
+
+        self._read_beam()
+
+        return
+
+    # command line parsers, getters and help functions.
+
+    def _read_beam(self):
+        '''Read the beam centre from the command line.'''
+
+        index = -1
+
+        try:
+            index = sys.argv.index('-beam')
+        except ValueError, e:
+            # the token is not on the command line
+            self.write('No beam passed in on the command line')
+            self._beam = (0.0, 0.0)
+
+        if index < 0:
+            raise RuntimeError, 'nagative index'
+
+        try:
+            beam = sys.argv[index + 1].split(',')
+        except IndexError, e:
+            raise RuntimeError, '-beam correct use "-beam x,y"'
+
+        if len(beam) != 2:
+            raise RuntimeError, '-beam correct use "-beam x,y"'
+
+        self.write('Beam passed on the command line: %7.2f %7.2f' % \
+                   (float(beam[0]), float(beam[1])))
+
+        self._beam = (float(beam[0]), float(beam[1]))
+
+        return
+
+    def _help_beam(self):
+        '''Return a help string for the read beam method.'''
+
+        return '-beam x,y'
+
+    def get_beam(self):
+        return self._beam
+
+CommandLine = _CommandLine()
+
+if __name__ == '__main__':
+    print CommandLine.get_beam()
+
+    
