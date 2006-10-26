06eacf294d5cb1837a9c5e48d42a18b09f5600bb
Graeme Winter <graeme.winter@gmail.com>
2006-10-26 15:15:01 +0000

    An emptyish cad wrapper holder

---
+++ Wrappers/CCP4/Cad.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Cad.py
+#   Copyright (C) 2006 CCLRC, Graeme Winter
+#
+#   This code is distributed under the terms and conditions of the
+#   CCP4 Program Suite Licence Agreement as a CCP4 Library.
+#   A copy of the CCP4 licence can be obtained by writing to the
+#   CCP4 Secretary, Daresbury Laboratory, Warrington WA4 4AD, UK.
+#
+# 26th October 2006
+# 
+# A wrapper for the CCP4 program Cad, for merging multiple reflection files
+# into a single reflection file, e.g. in preparation for phasing.
+# 
+# This will take a single HKLOUT file and a list of HKLIN files, c/f the
+# Sortmtz wrapper implementation.
+
