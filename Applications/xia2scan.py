1b49e391f802c9e8b7f0107fc7946fb7132a5dd9
Graeme Winter <graeme.winter@gmail.com>
2006-10-27 12:29:07 +0000

    Adding the new functionality which Rajan needs to make the humidifer analysis stuff work - new command line option -first_last first,last and now doesn't print where indexing fails.
    Also switches of Chatter output.

--- Applications/xia2scan.py
+++ Applications/xia2scan.py
@@ -21,13 +21,19 @@
 
 import sys
 import os
+import exceptions
 
 if not os.environ.has_key('DPA_ROOT'):
     raise RuntimeError, 'DPA_ROOT not defined'
 if not os.environ.has_key('XIA2CORE_ROOT'):
     raise RuntimeError, 'XIA2CORE_ROOT not defined'
 
-sys.path.append(os.path.join(os.environ['DPA_ROOT']))
+if not os.environ['DPA_ROOT'] in sys.path:
+    sys.path.append(os.environ['DPA_ROOT'])
+
+# prevent the usual standard output stuff...
+from Handlers.Streams import streams_off
+streams_off()
 
 from Handlers.CommandLine import CommandLine
 from Schema.Sweep import SweepFactory
@@ -39,8 +45,10 @@ from Wrappers.Labelit.LabelitStats_distl import LabelitStats_distl
 def xia2scan():
     '''Do the scan.'''
 
-    template, directory = CommandLine.getTemplate(), \
-                          CommandLine.getDirectory()
+    template, directory = CommandLine.get_template(), \
+                          CommandLine.get_directory()
+
+    first, last = CommandLine.get_first_last()
 
     if not template or not directory:
         raise RuntimeError, 'no image information supplied'
@@ -48,7 +56,7 @@ def xia2scan():
     # if this isnot set, then we will presume that it is correct
     # in the image headers
     
-    beam = CommandLine.getBeam()
+    beam = CommandLine.get_beam()
 
     sweeps = SweepFactory(template, directory, beam)
 
@@ -59,25 +67,34 @@ def xia2scan():
         # if > 1 image then these are probably stills... but we don't care
         # about that!
 
-        for i in s.getImages():
-            sys.stdout.write('.')
-            sys.stdout.flush()
+        for i in s.get_images():
 
-            beam = s.getBeam()
+            beam = s.get_beam()
+
+            # FIXME 27/OCT/06 want to be able to deal with first,
+            # last images
+            if (first > 0 and last > 0) and (i < first or i > last):
+                sys.stdout.write('.')
+                sys.stdout.flush()
+                continue
+
+            sys.stdout.write('o')
+            sys.stdout.flush()
 
             try:
 
                 ls = LabelitScreen()
-                ls.addImage(s.imagename(i))
-                ls.setBeam(beam[0], beam[1])
-                ls.setRefine_beam(False)
+                ls.setup_from_image(s.imagename(i))
+                ls.add_indexer_image_wedge(i)
+                ls.set_beam(beam)
+                ls.set_refine_beam(False)
                 ls.index()
 
-                solution = ls.getSolutions()[1]
+                solution = ls.get_solutions()[1]
                 # P1 cell is always solution #1
                 lsd = LabelitStats_distl()
                 lsd.stats_distl()
-                stats = lsd.getStatistics(s.imagename(i))
+                stats = lsd.get_statistics(s.imagename(i))
                 summary_info[i] = { }
                 summary_info[i]['volume'] = solution['volume']
                 summary_info[i]['mosaic'] = solution['mosaic']
@@ -86,7 +103,9 @@ def xia2scan():
                 summary_info[i]['resol_one'] = stats['resol_one']
                 summary_info[i]['resol_two'] = stats['resol_two']
 
-            except:
+            except exceptions.Exception, e:
+                print 'Uh oh! %s' % str(e)
+
                 summary_info[i] = { }
                 summary_info[i]['volume'] = 0.0
                 summary_info[i]['mosaic'] = 0.0
@@ -100,13 +119,14 @@ def xia2scan():
     images = summary_info.keys()
     images.sort()
     for i in images:
-        print '%3d %6d %6d %6.2f %6.2f %6.2f %9d' % \
-              (i, summary_info[i]['spots_total'],
-               summary_info[i]['spots_good'],
-               summary_info[i]['resol_one'],
-               summary_info[i]['resol_two'],
-               summary_info[i]['mosaic'],
-               summary_info[i]['volume'])
+        if summary_info[i]['volume'] > 0:
+            print '%3d %6d %6d %6.2f %6.2f %6.2f %9d' % \
+                  (i, summary_info[i]['spots_total'],
+                   summary_info[i]['spots_good'],
+                   summary_info[i]['resol_one'],
+                   summary_info[i]['resol_two'],
+                   summary_info[i]['mosaic'],
+                   summary_info[i]['volume'])
 
 if __name__ == '__main__':
     xia2scan()
