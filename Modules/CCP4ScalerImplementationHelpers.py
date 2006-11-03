321358fd75c50725e790303206a104a70c566948
Graeme Winter <graeme.winter@gmail.com>
2006-11-03 09:16:12 +0000

    Added header banner...

---
+++ Modules/CCP4ScalerImplementationHelpers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# CCP4ScalerImplementationHelpers.py
+#   Copyright (C) 2006 CCLRC, Graeme Winter
+#
+#   This code is distributed under the terms and conditions of the
+#   CCP4 Program Suite Licence Agreement as a CCP4 Library.
+#   A copy of the CCP4 licence can be obtained by writing to the
+#   CCP4 Secretary, Daresbury Laboratory, Warrington WA4 4AD, UK.
+#
+# 3rd November 2006
+# 
+# Helpers for the "CCP4" Scaler implementation - this contains little
+# functions which wrap the wrapper which are needed.
+# 
+#
+
