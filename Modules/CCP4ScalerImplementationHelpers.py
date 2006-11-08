f9ac281835580e9bc72ba745aa38d3c4eeab9cb5
Graeme Winter <graeme.winter@gmail.com>
2006-11-08 12:00:34 +0000

    Added resolution estimating function

--- Modules/CCP4ScalerImplementationHelpers.py
+++ Modules/CCP4ScalerImplementationHelpers.py
@@ -10,7 +10,43 @@
 # 3rd November 2006
 # 
 # Helpers for the "CCP4" Scaler implementation - this contains little
-# functions which wrap the wrapper which are needed.
-# 
+# functions which wrap the wrappers which are needed. It will also contain
+# small functions for computing e.g. resolution limits.
 #
+
+def _resolution_estimate(ordered_pair_list, cutoff):
+    '''Come up with a linearly interpolated estimate of resolution at
+    cutoff cutoff from input data [(resolution, i_sigma)].'''
+
+    x = []
+    y = []
+
+    for o in ordered_pair_list:
+        x.append(o[0])
+        y.append(o[1])
+
+    if max(y) < cutoff:
+        # there is no point where this exceeds the resolution
+        # cutoff
+        return -1.0
+
+    # this means that there is a place where the resolution cutof
+    # can be reached - get there by working backwards
+
+    x.reverse()
+    y.reverse()
+
+    if y[0] >= cutoff:
+        # this exceeds the resolution limit requested
+        return x[0]
+
+    j = 0
+    while y[j] < cutoff:
+        j += 1
+
+    resolution = x[j] + (cutoff - y[j]) * (x[j - 1] - x[j]) / \
+                 (y[j - 1] - y[j])
+
+    return resolution
+
 
