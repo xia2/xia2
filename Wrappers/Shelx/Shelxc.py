c92a2e9687458b302fcca2bd49282f522ccbc623
Graeme Winter <graeme.winter@gmail.com>
2006-11-16 09:30:10 +0000

    Added an empty banner for copyright stuff and paths.

---
+++ Wrappers/Shelx/Shelxc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# FileName.py
+#   Copyright (C) 2006 CCLRC, Graeme Winter
+#
+#   This code is distributed under the terms and conditions of the
+#   CCP4 Program Suite Licence Agreement as a CCP4 Library.
+#   A copy of the CCP4 licence can be obtained by writing to the
+#   CCP4 Secretary, Daresbury Laboratory, Warrington WA4 4AD, UK.
+#
+# 16th November 2006
+# 
+
+import sys
+import os
+
+if not os.path.join(os.environ['XIA2CORE_ROOT'], 'Python') in sys.path:
+    sys.path.append(os.path.join(os.environ['XIA2CORE_ROOT'], 'Python'))
+
+if not os.environ['DPA_ROOT'] in sys.path:
+    sys.path.append(os.environ['DPA_ROOT'])
+
+
