17c11e25057ee40eb373c022ec0756f92f8bdaab
Graeme Winter <graeme.winter@gmail.com>
2006-11-20 13:01:05 +0000

    A wrapper for hyss..

--- Wrappers/Phenix/Hyss.py
+++ Wrappers/Phenix/Hyss.py
@@ -12,6 +12,11 @@
 # FIXME 16/NOV/06 this needs to express the interface for substructure
 #                 determination.
 # 
+# FIXME 20/NOV/06 no it doesn't - that should be handled in the Hyss
+#                 SubstructureFinder module, which can include some
+#                 proper preparation of the data. This should just
+#                 provide the simple interface to the program and allow
+#                 composition to handle everything else.
 
 import sys
 import os
@@ -19,15 +24,14 @@ import os
 if not os.path.join(os.environ['XIA2CORE_ROOT'], 'Python') in sys.path:
     sys.path.append(os.path.join(os.environ['XIA2CORE_ROOT'], 'Python'))
 
-if not os.environ['DPA_ROOT'] in sys.path:
-    sys.path.append(os.environ['DPA_ROOT'])
-
 if not os.environ['SS_ROOT'] in sys.path:
     sys.path.append(os.environ['SS_ROOT'])
 
 from Driver.DriverFactory import DriverFactory
+from lib.SubstructureLib import parse_pdb_sites_file, \
+     write_pdb_sites_file
 
-from Schema.Interfaces.SubstructureFinder import SubstructureFinder
+# from Schema.Interfaces.SubstructureFinder import SubstructureFinder
 
 def Hyss(DriverType = None):
     '''Factory for Hyss wrapper classes, with the specified
@@ -35,8 +39,8 @@ def Hyss(DriverType = None):
 
     DriverInstance = DriverFactory.Driver(DriverType)
 
-    class HyssWrapper(DriverInstance.__class__,
-                      SubstructureFinder):
+    class HyssWrapper(DriverInstance.__class__):
+        # SubstructureFinder):
         '''A wrapper for the program phenix.hyss, for locating the heavy
         atom substructure from an anomalous data set.'''
 
@@ -51,40 +55,117 @@ def Hyss(DriverType = None):
             # happens to be hklf3, the type of heavy atoms and the
             # spacegroup...
 
-        def _substructure_find_prepare(self):
-            '''Prepare data for hyss to work with.'''
+            self._hklin = None
+            self._hklin_type = None
+            self._spacegroup = None
+            self._cell = None
+            self._n_sites = None
+            self._atom = None
 
-            # get the data from the scaler
+            # output stuff
 
-            # decide what to do with it based on the input format
+            self._sites = None
 
-            # perhaps run something (e.g. shelxc) to prepare the data
-
-            # record the prepared reflection file
-
-            # set the spacegroup if it is not already set
-
+        def set_hklin(self, hklin):
+            self._hklin = hklin
+            return
+            
+        def set_hklin_type(self, hklin_type):
+            self._hklin_type = hklin_type
             return
 
-        def _substructure_find(self):
+        def set_spacegroup(self, spacegroup):
+            self._spacegroup = spacegroup
+            return
+
+        def set_cell(self, cell):
+            self._cell = cell
+
+        def set_atom(self, atom):
+            self._atom = atom
+            return
+
+        def set_n_sites(self, n_sites):
+            self._n_sites = n_sites
+            return
+
+        def find_substructure(self):
             '''Actually run hyss to find the sites.'''
+
+            # check the input
+
+            if not self._hklin:
+                raise RuntimeError, 'hklin not defined'
+
+            if not self._n_sites:
+                raise RuntimeError, 'number of sites not defined'
+
+            if not self._atom:
+                raise RuntimeError, 'atom type not defined'
+
+            if not self._spacegroup:
+                raise RuntimeError, 'spacegroup not set'
 
             # get the prepared reflection file
 
+            if self._hklin_type:
+                self.add_command_line('%s=%s' %
+                                      (self._hklin, self._hklin_type))
+            else:
+                self.add_command_line('%s' % self._hklin)
+
+            self.add_command_line('%d' % self._n_sites)
+            self.add_command_line('%s' % self._atom)
+
+            self.add_command_line('--space_group=%s' % self._spacegroup)
+
+            if self._cell:
+                self.add_command_line('--unit_cell=%f,%f,%f,%f,%f,%f' % \
+                                      tuple(self._cell))
+
             # start hyss
 
-            # check that hys has worked ok - if not either
-            # (i) change something so it will work fine next time - or -
-            # (ii) raise an exception so we get out of teh stuck loop
+            self.start()
 
-            # get the output heavy atom pdb file
+            self.close_wait()
 
-            # transmogrify it into the standard format
+            self.check_for_errors()
 
-            # return
+            for line in self.get_all_output():
+                if 'Writing consensus model as PDB' in line:
+                    pdb_file = line.split()[-1]
+                    self._sites = parse_pdb_sites_file(pdb_file)
 
+            return
+
+        def get_sites(self):
+            return self._sites
 
     return HyssWrapper()
 
+if __name__ == '__main__':
 
+    # then run a test
 
+    if not os.environ.has_key('X2TD_ROOT'):
+        raise RuntimeError, 'X2TD_ROOT not defined'
+
+    hklin = os.path.join(os.environ['X2TD_ROOT'],
+                         'Test', 'UnitTest', 'Wrappers', 'Hyss',
+                         '1VR5_13193_fa.hkl')
+
+    hyss = Hyss()
+
+    hyss.set_hklin(hklin)
+    hyss.set_hklin_type('hklf3')
+    hyss.set_spacegroup('p21212')
+    hyss.set_cell((140.26, 96.79, 115.88, 90.0, 90.0, 90.0))
+    hyss.set_n_sites(18)
+    hyss.set_atom('se')
+
+    hyss.write_log_file('hyss.log')
+    hyss.find_substructure()
+
+    sites = hyss.get_sites()
+
+    write_pdb_sites_file(sites)
