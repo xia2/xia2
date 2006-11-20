520693af5c42f1ba40ca3da14036b3858b61b311
Graeme Winter <graeme.winter@gmail.com>
2006-11-20 13:14:39 +0000

    Explicitly add the ss/lib directory to the system path

--- Wrappers/Phenix/Hyss.py
+++ Wrappers/Phenix/Hyss.py
@@ -27,8 +27,13 @@ if not os.path.join(os.environ['XIA2CORE_ROOT'], 'Python') in sys.path:
 if not os.environ['SS_ROOT'] in sys.path:
     sys.path.append(os.environ['SS_ROOT'])
 
+# explicitly add the "lib" directory to the path...
+
+if not os.path.join(os.environ['SS_ROOT'], lib) in sys.path:
+    sys.path.append(os.path.join(os.environ['SS_ROOT'], lib))
+
 from Driver.DriverFactory import DriverFactory
-from lib.SubstructureLib import parse_pdb_sites_file, \
+from SubstructureLib import parse_pdb_sites_file, \
      write_pdb_sites_file
 
 # from Schema.Interfaces.SubstructureFinder import SubstructureFinder
