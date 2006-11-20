e007e95ea1ed1b118ddb20192f62777e4730deb5
Graeme Winter <graeme.winter@gmail.com>
2006-11-20 15:49:09 +0000

    Added framework for "by run" resolution limits

--- Wrappers/CCP4/Scala.py
+++ Wrappers/CCP4/Scala.py
@@ -74,6 +74,8 @@ def Scala(DriverType = None):
             # scaling parameters
             self._resolution = None
 
+            self._resolution_by_run = { }
+
             # scales file for recycling
             self._scales_file = None
 
@@ -164,6 +166,12 @@ def Scala(DriverType = None):
             default is to include all reflections.'''
 
             self._resolution = resolution
+            return
+
+        def set_resolution_by_run(self, run, resolution):
+            '''Set the resolution for a particular run.'''
+
+            self._resolution_by_run = { }
             return
 
         def set_scales_file(self, scales_file):
@@ -423,6 +431,10 @@ def Scala(DriverType = None):
 
             if self._resolution:
                 self.input('resolution %f' % self._resolution)
+
+            if self._resolution_by_run != { }:
+                # FIXME 20/NOV/06 this needs implementing somehow...
+                pass
 
             self.input('cycles %d' % self._cycles)
 
