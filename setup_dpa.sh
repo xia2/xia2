45933669e3e36ffbaff4c873e824996b473ec27d
Graeme Winter <graeme.winter@gmail.com>
2006-11-27 13:46:32 +0000

    Added power pc architecture switch if Darwin

--- setup_dpa.sh
+++ setup_dpa.sh
@@ -7,7 +7,12 @@ platform=`uname`
 # FIXME this needs to account for Darwin on 386 & ppc.
 
 if [ "$platform" = "Darwin" ]; then
-  export PATH=${PATH}:${DPA_ROOT}/binaries/mac_386
+  arch=`uname -a | awk '{print $NF}'`
+  if [ "$arch" = "powerpc" ]; then
+    export PATH=${PATH}:${DPA_ROOT}/binaries/mac_ppc
+  elif [ "$arch" = "i386" ]; then
+    export PATH=${PATH}:${DPA_ROOT}/binaries/mac_386  
+  fi
 elif [ "$platform" = "Linux" ]; then
   export PATH=${PATH}:${DPA_ROOT}/binaries/linux_386
 fi
