d3e9692a1f71b25a9c815bd4a03e1919a2adb962
Graeme Winter <graeme.winter@gmail.com>
2006-12-04 10:15:23 +0000

    An empty scaler implementation - to get this working will require changes to the .xinfo file (which needs doing anyway)

---
+++ Modules/NullScalerImplementation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# NullScalerImplementation.py
+#   Copyright (C) 2006 CCLRC, Graeme Winter
+#
+#   This code is distributed under the terms and conditions of the
+#   CCP4 Program Suite Licence Agreement as a CCP4 Library.
+#   A copy of the CCP4 licence can be obtained by writing to the
+#   CCP4 Secretary, Daresbury Laboratory, Warrington WA4 4AD, UK.
+#
+# 30/OCT/06
+#
+# An empty integrater - this presents the Scaler interface but does 
+# nothing, making it ideal for when you have the reduced data already -
+# this will simply return that reduced data...
+#
+
