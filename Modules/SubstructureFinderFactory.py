a6800aef15a60a7dce6e71d7e993aa07c992d595
Graeme Winter <graeme.winter@gmail.com>
2006-12-05 14:28:13 +0000

    Updated all licenses from ccp4 library to BSD.

--- Modules/SubstructureFinderFactory.py
+++ Modules/SubstructureFinderFactory.py
@@ -2,10 +2,10 @@
 # SubstructureFinderFactory.py
 #   Copyright (C) 2006 CCLRC, Graeme Winter
 #
-#   This code is distributed under the terms and conditions of the
-#   CCP4 Program Suite Licence Agreement as a CCP4 Library.
-#   A copy of the CCP4 licence can be obtained by writing to the
-#   CCP4 Secretary, Daresbury Laboratory, Warrington WA4 4AD, UK.
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+
+
 # 
 # A factory for SubstructureFinder implementations. At the moment this will 
 # support only Hyss.
