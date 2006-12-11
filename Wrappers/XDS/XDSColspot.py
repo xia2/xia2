826b2b212c4def3db50d82869033ac013e5b16ed
Graeme Winter <graeme.winter@gmail.com>
2006-12-11 14:34:38 +0000

    Started work on XDS interface, brought together notes from a number of xds.inp files on the standard records which appear in the header.

---
+++ Wrappers/XDS/XDSColspot.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# XDSColspot.py
+#   Copyright (C) 2006 CCLRC, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+#
