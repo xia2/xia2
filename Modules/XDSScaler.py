f8361fa536fc64d6a3e1491f3f2ebe2e453684c0
Graeme Winter <graeme.winter@gmail.com>
2007-01-08 10:50:57 +0000

    Thinking about how this will work without pointless...

--- Modules/XDSScaler.py
+++ Modules/XDSScaler.py
@@ -12,3 +12,35 @@
 #
 # This will run XDS CORRECT and XSCALE.
 #
+# Process (based on CCP4 Scaler)
+# 
+# _scale_prepare:
+# 
+# gather sweep information
+# [check integraters are xds]
+# check sweep information
+# generate reference set: pointless -> sort -> quick scale
+# reindex all data to reference: pointless -> eliminate lattices-> pointless
+# verify pointgroups
+# ?return if lattice in integration too high?
+# reindex, rebatch
+# sort
+# pointless (spacegroup)
+#
+# _scale:
+# 
+# decide resolution limits / wavelength
+# ?return to integration?
+# refine parameters of scaling
+# record best resolution limit
+# do scaling
+# truncate
+# standardise unit cell
+# update all reflection files
+# cad together 
+# add freer flags
+# 
+# In XDS terms this will be a little different. CORRECT provides GXPARM.XDS
+# which could be recycled. A pointgroup determination routine will be needed.
+# XDS reindexing needs to be sussed, as well as getting comparative R factors
+# - how easy is this?
