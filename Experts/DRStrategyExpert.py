8443aebbb111ace90e85690b6e5fbdfb4a7c1061
Graeme Winter <graeme.winter@gmail.com>
2007-01-10 16:13:50 +0000

    Work towards a strategy expert

---
+++ Experts/DRStrategyExpert.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# DRStrategyExpert.py
+#   Copyright (C) 2006 CCLRC, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+#
+# 10th January 2007
+# 
+# An expert system to propose a strategy for data reduction, based on
+# the existing computational environment and some characteristics of the
+# data set.
+#
+# Those characteristics are:
+#
+# ismosaic
+#
+# and the computational environment is:
+#
+# is the software available
+#
+
+import os
+import sys
+
+import os
+import sys
+
+if not os.environ.has_key('XIA2CORE_ROOT'):
+    raise RuntimeError, 'XIA2CORE_ROOT not defined'
+
+if not os.environ.has_key('XIA2_ROOT'):
+    raise RuntimeError, 'XIA2_ROOT not defined'
+
+if not os.path.join(os.environ['XIA2CORE_ROOT'],
+                    'Python') in sys.path:
+    sys.path.append(os.path.join(os.environ['XIA2CORE_ROOT'],
+                                 'Python'))
+    
+if not os.environ['XIA2_ROOT'] in sys.path:
+    sys.path.append(os.environ['XIA2_ROOT'])
+
+from Driver.DriverHelper import executable_exists
+
+# write in strategy dictionary here
