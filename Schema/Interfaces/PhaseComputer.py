2c5a9883a6a7ee8daf86d38173545151160be94a
Graeme Winter <graeme.winter@gmail.com>
2007-02-06 15:51:09 +0000

    Started thinking about implementing the phasing version of xia2

--- Schema/Interfaces/PhaseComputer.py
+++ Schema/Interfaces/PhaseComputer.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
-# FileName.py
+# PhaseComputer.py
 #   Copyright (C) 2006 CCLRC, Graeme Winter
 #
 #   This code is distributed under the BSD license, a copy of which is 
 #   included in the root directory of this package.
-
-
 #
 # 16th November 2006
 # 
+# An interface to represent programs which will compute initial phases
+# from heavy atoms sites, perhaps refining them along the way. Examples
+# are bp3, phaser, sharp. This will take as input a list of heavy atom
+# sites from a SubstructureFinder (actually it will take the finder) and
+# some reflections with associated F', F'' values - most likely set up from
+# an XCrystal object.
 
 import sys
 import os
--- Schema/Interfaces/PhaseImprover.py
+++ Schema/Interfaces/PhaseImprover.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
-# FileName.py
+# PhaseImprover.py
 #   Copyright (C) 2006 CCLRC, Graeme Winter
 #
 #   This code is distributed under the BSD license, a copy of which is 
 #   included in the root directory of this package.
-
-
 #
 # 16th November 2006
-# 
+#
+# An interface to represent improvers of phases, for example through 
+# density modification, which may be implemented by e.g. dm, pirate,
+# solomon, resolve. This will take as input a solvent fraction (probably)
+# and some initially phased reflections in a reflection file, most
+# likely MTZ in the first instance. Actually this should take a phase
+# computer as input...
 
 import sys
 import os
