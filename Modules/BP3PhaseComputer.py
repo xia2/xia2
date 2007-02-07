d0b9903feb7d69d9d51a1eef0891e3e7e0d9f715
Graeme Winter <graeme.winter@gmail.com>
2007-02-07 16:04:03 +0000

    Started work on implementing the phase computer interface using bp3 (and wilson, mtzdump and friends)

---
+++ Modules/BP3PhaseComputer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# BP3PhaseComputer.py
+#   Copyright (C) 2006 CCLRC, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+# 
+# An implementation of the PhaseComputer interface using the program BP3.
+# This will also include Wilson for assignment of the B factor - just 
+# in case it is not provided as input.
+# 
+
+import os
+import sys
+import math
+
+if not os.environ.has_key('XIA2_ROOT'):
+    raise RuntimeError, 'XIA2_ROOT not defined'
+
+if not os.environ['XIA2_ROOT'] in sys.path:
+    sys.path.append(os.environ['XIA2_ROOT'])
+
+# the interface definition that this will conform to 
+from Schema.Interfaces.PhaseComputer import PhaseComputer
+
+# the wrappers that this will use - these are renamed so that the internal
+# factory version can be used...
+
+from Wrappers.CCP4.Wilson import Wilson as _Wilson
+from Wrappers.CCP4.BP3 import BP3 as _BP3
+
+class BP3PhaseComputer(PhaseComputer):
+
+    # constructor
+
+    # need working directory stuff
+
+    # factory
+
+    pass
+
+
+
