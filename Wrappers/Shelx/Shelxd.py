c150492ff185cf443a673f99c7ad85178cc95458
Graeme Winter <graeme.winter@gmail.com>
2007-02-20 09:29:48 +0000

    Copied code from core examples to shelx wrappers doc'd plans for a peer implementation test.

--- Wrappers/Shelx/Shelxd.py
+++ Wrappers/Shelx/Shelxd.py
@@ -17,4 +17,38 @@ if not os.path.join(os.environ['XIA2CORE_ROOT'], 'Python') in sys.path:
 if not os.environ['XIA2_ROOT'] in sys.path:
     sys.path.append(os.environ['XIA2_ROOT'])
 
+from Driver.DriverFactory import DriverFactory
 
+def Shelxd(DriverType = None):
+    '''Create a Shelxd instance based on the DriverType.'''
+
+    DriverInstance = DriverFactory.Driver(DriverType)
+
+    class ShelxdWrapper(DriverInstance.__class__):
+        '''A wrapper class for Shelxd.'''
+
+        def __init__(self):
+            DriverInstance.__class__.__init__(self)
+
+            self.set_executable('shelxd')
+
+            self._name = None
+
+        def set_name(self, name):
+            self._name = name
+
+            return
+
+        def find_sites(self):
+            '''Find the HA sites.'''
+
+            self.add_command_line('%s_fa' % self._name)
+
+            self.start()
+            self.close_wait()
+
+            output = self.get_all_output()
+
+            return
+
+    return ShelxdWrapper()
--- Wrappers/Shelx/Shelxe.py
+++ Wrappers/Shelx/Shelxe.py
@@ -17,4 +17,46 @@ if not os.path.join(os.environ['XIA2CORE_ROOT'], 'Python') in sys.path:
 if not os.environ['XIA2_ROOT'] in sys.path:
     sys.path.append(os.environ['XIA2_ROOT'])
 
+from Driver.DriverFactory import DriverFactory
 
+def Shelxe(DriverType = None):
+    '''Create a Shelxe instance based on the DriverType.'''
+
+    DriverInstance = DriverFactory.Driver(DriverType)
+
+    class ShelxeWrapper(DriverInstance.__class__):
+        '''A wrapper class for Shelxe.'''
+
+        def __init__(self):
+            DriverInstance.__class__.__init__(self)
+
+            self.set_executable('shelxe')
+
+            self._name = None
+
+            self._solvent = 0.0
+
+        def set_solvent(self, solvent):
+            self._solvent = solvent
+            return
+
+        def set_name(self, name):
+            self._name = name
+            return
+
+        def phase(self):
+            '''Actually compute the phases from the heavy atom locations.'''
+
+            self.add_command_line('%s' % self._name)
+            self.add_command_line('%s_fa' % self._name)
+            self.add_command_line('-h')
+            self.add_command_line('-s%f' % self._solvent)
+            self.add_command_line('-m20')
+
+            self.start()
+
+            self.close_wait()
+
+            output = self.get_all_output()
+
+    return ShelxeWrapper()
