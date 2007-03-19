5e50805f8414441628561da06b69f7c716056d37
Graeme Winter <graeme.winter@gmail.com>
2007-03-19 13:51:13 +0000

    Renamed to fit in with conventions with rest of handlers, added header which needs to be fixed

---
+++ Handlers/Citations.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# CitationHandler.py
+#   Copyright (C) 2006 CCLRC, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+# 
+# A handler for management of program citations. This should initialise
+# from a citations.dat file which can be found in a number of places...
+# 
