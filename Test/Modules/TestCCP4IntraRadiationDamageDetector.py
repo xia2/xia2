68d0f547e1473845cfaa921eaa1191725ddbfb8c
Graeme Winter <graeme.winter@gmail.com>
2007-04-05 10:05:48 +0000

    Fixed dumb errors...

--- Test/Modules/TestCCP4IntraRadiationDamageDetector.py
+++ Test/Modules/TestCCP4IntraRadiationDamageDetector.py
@@ -10,7 +10,7 @@ import sys
 if not os.environ.has_key('XIA2_ROOT'):
     raise RuntimeError, 'XIA2_ROOT not defined'
 
-if os.environ['XIA2_ROOT'] in sys.path:
+if not os.environ['XIA2_ROOT'] in sys.path:
     sys.path.append(os.environ['XIA2_ROOT'])
 
 from Modules.CCP4IntraRadiationDamageDetector import \
@@ -20,7 +20,7 @@ def run(mtz, sweep_info):
     irdd = CCP4IntraRadiationDamageDetector()
 
     irdd.set_hklin(mtz)
-    irdd.set_sweep_information(eval(open(sweep_info, 'r').read()))
+    irdd.set_sweep_information(eval(open(sweep_info, 'r').read().strip()))
     return irdd.analyse()
 
 if __name__ == '__main__':
