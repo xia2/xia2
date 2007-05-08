4423cc7bb274065b600c889483de4fb748f182c8
Graeme Winter <graeme.winter@gmail.com>
2007-05-08 15:04:55 +0000

    Some more thoughts about these interfaces,

--- Schema/Interfaces/StrategyComputer.py
+++ Schema/Interfaces/StrategyComputer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# StrategyComputer
+# StrategyComputer.py
 #   Copyright (C) 2006 CCLRC, Graeme Winter
 #
 #   This code is distributed under the BSD license, a copy of which is 
