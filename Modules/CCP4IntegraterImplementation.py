a5bae55ad8421dec84b7a40c9c719d90d0f409dc
Graeme Winter <graeme.winter@gmail.com>
2007-05-20 19:49:53 +0000

    Start on implementing this interface...

---
+++ Modules/CCP4IntegraterImplementation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# CCP4IntegraterImplementation.py
+#   Copyright (C) 2006 CCLRC, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+#
+# 20th May 2007
+# 
+# An implementation of the integrater interface using mostly Mosflm, with a
+# little CCP4.
+# 
+
+import os
+import sys
+import math
+
+if not os.environ.has_key('XIA2_ROOT'):
+    raise RuntimeError, 'XIA2_ROOT not defined'
+
+if not os.environ['XIA2_ROOT'] in sys.path:
+    sys.path.append(os.environ['XIA2_ROOT'])
+
+# the interface definition that this will conform to 
+from Schema.Interfaces.Integrater import Integrater
+
+
