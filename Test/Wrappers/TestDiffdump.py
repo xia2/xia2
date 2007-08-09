74031d92ba078809d28d838b868613a23bcb2306
Graeme Winter <graeme.winter@gmail.com>
2007-08-09 14:22:58 +0000

    Start of a diffdump unit test...

---
+++ Test/Wrappers/TestDiffdump.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# TestLabelitScreen.py
+#   Copyright (C) 2006 CCLRC, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+#
+# 2nd June 2006
+# 
+# Unit tests to ensure that the LabelitScreen wrapper is behaving
+# properly.
+# 
+# 
+
+import os, sys
+
+if not os.environ.has_key('XIA2_ROOT'):
+    raise RuntimeError, 'XIA2_ROOT not defined'
+if not os.environ.has_key('X2TD_ROOT'):
+    raise RuntimeError, 'X2TD_ROOT (xia2 Test Data Root) not defined'
+
+sys.path.append(os.path.join(os.environ['XIA2_ROOT']))
+
+from Wrappers.XIA.Diffdump import Diffdump
+import unittest
+
+from Handlers.Streams import streams_off
+
+streams_off()
+
+# this should be placed in a gwmath module or something...
+
+def nint(a):
+    b = int(a)
+    if a - b > 0.5:
+        b += 1
+    return b
+
+class TestDiffractionImage(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def testadsc(self):
+        image = os.path.join(os.environ['X2TD_ROOT'],
+                             'Test', 'UnitTest', 'Wrappers', 'Diffdump',
+                             '2287_1_E1_001.img')
+        
+        dd = Diffdump()
+        p.set_image(image)
+        header  = p.readheader()
+
+        # check that the values are correct
+        
+        self.assertEqual(nint(header['distance']), 170)
+        
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+    
+
+
