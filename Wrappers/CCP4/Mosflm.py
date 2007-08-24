4e6cf96e0382289fa75e6f3c37dd81be8fb09b98
Graeme Winter <graeme.winter@gmail.com>
2007-08-24 12:31:47 +0000

    Unrolling again to previous trusted mosflm version

--- Wrappers/CCP4/Mosflm.py
+++ Wrappers/CCP4/Mosflm.py
@@ -261,7 +261,9 @@ def Mosflm(DriverType = None):
         def __init__(self):
             # generic things
             CCP4DriverInstance.__class__.__init__(self)
-	    self.set_executable('ipmosflm-7.0.1')
+            # this is still not really ready
+	    # self.set_executable('ipmosflm-7.0.1')
+            self.set_executable('ipmosflm')
 
             FrameProcessor.__init__(self)
             Indexer.__init__(self)
