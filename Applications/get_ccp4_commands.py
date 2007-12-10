f3514a1c0fd1f5d13733ae1f6b065fd9d7585f4b
Graeme Winter <graeme.winter@gmail.com>
2007-12-10 12:50:11 +0000

    Now also get the logical names from the input...

--- Applications/get_ccp4_commands.py
+++ Applications/get_ccp4_commands.py
@@ -5,19 +5,36 @@ import sys
 def get_ccp4_commands(lines_of_input):
     '''Get the commands which were sent to a CCP4 program.'''
 
-    result = []
+    # first look through for hklin / hklout
+
+    logicals = { }
+
+    for line in lines_of_input:
+        if 'Logical Name:' in line:
+            token = line.split(':')[1].split()[0]
+            value = line.split(':')[-1].strip()
+            logicals[token] = value
+
+    # then look for standard input commands
+
+    script = []
 
     for line in lines_of_input:
         if 'Data line---' in line:
-            result.append(line.replace('Data line---', '').strip())
+            script.append(line.replace('Data line---', '').strip())
 
-    return result
+    return script, logicals
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         raise RuntimeError, '%s ccp4_program.log' % sys.argv[0]
 
-    for line in get_ccp4_commands(open(sys.argv[1], 'r').readlines()):
+    script, logicals = get_ccp4_commands(open(sys.argv[1], 'r').readlines())
+
+    for token in logicals.keys():
+        print token, logicals[token]
+
+    for line in script:
         print line
 
 
