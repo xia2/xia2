03830090b7c6b642f280a5e935720feacb8c7780
Graeme Winter <graeme.winter@gmail.com>
2008-08-11 19:43:30 +0000

    Start of a jiffy to calculate the best resolution limit using the Wilson distribution and a reflection file.

---
+++ Modules/WilsonResolution.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# WilsonResolution.py
+#   Copyright (C) 2008 STFC, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+#
+# 11/AUG/08
+#
+# A module for calculating the limit of diffraction of a crystal based only
+# on the results of integration. This is not worrying about the agreement of 
+# the measurements, simply about the I/sigma of individual observations.
+# To calculate the resolution, the distance and wavelength are necessary,
+# as well as the beam position.
+# 
+# This will use either a recent version of pointless to sum the partial
+# reflections or a spell using sortmtz, scala and mtzdump. The latter
+# if obviously more painful...
+#
+#
+
