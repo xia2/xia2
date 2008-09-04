f80299787765d8e3e7a21c857970d8e0cfbc6d9f
Graeme Winter <graeme.winter@gmail.com>
2008-09-04 20:01:06 +0000

    A start at resolution calculations..

---
+++ Experts/ResolutionExperts.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# ResolutionExperts.py
+# 
+#   Copyright (C) 2008 STFC, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+# 
+# A couple of classes to assist with resolution calculations - these
+# are for calculating resolution (d, s) for either distance / beam /
+# wavelength / position or h, k, l, / unit cell.
+#
+
+import os
+import sys
+import math
+
+# jiffy functions
+
