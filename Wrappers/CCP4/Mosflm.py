08a9b98c8c0e252c93fc554155a7f7aa7d68040d
Graeme Winter <graeme.winter@gmail.com>
2008-12-22 11:58:34 +0000

    Allow slightly greater residual in refinement...

--- Wrappers/CCP4/Mosflm.py
+++ Wrappers/CCP4/Mosflm.py
@@ -1775,7 +1775,7 @@ def Mosflm(DriverType = None):
 
             # fudge factors to prevent Mosflm from being too fussy
             self.input('separation close')
-            self.input('refinement residual 10.0')
+            self.input('refinement residual 12.5')
             self.input('refinement include partials')
 
             # set up the cell refinement - allowing quite a lot of
@@ -2105,7 +2105,7 @@ def Mosflm(DriverType = None):
 
             # fudge factors to prevent Mosflm from being too fussy
             self.input('separation close')
-            self.input('refinement residual 10.0')
+            self.input('refinement residual 12.5')
             self.input('refinement include partials')
 
             # compute the detector limits to use for this...
