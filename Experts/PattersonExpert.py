9291c59ca4099a02ce204329fadc2aa415e540b0
Graeme Winter <graeme.winter@gmail.com>
2008-12-23 11:43:38 +0000

    (stub) Patterson expert

---
+++ Experts/PattersonExpert.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# PattersonExpert.py
+#   Copyright (C) 2006 CCLRC, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+# 
+# 24th July 2007
+# 
+# A small expert to handle patterson map calculations. This will factor in
+# things like the symmetry of the crystal in the analysis.
+
+import os
+import sys
+import math
