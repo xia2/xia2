1d7afed9c8b168513b1afb39ac00441c05b60eb5
Graeme Winter <graeme.winter@gmail.com>
2008-12-26 11:22:02 +0000

    Modified usage of this to now use getopt style arguments (maybe should think about this for the rest of xia in the command line handler?) and also added option to provide reference pdb file calculated for instance from the ccp4 vector program:
    xia2patterson -m PPE_X150A_free.mtz -r patt.pdb
     25.16  21.25   1.74  30.75
     25.16  36.74   1.74  30.75
     22.02  29.00  39.09  28.05
     28.30  29.00  39.09  28.05
     47.17   7.77  37.34  27.81
     47.17  50.23  37.34  27.81
    
    vector xyzin ppe_xe.pdb xyzout patt.pdb << eof
    xyzlim 0 1 0 1 0 1
    eof

--- Applications/xia2patterson.py
+++ Applications/xia2patterson.py
@@ -7,6 +7,7 @@
 
 import sys
 import os
+import math
 
 if not os.environ.has_key('XIA2_ROOT'):
     raise RuntimeError, 'XIA2_ROOT not defined'
@@ -18,25 +19,84 @@ sys.path.append(os.path.join(os.environ['XIA2_ROOT']))
 from Experts.PattersonExpert import anomalous_patterson_jiffy
 
 if __name__ == '__main__':
+
+    # big change - move to a getopts style input which will allow me
+    # to provide much more in the way of command-line arguments
+    #
+    # options are:
+    # 
+    # -m hklin 
+    # -s symmetry
+    # -l low resolution limit
+    # -h high resolution limit
+    # -r reference pdb
+    #
+
+    import getopt
+
+    args = sys.argv[1:]
+
+    hklin = None
+    symmetry = None
+    reference = None
+    dmin = None
+    dmax = None
+
     if len(sys.argv) < 2:
         raise RuntimeError, '%s hklin [symmetry]' % sys.argv[0]
 
-    hklin = sys.argv[1]
-    if len(sys.argv) > 2:
-        symmetry = sys.argv[2]
-    else:
-        symmetry = None
+    optlist, args = getopt.getopt(args, 'm:s:l:h:r:')
 
-    if len(sys.argv) > 4:
-        dmin, dmax = tuple(map(float, sys.argv[3:5]))
-    else:
-        dmin = None
-        dmax = None
+    if args:
+        raise RuntimeError, 'unknown arguments: %s' % str(args)
+
+    for opt in optlist:
+        o = opt[0]
+        v = opt[1]
+
+        if o == '-m':
+            hklin = v
+        elif o == '-r':
+            reference = v
+        elif o == '-s':
+            symmetry = v
+        elif o == '-h':
+            dmin = float(v)
+        elif o == '-l':
+            dmax = float(v)
+
+    if (dmin and not dmax) or (dmax and not dmin):
+        raise RuntimeError, 'only one resolution limit provided'
 
     peaks = anomalous_patterson_jiffy(hklin, symmetry,
                                       dmin = dmin, dmax = dmax)
 
-    for p in peaks:
-        print '%6.2f %6.2f %6.2f %6.2f' % p
+    if not reference:
+
+        for p in peaks:
+            print '%6.2f %6.2f %6.2f %6.2f' % p
+
+    else:
+
+        # first parse the reference file
+
+        reference_peaks = []
+
+        for record in open(reference, 'r').readlines():
+            if 'ATOM' in record:
+                reference_peaks.append(
+                    tuple(map(float, record.split()[5:8])))
+
+        for p in peaks:
+            x, y, z, o = p
+            for r in reference_peaks:
+                rx, ry, rz = r
+
+                if math.sqrt((x - rx) * (x - rx) +
+                             (y - ry) * (y - ry) +
+                             (z - rz) * (z - rz)) < 2:
+                    print '%6.2f %6.2f %6.2f %6.2f' % p
+                             
+                
     
 
