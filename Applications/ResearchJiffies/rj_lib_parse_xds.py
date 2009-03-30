07c57092013ec70d3560138e4fca6ec0f80e12d5
Graeme Winter <graeme.winter@gmail.com>
2009-03-30 12:21:58 +0000

    Got this chapper producing a worthwhile XDS.INP file for correct from INTEGRATE.LP

--- Applications/ResearchJiffies/rj_lib_parse_xds.py
+++ Applications/ResearchJiffies/rj_lib_parse_xds.py
@@ -32,3 +32,102 @@ def rj_parse_idxref_lp(xds_lp_lines):
             cell = tuple(map(float, record.split()[-6:]))
 
     return cell
+
+def rj_parse_integrate_lp(integrate_lp_lines):
+    general_records = []
+    images = None
+    phi = None
+    cell = None
+
+    ignore = [
+        'MINIMUM_VALID_PIXEL_VALUE',
+        'BACKGROUND_PIXEL',
+        'MAXIMUM_ERROR_OF_SPOT_POSITION',
+        'MINPK'
+        ]
+        
+
+    for record in integrate_lp_lines:
+        if '*********' in record:
+            break
+
+        if not '=' in record:
+            continue
+
+        if record.split('=')[0].strip() in ignore:
+            continue
+
+        if 'DATA_RANGE' in record:
+            images = tuple(map(int, record.split('=')[-1].split()))
+            continue
+
+        if 'OSCILLATION_RANGE' in record:
+            phi = float(record.replace('DEGREES', '').split()[-1])
+            continue
+
+        if 'UNIT_CELL_CONSTANTS' in record:
+            cell = tuple(map(float, record.split('=')[-1].split()))
+            continue
+
+        if 'SPACE_GROUP_NUMBER' in record:
+            continue
+
+        if 'STARTING_ANGLE' in record:
+            continue
+
+        if 'RESOLUTION RANGE' in record:
+            continue
+
+        if 'NUMBER OF TRUSTED' in record:
+            continue
+
+        if 'MEAN CONTENTS' in record:
+            continue
+
+        if 'MEAN VALUE' in record:
+            continue
+
+        if 'NUMBER OF' in record:
+            continue
+
+        if 'PIXEL VALUE' in record:
+            continue
+
+        if 'X-RAY_WAVELENGTH' in record:
+            general_records.append(record.replace('ANGSTROM', '').strip())
+            continue
+
+        if record.strip():
+            general_records.append(record.replace('-0', ' -0').strip())
+
+    return images, phi, cell, general_records
+
+if __name__ == '__main__':
+    import sys
+
+    images, phi, cell, junk = rj_parse_integrate_lp(open(sys.argv[1]).readlines())
+
+    print 'DATA_RANGE= %d %d' % images
+    print 'OSCILLATION_RANGE= %.2f' % phi
+    print 'UNIT_CELL_CONSTANTS= %.2f %.2f %.2f %.2f %.2f %.2f' % cell
+    print 'SPACE_GROUP_NUMBER=1'
+
+    for record in junk:
+        print record
+        
+    standard = [
+        'JOB=CORRECT',
+        'MAXIMUM_NUMBER_OF_PROCESSORS=4',
+        'CORRECTIONS=!',
+        'REFINE(CORRECT)=CELL',
+        'OVERLOAD=65000',
+        'DIRECTION_OF_DETECTOR_X-AXIS=1.0 0.0 0.0',
+        'DIRECTION_OF_DETECTOR_Y-AXIS=0.0 1.0 0.0',
+        'TRUSTED_REGION=0.0 1.41'
+        
+        ]
+
+    for record in standard:
+        print record
+        
+        
