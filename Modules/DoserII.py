9257d2c0218965acf7b4df89e8e5683be8817c96
Graeme Winter <graeme.winter@gmail.com>
2009-06-29 08:49:34 +0000

    Woo! Pure Python implementation of doser, using CCTBX.

---
+++ Modules/DoserII.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# DoserII.py
+# 
+#   Copyright (C) 2009 Diamond Light Source, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+# 
+# A replacement for the FORTRAN program DOSER, which adds two columns DOSE
+# and TIME to unmerged MTZ files, so that they can be analysed dose-wise
+# by CHEF. In reality, the functionality of both could be mapped to a 
+# single program now...
+# 
+
+import os
+import sys
+
+from iotbx import mtz
+from cctbx.array_family import flex
+
+def Doser(hklin, hklout, doses, times):
+    '''Add doses and times from dictionaries doses, times to hklin to
+    produce hklout. The dictionaries are indexed by the BATCH column in
+    hklin. Will raise exception if no BATCH column.'''
+
+    # instantiate the MTZ object representation
+
+    mtz_obj = mtz.object(file_name = hklin)
+    
+    # construct a list of columns in the file
+
+    cnames = []
+    ctypes = []
+
+    batch_column = None
+    batch_dataset = None
+
+    for crystal in mtz_obj.crystals():
+        for dataset in crystal.datasets():
+            for column in dataset.columns():
+                if column.label() == 'BATCH':
+                    batch_column = column
+                    batch_dataset = dataset
+
+    if not batch_column:
+        raise RuntimeError, 'no BATCH column found in %s' % hklin
+
+    # right, so get the values out from the batch column, create a flex
+    # array of the same size and assign DOSE, TIME, then add these to the
+    # same dataset.
+
+    batch_column_values = batch_column.extract_values(
+        not_a_number_substitute = -1)
+
+    dose_column = batch_dataset.add_column(label = 'DOSE', type = 'F')
+    time_column = batch_dataset.add_column(label = 'TIME', type = 'F')
+
+    dose_column_values = flex.float()
+    time_column_values = flex.float()
+
+    valid = flex.bool()
+
+    for b in batch_column_values:
+        dose_column_values.append(doses.get(b, -1.0))
+        time_column_values.append(times.get(b, -1.0))
+        valid.append(True)
+
+    # add the columns back to the MTZ file structure
+
+    dose_column.set_values(values = dose_column_values,
+                           selection_valid = valid)
+    time_column.set_values(values = time_column_values,
+                           selection_valid = valid)
+
+    # and write this lot out as hklout
+    
+    mtz_obj.write(file_name = hklout)
+
+if (__name__ == "__main__"):
+
+    doses = { }
+    times = { }
+
+    for record in open('doser.in', 'r').readlines():
+        values = record.split()
+        if not values:
+            continue
+        batch = int(values[1])
+        dose = float(values[3])
+        time = float(values[5])
+        
+        doses[batch] = dose
+        times[batch] = time
+
+    Doser(hklin = 'TS03_12287_chef_INFL.mtz',
+          hklout = 'hklout.mtz',
+          doses = doses,
+          times = times)
