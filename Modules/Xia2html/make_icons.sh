58eb7fab60a3a2381f885e904cbd65cebd095177
Peter Briggs <peter.briggs@diamond.ac.uk>
2009-11-19 18:55:50 +0000

    Added drawing instructions for new icon "img_abandoned.png".

--- Modules/Xia2html/make_icons.sh
+++ Modules/Xia2html/make_icons.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -fe
 #
-# CVS Id $Id: make_icons.sh,v 1.1 2009/11/12 18:43:07 pjx Exp $
+# CVS Id $Id: make_icons.sh,v 1.2 2009/11/19 18:55:50 pjx Exp $
 #
 # Use ImageMagick convert to make little circle PNGs
 # Each PNG represents one of the possible image statuses
@@ -21,5 +21,7 @@ convert -size 12x12 xc:transparent -fill lightblue -stroke white -draw "rectangl
 convert -size 12x12 xc:transparent -fill black -stroke white -draw "rectangle 0,0 12,12" icons/img_many_bad.png
 
 convert -size 12x12 xc:transparent -fill yellow -stroke white -draw "rectangle 0,0 12,12" icons/img_bad_rmsd.png
+
+convert -size 12x12 xc:transparent -fill orange -stroke white -draw "rectangle 0,0 12,12" icons/img_abandoned.png
 
 convert -size 12x12 xc:transparent icons/img_blank.png
