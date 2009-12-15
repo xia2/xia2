0fcaaf10043a007300d60798cfc532b8368e1fd3
Peter Briggs <peter.briggs@diamond.ac.uk>
2009-12-15 09:47:54 +0000

    Fixed white border now along all sides of each icon, plus the "blank" image is completely white (rather than being transparent).

--- Modules/Xia2html/make_icons.sh
+++ Modules/Xia2html/make_icons.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -fe
 #
-# CVS Id $Id: make_icons.sh,v 1.2 2009/11/19 18:55:50 pjx Exp $
+# CVS Id $Id: make_icons.sh,v 1.3 2009/12/15 09:47:54 pjx Exp $
 #
 # Use ImageMagick convert to make little circle PNGs
 # Each PNG represents one of the possible image statuses
@@ -12,16 +12,16 @@
 # -draw "circle x0,y0 x1,y1"
 # uses x0,y0 = centre and x1,y1 = any point on the perimeter
 #
-convert -size 12x12 xc:transparent -fill green -stroke white -draw "rectangle 0,0 12,12" icons/img_good.png
+convert -size 12x12 xc:white -fill green -stroke white -draw "rectangle 0,0 11,11" icons/img_good.png
 
-convert -size 12x12 xc:transparent -fill red -stroke white -draw "rectangle 0,0 12,12" icons/img_overloaded.png
+convert -size 12x12 xc:white -fill red -stroke white -draw "rectangle 0,0 11,11" icons/img_overloaded.png
 
-convert -size 12x12 xc:transparent -fill lightblue -stroke white -draw "rectangle 0,0 12,12" icons/img_ok.png
+convert -size 12x12 xc:white -fill lightblue -stroke white -draw "rectangle 0,0 11,11" icons/img_ok.png
 
-convert -size 12x12 xc:transparent -fill black -stroke white -draw "rectangle 0,0 12,12" icons/img_many_bad.png
+convert -size 12x12 xc:white -fill black -stroke white -draw "rectangle 0,0 11,11" icons/img_many_bad.png
 
-convert -size 12x12 xc:transparent -fill yellow -stroke white -draw "rectangle 0,0 12,12" icons/img_bad_rmsd.png
+convert -size 12x12 xc:white -fill yellow -stroke white -draw "rectangle 0,0 11,11" icons/img_bad_rmsd.png
 
-convert -size 12x12 xc:transparent -fill orange -stroke white -draw "rectangle 0,0 12,12" icons/img_abandoned.png
+convert -size 12x12 xc:white -fill orange -stroke white -draw "rectangle 0,0 11,11" icons/img_abandoned.png
 
-convert -size 12x12 xc:transparent icons/img_blank.png
+convert -size 12x12 xc:white icons/img_blank.png
