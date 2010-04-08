1cb65510067312dc00f392ea266de79dcc06df3e
Graeme Winter <graeme.winter@gmail.com>
2010-04-08 10:58:58 +0000

    Ok, now it looks like I have the scaling working right - as both linear scales minimizing r and log scales

--- Toolkit/KBScale.py
+++ Toolkit/KBScale.py
@@ -12,7 +12,7 @@ def residual_kb(ref, work, params):
 
     res = sum([math.fabs(r[1] - w[1] * kb(w[0], k, b)) \
                for r, w in zip(ref, work)]) / \
-               sum(math.fabs(r[1]) for r in ref)
+               sum([math.fabs(r[1]) for r in ref])
 
     return res
 
@@ -53,27 +53,83 @@ class kb_scale:
 
     def get_kb(self):
         return tuple(self.x)
+
+# log versions
+
+def lkb(s, k, b):
+    return k - b * s
+
+def residual_lkb(ref, work, params):
+    
+    k, b = params
+
+    res = sum([math.pow(r[1] - w[1] - lkb(w[0], k, b), 2) \
+               for r, w in zip(ref, work)]) 
+
+    return res
+
+def gradients_lkb(ref, work, params):
+
+    eps = 1.0e-6
+
+    g = flex.double()
+
+    for j in range(2):
+        rs = []
+        for signed_eps in [- eps, eps]:
+            params_eps = params[:]
+            params_eps[j] += signed_eps
+            rs.append(residual_lkb(ref, work, params_eps))
+        g.append((rs[1] - rs[0]) / (2 * eps))
+
+    return g
+        
+class lkb_scale:
+
+    def __init__(self, ref, work):
+
+        self.x = flex.double([0.0, 0.0])
+        self._ref = [(r[0], math.log(r[1])) for r in ref]
+        self._work = [(w[0], math.log(w[1])) for w in work]
+
+        return
+
+    def refine(self):
+
+        return lbfgs.run(target_evaluator = self)
+
+    def compute_functional_and_gradients(self):
+
+        return residual_lkb(self._ref, self._work, self.x), \
+               gradients_lkb(self._ref, self._work, self.x)
+
+    def get_kb(self):
+        return math.exp(self.x[0]), self.x[1]
         
 if __name__ == '__main__':
 
     import random
 
-    k = 5
-    b = 10
+    for size in [10 ** r for r in range(1, 5)]:
 
-    ref = [(random.random(), random.random()) for k in range(1000)]
+        print size
+        
+        k = 5
+        b = 10
 
-    work = [(r[0], r[1] / kb(r[0], k, b)) for r in ref]
+        ref = [(0.5 * random.random(), 10 * random.random()) \
+               for j in range(size)]
 
-    print residual_kb(ref, work, (1, 0))
-    print residual_kb(ref, work, (k, b))
+        work = [(r[0], r[1] / kb(r[0], k, b)) for r in ref]
 
-    kbs = kb_scale(ref, work)
+        kbs = kb_scale(ref, work)
 
-    kbs.refine()
+        kbs.refine()
 
-    k2, b2 = kbs.get_kb()
+        print 'Linear: %.2f %.2f' % kbs.get_kb()
 
-    print k2, b2
+        lkbs = lkb_scale(ref, work)
 
-    print residual_kb(ref, work, (k2, b2))
+        lkbs.refine()
+
+        print 'Log:    %.2f %.2f' % kbs.get_kb()
