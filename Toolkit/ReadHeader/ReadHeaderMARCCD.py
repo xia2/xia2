df8d2d8aa8165c45b7e7f4a32a3b66ebb878e9bd
Graeme Winter <graeme.winter@gmail.com>
2010-07-13 12:45:58 +0000

    Now implemented reading for marccd - better than before, can get the serial number from the comment field

--- Toolkit/ReadHeader/ReadHeaderMARCCD.py
+++ Toolkit/ReadHeader/ReadHeaderMARCCD.py
@@ -51,21 +51,86 @@ class ReadHeaderMARCCD(ReadHeader):
 
         if byte_order == little_endian or byte_order == big_endian:
             header_unsigned_ints = struct.unpack('768I', marccd_header_bytes)
+            header_signed_ints = struct.unpack('768i', marccd_header_bytes)
         else:
             header_unsigned_ints = struct.unpack('768I', marccd_header_bytes)
             header_array = array.array('I', header_unsigned_ints)
             header_array.byteswap()
             header_unsigned_ints = tuple(header_array)
 
-        nfast, nslow = header_unsigned_ints[20], header_unsigned_ints[21]
+            header_signed_ints = struct.unpack('768i', marccd_header_bytes)
+            header_array = array.array('i', header_signed_ints)
+            header_array.byteswap()
+            header_signed_ints = tuple(header_array)
 
-        print nfast, nslow
+        # now we have the header right, copy & transform to correct format
 
+        self.image_size_pixels_fast = header_unsigned_ints[20]
+        self.image_size_pixels_slow = header_unsigned_ints[21]
 
+        #self.date_struct = 
+        #self.epoch_ms = 
 
-    def __str__(self):
-        return ''
+        self.exposure_time_s = 0.001 * header_unsigned_ints[164]
+        
+        if header_unsigned_ints[160]:
+            self.distance_mm = 0.001 * header_signed_ints[160]
+        else:
+            self.distance_mm = 0.001 * header_signed_ints[174]
+            
+        self.wavelength_angstroms = 0.00001 * header_unsigned_ints[227]
 
+        self.pixel_size_mm_fast = 0.000001 * header_unsigned_ints[193]
+        self.pixel_size_mm_slow = 0.000001 * header_unsigned_ints[194]
+
+        self.pixel_depth_bytes = 2
+
+        axis_names = ['twotheta', 'omega', 'chi', 'kappa',
+                      'phi', 'delta', 'gamma']
+
+        axis = header_unsigned_ints[183]
+        self.axis_name = axis_names[axis]
+
+        self.osc_start_deg = 0.001 * header_signed_ints[167 + axis]
+
+        if header_signed_ints[175 + axis]:
+            osc_end = 0.001 * header_signed_ints[175 + axis]
+            self.osc_range_deg = osc_end - self.osc_start_deg
+        else:
+            self.osc_range_deg = 0.001 * header_signed_ints[184]
+            
+        self.angle_twotheta_deg = 0.001 * header_signed_ints[167]
+        self.angle_kappa_deg = 0.001 * header_signed_ints[170]
+        self.angle_chi_deg = 0.001 * header_signed_ints[169]
+        
+        self.image_offset = 0
+        self.maximum_value = header_unsigned_ints[26]
+
+        self.beam_centre_pixels_fast = 0.001 * header_signed_ints[161]
+        self.beam_centre_pixels_slow = 0.001 * header_signed_ints[162]
+
+        self.header_length = 4096
+
+        datestring = marccd_header_bytes[1376:1408].strip()
+
+        month = int(datestring[:2])
+        day = int(datestring[2:4])
+        hour = int(datestring[4:6])
+        minute = int(datestring[6:8])
+        year = int(datestring[8:12])
+        second = int(datestring[13:15])
+        self.date_struct = datetime.datetime(year, month, day,
+                                             hour, minute, second).timetuple()
+
+        self.detector_serial_number = 'N/A'
+        
+        comments = marccd_header_bytes[1440:1440 + 512]
+
+        for record in comments.split('\n'):
+            if 'Detector Serial Number' in record:
+                self.detector_serial_number = record.split()[-1]
+
+        return
 
 if __name__ == '__main__':
 
