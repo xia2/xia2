7508f04924e9cf65d9348ddddb06430788734995
Graeme Winter <graeme.winter@gmail.com>
2010-07-27 13:53:06 +0000

    Making some progress on unpackinng the cbf with the PyCifRW library: beam centre is fiddly though...

--- Toolkit/ReadHeader/ReadHeaderCBF.py
+++ Toolkit/ReadHeader/ReadHeaderCBF.py
@@ -10,12 +10,17 @@
 # image header ReadHeader class.
 
 from ReadHeader import ReadHeader
-from ReadHeaderSMV import ReadHeaderSMV
 
+import tempfile
 import time
+import os
+
+from PyCifRW import CifFile
 
 class ReadHeaderCBF(ReadHeader):
-    '''A class to read CBF image headers.'''
+    '''A class to read CBF image headers. N.B. to work around features of the
+    PyCIFRW library, will first read the file into memory and dumo out
+    just the header text (i.e. everything to the first binary block.)'''
 
     def __init__(self, image = None):
         ReadHeader.__init__(self)
@@ -28,8 +33,46 @@ class ReadHeaderCBF(ReadHeader):
     def _read_cbf_header(self, image):
         '''Read the contents of the header of this image.'''
 
-        size, header = ReadHeaderSMV(image)
+        # first copy to a temporary directory only the header information
 
+        t = time.time()
+        
+        image_data = open(image, 'rb').read()
+        header_text = image_data[:image_data.index('_array_data.array_id')]
+
+        fid, fname = tempfile.mkstemp()
+        fout = os.fdopen(fid, 'wb')
+        fout.write(header_text)
+        fout.close()
+
+        cif = CifFile.ReadCif(fname)
+
+        os.remove(fname)
+
+        # now pull the information from the image header...
+
+        i1 = cif['image_1']
+
+        pixel_order = tuple(map(int, i1['_array_element_size.index']))
+        pixel_dim = tuple(map(float, i1['_array_element_size.size']))
+        image_order = tuple(map(int, i1['_array_structure_list.precedence']))
+        image_dim = tuple(map(int, i1['_array_structure_list.dimension']))
+
+        assert(image_order in [(1, 2), (2, 1)])
+
+        overload = int(i1['_array_intensities.overload'])
+
+        wavelength = float(i1['_diffrn_radiation_wavelength.wavelength'])
+        distance = float(i1['_diffrn_measurement.sample_detector_distance'])
+        exposure_time = float(i1['_diffrn_scan_frame.integration_time'])
+        # this is YYYY-MM-DDTHH:MM:SS
+        date_str = i1['_diffrn_scan_frame.date']
+
+        
+        
+        if True:
+            return
+        
         beam_centre_x_mm = float(header['BEAM_CENTER_X'])
         beam_centre_y_mm = float(header['BEAM_CENTER_Y'])
         
@@ -41,8 +84,6 @@ class ReadHeaderCBF(ReadHeader):
         self.exposure_time_s = float(header['TIME'])
         self.distance_mm = float(header['DISTANCE'])
         self.wavelength_angstroms = float(header['WAVELENGTH'])
-
-        pixel_size = float(header['PIXEL_SIZE'])
 
         self.pixel_size_mm_fast = pixel_size
         self.pixel_size_mm_slow = pixel_size
@@ -77,4 +118,4 @@ if __name__ == '__main__':
     import sys
 
     for arg in sys.argv[1:]:
-        print ReadHeaderCBF(arg)
+        ReadHeaderCBF(arg)
