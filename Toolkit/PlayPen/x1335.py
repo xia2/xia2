6e24bac4ac2dc8dadabea60aef7a1cded599a916
Graeme Winter <graeme.winter@gmail.com>
2011-02-07 14:46:31 +0000

    Added readout of SWEEP names from HKL files...

--- Toolkit/PlayPen/x1335.py
+++ Toolkit/PlayPen/x1335.py
@@ -5,9 +5,26 @@ def get_ccs(xscale_lp):
 
     ccs = { }
 
+    file_names = { }
+
     xmax = 0
 
     records = open(xscale_lp).readlines()
+
+    # first scan through to get the file names...
+
+    for j, record in enumerate(records):
+        if 'NUMBER OF UNIQUE REFLECTIONS' in record:
+
+            k = j + 5
+
+            while len(records[k].split()) == 5:
+                values = records[k].split()
+                file_names[int(values[0])] = values[-1]
+
+                k += 1
+
+            break
 
     for j, record in enumerate(records):
 
@@ -30,10 +47,14 @@ def get_ccs(xscale_lp):
 
                 k += 1
 
+            break
+
     for j in range(xmax):
         ccs[(j + 1, j + 1)] = (0, 0)
-        print '%4d %6.4f' % (j + 1, sum([ccs[(i + 1, j + 1)][1]
-                                         for i in range(xmax)]) / xmax)
+        print '%4d %6.4f %s' % (j + 1,
+                                sum([ccs[(i + 1, j + 1)][1]
+                                     for i in range(xmax)]) / (xmax - 1),
+                                file_names[j + 1])
 
 if __name__ == '__main__':
 
