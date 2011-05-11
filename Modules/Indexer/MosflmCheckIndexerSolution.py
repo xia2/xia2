e97705fc328f6c7896798fb31618194b0344cbbc
Graeme Winter <graeme.winter@gmail.com>
2011-05-11 11:02:27 +0000

    This gave a false positive, switching off - need to find a better way to implement this test... #1348

--- Modules/Indexer/MosflmCheckIndexerSolution.py
+++ Modules/Indexer/MosflmCheckIndexerSolution.py
@@ -94,7 +94,7 @@ def mosflm_check_indexer_solution(indexer):
 
     sg = sgtbx.space_group(spacegroup)
 
-    if not (sg.n_ltr() - 1):
+    if not (sg.n_ltr() - 1) or True:
         # primitive solution - just return ... something
         return None, None, None, None
 
