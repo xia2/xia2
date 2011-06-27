6fc92c4facb48d0ca512fd8bd25a6f5440e30c47
Graeme Winter <graeme.winter@gmail.com>
2011-06-27 08:45:11 +0000

    Started to add a unit test which could be included in cctbx

--- Toolkit/cbfdump.py
+++ Toolkit/cbfdump.py
@@ -43,6 +43,117 @@ def find_detector_id(cbf_handle):
 
     return detector_id
 
+def test_example_adsc_cif_header():
+    '''A unit test to ensure that the reading of CIF headers using pycbf
+    is working correcty.'''
+
+    cbf_image = 'example-adsc-cif-header.cif'
+
+    cbf_handle = pycbf.cbf_handle_struct()
+    cbf_handle.read_file(cbf_image, pycbf.MSG_DIGEST)
+
+    cbf_handle.rewind_datablock()
+
+    # find the direct beam vector - takes a few steps
+    cbf_handle.find_category('axis')
+
+    # find record with equipment = source
+    cbf_handle.find_column('equipment')
+    cbf_handle.find_row('source')
+
+    # then get the vector and offset from this
+    
+    beam_direction = []
+    
+    for j in range(3):
+        cbf_handle.find_column('vector[%d]' % (j + 1))
+        beam_direction.append(cbf_handle.get_doublevalue())
+    
+    detector = cbf_handle.construct_detector(0)
+
+    beam = detector.get_beam_center()
+
+    beam_pixel = tuple(beam[:2])
+    beam_mm = tuple(beam[2:])
+    detector_normal = tuple(detector.get_detector_normal())
+    distance = detector.get_detector_distance()
+    pixel = (detector.get_inferred_pixel_size(1),
+             detector.get_inferred_pixel_size(2))
+    
+    gonio = cbf_handle.construct_goniometer()
+    
+    axis = tuple(gonio.get_rotation_axis())
+    angles = tuple(gonio.get_rotation_range())
+    
+    date = cbf_handle.get_datestamp()
+
+    time = cbf_handle.get_timestamp()
+    size = tuple(cbf_handle.get_image_size(0))
+    exposure = cbf_handle.get_integration_time()
+    overload = cbf_handle.get_overload(0)
+    wavelength = cbf_handle.get_wavelength()
+
+    assert(size == (3072, 3072))
+    assert(math.fabs(pixel[0] - 0.103) < 0.01)
+    assert(math.fabs(pixel[1] - 0.103) < 0.01)
+    assert(math.fabs(distance - 116) < 0.1)
+
+    assert(math.fabs(detector_normal[0] - 0.0) < 0.01)
+    assert(math.fabs(detector_normal[1] - 0.0) < 0.01)
+    assert(math.fabs(detector_normal[2] - 1.0) < 0.01)
+    
+    assert(math.fabs(exposure - 0.5) < 0.01)
+    assert(int(overload) = 65535)
+
+    if do_print: print 'Beam:       %.2f %.2f' % beam_mm
+    if do_print: print 'Beam:       %.2f %.2f %.2f' % tuple(beam_direction)
+    
+    if do_print: print 'Goniometer:'
+    if do_print: print 'Axis:       %.2f %.2f %.2f' % axis
+    if do_print: print 'Angles:     %.2f %.2f' % angles
+    
+    if do_print: print 'Experiment:'
+    if do_print: print 'Wavelength: %.5f' % wavelength
+    
+    # now need to dig out the detector axes
+    # perhaps bodge this by looking at the displacements of pixels in the
+    # fast and slow directions?
+    
+    origin = detector.get_pixel_coordinates(0, 0)
+    fast = detector.get_pixel_coordinates(0, 1)
+    slow = detector.get_pixel_coordinates(1, 0)
+
+    if do_print: print 'Origin:     %.2f %.2f %.2f' % tuple(origin)
+    
+    dfast = [fast[j] - origin[j] for j in range(3)]
+    dslow = [slow[j] - origin[j] for j in range(3)]
+    
+    lfast = math.sqrt(sum([dfast[j] * dfast[j] for j in range(3)]))
+    lslow = math.sqrt(sum([dslow[j] * dslow[j] for j in range(3)]))
+    
+    fast = tuple([dfast[j] / lfast for j in range(3)])
+    slow = tuple([dslow[j] / lslow for j in range(3)])
+    
+    if do_print: print 'Fast direction: %.2f %.2f %.2f' % fast
+    if do_print: print 'Slow direction: %.2f %.2f %.2f' % slow
+
+    if hasattr(detector, 'get_detector_axis_fast'):
+
+        if do_print: print 'CBF fast: %.2f %.2f %.2f' % \
+           tuple(detector.get_detector_axis_fast())
+        if do_print: print 'CBF slow: %.2f %.2f %.2f' % \
+           tuple(detector.get_detector_axis_slow())
+        
+    detector.__swig_destroy__(detector)
+    del(detector)
+
+    gonio.__swig_destroy__(gonio)
+    del(gonio)
+
+    return
+
+    
+
 def cbfdump(cbf_image, do_print = False):
 
     cbf_handle = pycbf.cbf_handle_struct()
