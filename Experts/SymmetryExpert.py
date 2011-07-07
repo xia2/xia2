e64d44a26545c82393320d3b5676d4b049514fc1
Graeme Winter <graeme.winter@gmail.com>
2011-07-07 15:19:04 +0000

    Moved reduce to attic from SE

--- Experts/SymmetryExpert.py
+++ Experts/SymmetryExpert.py
@@ -239,41 +239,7 @@ def modulo(m, x):
     while x > m:
         x -= m
 
-    return x
-    
-def reduce(symops, v):
-    '''Reduce the vector v to the minimal standard i.e. the symmetry
-    related one with x < y < z ideally. x, y, z are fractional
-    coordinates.'''
-
-    from MatrixExpert import matvecmul
-
-    reduced = v
-
-    for s in symops:
-        v2 = matvecmul(s, v)
-
-        v2[0] = modulo(1.0, v2[0])
-        v2[1] = modulo(1.0, v2[1])
-        v2[2] = modulo(1.0, v2[2])
-        
-        if v2[0] > reduced[0]:
-            continue
-        if v2[0] < reduced[0]:
-            reduced = v2
-            continue
-        if v2[1] > reduced[1]:
-            continue
-        if v2[1] < reduced[1]:
-            reduced = v2
-            continue
-        if v2[2] > reduced[2]:
-            continue
-        if v2[2] < reduced[2]:
-            reduced = v2
-            continue
-
-    return reduced
+    return x    
 
 if __name__ == '__main__':
 
