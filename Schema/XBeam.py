f680fdd8f061f627e8e887ceeaa22eb7f48c0c6e
Graeme Winter <graeme.winter@gmail.com>
2011-07-18 08:24:53 +0000

    Putative implementation of XBeam class, which is straighforward as it contains only the direction and wavelength.

---
+++ Schema/XBeam.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# XBeam.py
+#   Copyright (C) 2011 Diamond Light Source, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is 
+#   included in the root directory of this package.
+#  
+# A model for the beam for the "updated experimental model" project documented
+# in internal ticket #1555. This is not designed to be used outside of the 
+# XSweep classes.
+
+import math
+import pycbf
+from scitbx import matrix
+from scitbx.math import r3_rotation_axis_and_angle_from_matrix
+
+class XBeam:
+    '''A class to represent the X-ray primary beam for a standard rotation
+    geometry diffraction experiment. We assume (i) that the beam is
+    monochromatic (ii) that the beam is reasonably parallel. In the first
+    instance the only parameters are direction and wavelength, however over
+    time the intention is to add polarization, size, divergence, transmission
+    etc.'''
+
+    def __init__(self, direction, wavelength):
+        '''Initialize the beam model, with the direction of the beam (i.e.
+        towards the source) given in the CBF coordinate frame and the
+        wavelength in Angstroms.'''
+
+        assert(len(direction) == 3)
+        
+        self._direction = matrix.col(direction)
+        self._wavelength = wavelength
+        
+        return
+
+    def get_direction(self):
+        '''Get the beam direction.'''
+        
+        return self._direction.elems
+
+    def get_direction_c(self):
+        '''Get the beam direction as a cctbx vector.'''
+
+        return self._direction
+
+    def get_wavelength(self):
+        '''Get the wavelength in Angstroms.'''
+
+        return self._wavelength
+
+class XBeamFactory:
+    '''A factory class for beam objects, which encapsulate standard beam
+    models. In cases where a full cbf desctiption is available this
+    will be used, otherwise simplified descriptions can be applied.'''
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def Simple(wavelength):
+        '''Construct a beam object on the principle that the beam is aligned
+        with the +z axis, as is quite normal.'''
+
+        return XBeam((0.0, 0.0, 1.0), wavelength)
+
+    @staticmethod
+    def imgCIF(cif_file):
+        '''Initialize a detector model from an imgCIF file.'''
+
+        cbf_handle = pycbf.cbf_handle_struct()
+        cbf_handle.read_file(cif_file, pycbf.MSG_DIGEST)
+
+        cbf_handle.find_category('axis')
+
+        # find record with equipment = source
+        cbf_handle.find_column('equipment')
+        cbf_handle.find_row('source')
+        
+        # then get the vector and offset from this
+        direction = []
+        
+        for j in range(3):
+            cbf_handle.find_column('vector[%d]' % (j + 1))
+            direction.append(cbf_handle.get_doublevalue())
+
+        # and the wavelength
+        wavelength = cbf_handle.get_wavelength()
+
+        return XBeam(direction, wavelength)
+
+    @staticmethod
+    def CBF(cbf_file):
+        '''Initialize a beam model from a CBF file.'''
+        
+        return imgCIF(cbf_file)
+    
+
+        
+        
