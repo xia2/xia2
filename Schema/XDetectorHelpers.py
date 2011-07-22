f88a5b82e4940b4a010fb69abf27b321a7ac2666
Graeme Winter <graeme.winter@gmail.com>
2011-07-22 10:25:41 +0000

    Removed some chunder from the file now that the code is (apparently) debugged...

--- Schema/XDetectorHelpers.py
+++ Schema/XDetectorHelpers.py
@@ -64,13 +64,14 @@ def compute_frame_rotation(original, final):
     F = matrix.sqr(_x.elems + _y.elems + _z.elems).transpose()
     assert((F.determinant() - 1.0) < 1.0e-7)
 
-    # #1 rotate about x ^ (1, 0, 0) - if they are not coincident
+    # #1 rotate about x ^ (1, 0, 0) - if they are not coincident,
+    # rotate about _x ^ _y if they are colinear but in opposite
+    # directions
    
     if _x.angle(x) % math.pi:
         _ra_x = _x.cross(x)
         _a_x = _x.angle(x)
     elif math.fabs(_x.angle(x) - math.pi) < 1.0e-7:
-        # rotate by 180 degrees about an axis perpendicular to x
         _ra_x = _x.cross(_y)
         _a_x = math.pi
     else:
@@ -86,18 +87,9 @@ def compute_frame_rotation(original, final):
     _a_z = _z.angle(_m_x * z)
     _m_z = _ra_z.axis_and_angle_as_r3_rotation_matrix(- _a_z)
     
-    # _m is matrix to rotate XDS coordinate frame => CBF frame
-    
     _m = _m_z * _m_x
     
     assert(math.fabs(_m.determinant() - 1.0) < 1.0e-7)
-
-    print '%5.2f %5.2f %5.2f' % _x.elems, '=>', \
-          '%5.2f %5.2f %5.2f' % (_m * x).elems
-    print '%5.2f %5.2f %5.2f' % _y.elems, '=>', \
-          '%5.2f %5.2f %5.2f' % (_m * y).elems
-    print '%5.2f %5.2f %5.2f' % _z.elems, '=>', \
-          '%5.2f %5.2f %5.2f' % (_m * z).elems
 
     return _m
 
