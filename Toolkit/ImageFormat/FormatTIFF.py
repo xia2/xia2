426adadb241e5c164ea1cc34d10bdc2745955255
Graeme Winter <graeme.winter@gmail.com>
2011-07-25 15:01:03 +0000

    Progress on reading TIFF files - can now read the basic TIFF header for mar and pilatus machines, starting now on custom readers.
    cctbx.python FormatTIFFHelpers.py /dls/i16/data/2011/cm2063-3/pilatus100k/test3498670.tif
    (487 x 195) @ 32 + 4096

--- Toolkit/ImageFormat/FormatTIFF.py
+++ Toolkit/ImageFormat/FormatTIFF.py
@@ -1,1 +1,66 @@
-print 'Loading FormatTIFF'
+#!/usr/bin/env python
+# FormatTIFF.py
+#   Copyright (C) 2011 Diamond Light Source, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is
+#   included in the root directory of this package.
+#
+# Implementation of an ImageFormat class to read TIFF format image but not -
+# in the first instance - actually provide a full image representation. This
+# is simply there to set everything up for the ADSC and Rigaku Saturn image
+# readers which really will acquire the full image including header information
+# and generate the experimental model representations.
+
+from Format import Format
+from FormatTiffHelpers import read_basic_tiff_header
+from FormatTiffHelpers import LITTLE_ENDIAN
+from FormatTiffHelpers import BIG_ENDIAN
+
+class FormatTIFF(Format):
+    '''An image reading class for TIFF format images i.e. those from Dectris
+    and Rayonix, which start with a standard TIFF header (which is what is
+    handled here) and have their own custom header following, which must
+    be handled by the inheriting subclasses.'''
+
+    @staticmethod
+    def understand(image_file):
+        '''Check to see if this looks like an TIFF format image, i.e. we can
+        make sense of it.'''
+
+        try:
+            width, height, depth, header, order = read_basic_tiff_header(
+                image_file)
+            return 1
+        
+        except:
+            pass
+
+        return 0
+
+    def __init__(self, image_file):
+        '''Initialise the image structure from the given file.'''
+        
+        assert(FormatTIFF.understand(image_file) > 0)
+
+        Format.__init__(self, image_file)
+
+        return
+
+    def _start(self):
+        '''Open the image file, read the image header, copy it into memory
+        for future inspection.'''
+
+        width, height, depth, header, order = read_basic_tiff_header(
+            image_file)
+
+        self._tiff_width = width
+        self._tiff_height = height
+        self._tiff_depth = depth // 8
+        self._tiff_header_bytes = open(image_file, 'rb').read(header)
+        self._tiff_byte_order = order
+
+        return
+
+
+    
+
---
+++ Toolkit/ImageFormat/FormatTIFFHelpers.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# FormatTIFFHelpers.py
+#   Copyright (C) 2011 Diamond Light Source, Graeme Winter
+#
+#   This code is distributed under the BSD license, a copy of which is
+#   included in the root directory of this package.
+#
+# Helper code to assist with reading TIFF file headers, which are by their
+# nature binary so we need to mess with things like byte swapping.
+
+import struct
+
+LITTLE_ENDIAN = 1234
+BIG_ENDIAN = 4321
+
+def tiff_byte_order(filename):
+    '''Determine the byte order for the file from the magic numbers at the
+    very start of the file.'''
+
+    four_bytes = open(filename, 'rb').read(4)
+
+    if 'II' in four_bytes[:2]:
+        assert(struct.unpack('<H', four_bytes[2:])[0] == 42)
+        return LITTLE_ENDIAN
+    elif 'MM' in four_bytes[:2]:
+        assert(struct.unpack('>H', four_bytes[2:])[0] == 42)
+        return BIG_ENDIAN
+
+    raise RuntimeError, '%s not recognised as TIFF' % filename
+
+def read_basic_tiff_header(filename):
+    '''Read the TIFF header (assuming for the moment a 4k header...) and
+    return ... something.'''
+
+    # things we hope to learn from the vanilla TIFF header
+
+    image_width = None
+    image_height = None
+    image_depth = None
+    header_size = None
+    byte_order = None
+    
+    # OK then let's get started - and let's assume that the size is > 1 kb
+    
+    byte_order = tiff_byte_order(filename)
+    tiff_header = open(filename, 'rb').read(1024)
+
+    offset = struct.unpack('<I', tiff_header[4:8])[0]
+
+    ntags = struct.unpack('<H', tiff_header[offset:offset + 2])[0]
+    start = offset + 2
+
+    for j in range(ntags):
+        if byte_order == LITTLE_ENDIAN:
+            type_desc = struct.unpack('<H', tiff_header[start:start + 2])[0]
+            start += 2
+            type_type = struct.unpack('<H', tiff_header[start:start + 2])[0]
+            start += 2
+            type_size = struct.unpack('<I', tiff_header[start:start + 4])[0]
+            start += 4
+            if type_type == 4:
+                type_offset_or_value = struct.unpack(
+                    '<I', tiff_header[start:start + 4])[0]
+                start += 4
+            elif type_type == 3:
+                type_offset_or_value = struct.unpack(
+                    '<H', tiff_header[start:start + 2])[0]
+                start += 4
+        else:
+            type_desc = struct.unpack('>H', tiff_header[start:start + 2])[0]
+            start += 2
+            type_type = struct.unpack('>H', tiff_header[start:start + 2])[0]
+            start += 2
+            type_size = struct.unpack('>I', tiff_header[start:start + 4])[0]
+            start += 4
+            if type_type == 4:
+                type_offset_or_value = struct.unpack(
+                    '>I', tiff_header[start:start + 4])[0]
+                start += 4
+            elif type_type == 3:
+                type_offset_or_value = struct.unpack(
+                    '>H', tiff_header[start:start + 2])[0]
+                start += 4
+                
+        if type_desc == 256:
+            image_width = type_offset_or_value
+        elif type_desc == 257:
+            image_height = type_offset_or_value
+        elif type_desc == 258:
+            image_depth = type_offset_or_value
+        elif type_desc == 273:
+            header_size = type_offset_or_value
+            
+    return image_width, image_height, image_depth, header_size, byte_order
+
+if __name__ == '__main__':
+
+    import sys
+
+    for arg in sys.argv[1:]:
+    
+        width, height, depth, header, order = read_basic_tiff_header(arg)
+
+        print '(%d x %d) @ %d + %d' % (width, height, depth, header)
