f9a6128377fd29bb6a5eef042ab1cd2b9d0c62d6
Graeme Winter <graeme.winter@gmail.com>
2011-07-27 08:26:06 +0000

    Implemented basic CBF class, realized want mini and full subclass, followed by reorg of pilatus classes

--- Toolkit/ImageFormat/Tests/TestFormat.py
+++ Toolkit/ImageFormat/Tests/TestFormat.py
@@ -16,6 +16,7 @@ if not os.environ['XIA2_ROOT'] in sys.path:
     sys.path.append(os.environ['XIA2_ROOT'])
 
 from Toolkit.ImageFormat.Format import Format
+from Toolkit.ImageFormat.FormatCBF import FormatCBF
 
 def workout(args):
     for arg in args:
@@ -23,6 +24,9 @@ def workout(args):
             print 'bzip2 %s' % arg
         if Format.is_gzip(arg):
             print 'gzip %s' % arg
+
+    if FormatCBF.understand(arg):
+        print FormatCBF.get_cbf_header(arg)
 
 if __name__ == '__main__':
     workout(sys.argv[1:])
