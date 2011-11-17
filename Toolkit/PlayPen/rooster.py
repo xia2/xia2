1dc152575fd3098f4e8d1391e557d2838ce31c4d
Graeme Winter <graeme.winter@gmail.com>
2011-11-17 09:47:17 +0000

    Roostering

---
+++ Toolkit/PlayPen/rooster.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import math
+
+def main():
+
+    # do while len(spot.xds) > 100 (say)
+    # index, no unit cell / symmetry set
+    # copy out indexed spots, orientation matrix
+    # done
+
+    # compare list of orientation matrices - how are they related?
+    # graphical representation?
