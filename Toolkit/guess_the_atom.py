1587e7202eab3652bf78dfe08114b071d8fbda13
Graeme Winter <graeme.winter@gmail.com>
2012-01-31 11:35:03 +0000

    Got the atom guessing working, based in anomalous signal at SAD wavelength, gives: Best guess atom: Zn  - awesome

---
+++ Toolkit/guess_the_atom.py
@@ -0,0 +1,50 @@
+import sys
+import math
+import os
+from iotbx import mtz
+from cctbx.eltbx import sasaki
+
+def bijvoet(hklin, nsites, mw):
+    '''Compute bijvoet differences between I+/I- and F+/F- averaged over
+    some resolution bins.'''
+
+    mtz_obj = mtz.object(hklin)
+    mi = mtz_obj.extract_miller_indices()
+
+    wavelengths = { }
+
+    atoms = ['Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Se', 'Br', 'S', 'P']
+
+    tables = [sasaki.table(atom) for atom in atoms]
+
+    for crystal in mtz_obj.crystals():
+        if crystal.name() == 'HKL_base':
+            continue
+        assert(len(crystal.datasets()) == 1)
+        for dataset in crystal.datasets():
+            wavelength = dataset.wavelength()
+
+    mas = mtz_obj.as_miller_arrays()
+
+    best_atom = None
+    best_diff = 100.0
+
+    for ma in mas:
+        columns = ma.info().label_string()
+        if 'I(+)' in columns and 'I(-)' in columns:
+            signal = ma.anomalous_signal()
+            for j, atom in enumerate(atoms):
+                fdp = tables[j].at_angstrom(wavelength).fdp()
+                p_signal = fdp * math.sqrt(nsites / mw)
+                if math.fabs(p_signal - signal) < best_diff:
+                    best_diff = math.fabs(p_signal - signal)
+                    best_atom = atom
+
+    print 'Best guess atom: %s' % best_atom
+                    
+if __name__ == '__main__':
+
+    if len(sys.argv) != 4:
+        raise RuntimeError, '%s hklin nsites molecular_weight' % sys.argv[0]
+
+    bijvoet(sys.argv[1], int(sys.argv[2]), float(sys.argv[3]))
