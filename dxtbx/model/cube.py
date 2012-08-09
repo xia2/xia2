535999bb6e3451ff5ada9fddaaee15572acee9bf
Graeme Winter <graeme.winter@gmail.com>
2012-08-09 09:49:01 +0000

    Moving the cube across to where it will be needed

---
+++ dxtbx/model/cube.py
@@ -0,0 +1,104 @@
+# cube.py
+#
+# a toy data cube implementation (start thereof)
+#
+# aims
+# - to be able to read in images (and unload images) silently
+# - to be able to pass a 3 dimensional flex array corresponding to a
+#   subset of frames, area of detector (shoebox)
+# - to be instantiated with a filename template
+# - to behave a little like a flex array
+
+class cube:
+
+    def __init__(self, template):
+        '''
+        Instantiate cube instance based on template - will use the template
+        to find matching files, keep a record of the files available. Template
+        should use # in place of frame numbers.
+        '''
+
+        import glob
+        self._template = template
+        self._filenames = glob.glob(template.replace('#', '?'))
+
+        if not self._filenames:
+            raise RuntimeError, 'no files found matching %s' % template
+
+        prefix = template.split('#')[0]
+        suffix = template.split('#')[-1]
+
+        assert(prefix + '#' * template.count('#') + suffix == template)
+
+        self._format = '%s%%0%dd%s' % (prefix, template.count('#'), suffix)
+
+        self._frames = [int(filename[len(prefix):-len(suffix)]) \
+                        for filename in self._filenames]
+
+        # this will contain a dictionary of flex arrays
+        self._loaded_frames = { }
+
+        # this will contain references to that dictionary - at the moment
+        # keep 25 frames in memory
+        from Queue import Queue
+        self._cache = Queue(maxsize = 25)
+
+        self._nfast = 0
+        self._nslow = 0
+
+        return
+
+    def _load(self, frame_number):
+
+        if frame_number in self._loaded_frames:
+            return
+
+        if self._cache.full():
+            del(self._loaded_frames[self._cache.get()])
+        from iotbx.detectors import ImageFactory
+        image = ImageFactory(self._format % frame_number)
+        image.read()
+
+        if self._nfast == self._nslow == 0:
+            self._nfast = image.parameters['SIZE1']
+            self._nslow = image.parameters['SIZE2']
+        else:
+            assert(self._nfast == image.parameters['SIZE1'])
+            assert(self._nslow == image.parameters['SIZE2'])
+
+        self._loaded_frames[frame_number] = image.get_raw_data()
+        self._cache.put(frame_number)
+
+        return
+
+    def get(self, frame0, frame1, slow0, slow1, fast0, fast1):
+        '''
+        Return a flex array containing double-precision values from the
+        region fast0 <= i < fast1, etc.
+        '''
+
+        # first validate input - only really useful after first frame loaded
+
+        for frame in range(frame0, frame1):
+            assert frame in self._frames
+
+        self._load(frame0)
+
+        assert(fast0 >= 0)
+        assert(fast1 <= self._nfast)
+        assert(slow0 >= 0)
+        assert(slow1 <= self._nslow)
+
+        from scitbx.array_family import flex
+        grid = flex.grid((frame1 - frame0, slow1 - slow0, fast1 - fast0))
+        shoebox = flex.double(grid)
+
+        for frame in range(frame0, frame1):
+            self._load(frame)
+            for s in range(slow0, slow1):
+                for f in range(fast0, fast1):
+                    shoebox[(frame - frame0, s - slow0, f - fast0)] = \
+                        self._loaded_frames[frame][(s, f)]
+
+        return shoebox
+
