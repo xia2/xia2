48bab209509a5ae5f84a40221ed7644d0bb5b745
Graeme Winter <graeme.winter@gmail.com>
2013-06-28 09:34:08 +0000

    Move playpen to it's own repo

--- Toolkit/PlayPen/HDR_join.py
+++
@@ -1,9 +0,0 @@
-import json
-import sys
-
-result = { }
-
-for arg in sys.argv[1:]:
-    result.update(json.load(open(arg, 'r')))
-
-print json.dumps(result)
--- Toolkit/PlayPen/test_sg.py
+++
@@ -1,6 +0,0 @@
-from cctbx import sgtbx
-for space_group_number in range(1, 231):
-  spacegroup = sgtbx.space_group_symbols(space_group_number).hall()
-  sg = sgtbx.space_group(spacegroup)
-  print '%3d %3d %3d %3d %3d' % (space_group_number, sg.f_inv(), sg.n_smx(),
-                                 sg.n_ltr(), len(sg.all_ops()))
