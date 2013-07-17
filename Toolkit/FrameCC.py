ab225ee28ae624340e2661bab834f5a0b4ac8bf9
Graeme Winter <graeme.winter@gmail.com>
2013-07-17 13:00:33 +0000

    Have tis workibg on subset now...

--- Toolkit/FrameCC.py
+++ Toolkit/FrameCC.py
@@ -1,41 +1,27 @@
 def read_image(image_name):
-    from scitbx.array_family import flex
-    from cbflib_adaptbx import uncompress, compress
-    import binascii
-
-    start_tag = binascii.unhexlify('0c1a04d5')
-
-    data = open(image_name, 'rb').read()
-    data_offset = data.find(start_tag) + 4
-    cbf_header = data[:data_offset - 4]
-
-    fast = 0
-    slow = 0
-    length = 0
-    
-    for record in cbf_header.split('\n'):
-        if 'X-Binary-Size-Fastest-Dimension' in record:
-            fast = int(record.split()[-1])
-        elif 'X-Binary-Size-Second-Dimension' in record:
-            slow = int(record.split()[-1])
-        elif 'X-Binary-Number-of-Elements' in record:
-            length = int(record.split()[-1])
-            
-    assert(length == fast * slow)
-
-    pixel_values = uncompress(packed = data[data_offset:],
-                              fast = fast, slow = slow)
-
-    return pixel_values
-
-def read_image(image_name):
     import dxtbx
-    return dxtbx.load(image_name).get_raw_data()
+    from scitbx.array_family import flex
+    data = dxtbx.load(image_name).get_raw_data()
+    data.reshape(flex.grid(2527, 2463))
+    subset = data[1055:1473,984:1478]
+    return subset
 
 def cc(a, b):
-    pass
+    _a = a - (sum(a) / len(a))
+    _b = b - (sum(b) / len(b))
+    import math
+    return sum(_a * _b) / math.sqrt(sum(_a * _a) * sum(_b * _b))
 
 if __name__ == '__main__':
     import sys
-    i = read_image(sys.argv[1])
-    help(i)
+    results = { }
+    for i, frame in enumerate(sys.argv[1:]):
+        a = read_image(frame).as_double()
+        for j, other in enumerate(sys.argv[i + 1:]):
+            b = read_image(other).as_double()
+            c = cc(a, b)
+            results[(i, j + i)] = c
+            results[(j + i, i)] = c
+            
+    for k in sorted(results):
+        print '%4d %4d %.6e %.6e' % (k[0], k[1], results[k], 1.0 - results[k])
