75508ac80ad87b790737809a4d5e63865f3fed24
Graeme Winter <graeme.winter@gmail.com>
2013-09-20 08:29:01 +0000

    Putative fix for mangling of data in P 3(1) 2 1 which was remerged in P 3(1) 2 if reference reflection file was provided; probably more generally safe...

--- Modules/Mtzdump.py
+++ Modules/Mtzdump.py
@@ -60,19 +60,12 @@ class Mtzdump():
                                         in mtz_obj.columns()]
         self._resolution_range = mtz_obj.max_min_resolution()
         spacegroup_and_no = mtz_obj.space_group().info().symbol_and_number()
-        
-        # replace tokens which are 1 with nothing - unless P1 (D'oh!)
-        spacegroup_tokens = spacegroup_and_no.split('(')[0].split()
 
-        spacegroup = ''
-        if len(spacegroup_tokens) == 2:
-            for st in spacegroup_tokens:
-                spacegroup += st
-                
-        else:
-            for st in spacegroup_tokens:
-                if st != '1':
-                    spacegroup += st
+        spacegroup_number = int(spacegroup_and_no.replace(')', '').split()[-1])
+
+        from Handlers.Syminfo import Syminfo
+
+        spacegroup = Syminfo.spacegroup_number_to_name(spacegroup_number)
 
         self._header['spacegroup'] = spacegroup
         self._reflections = mtz_obj.n_reflections()
