721a3066c7ebdde861fd6643c9003a69924fd2a0
Graeme Winter <graeme.winter@gmail.com>
2013-12-03 15:43:54 +0000

    Put xia2 at start of PATH

--- setup.csh
+++ setup.csh
@@ -1,6 +1,6 @@
 #!/bin/csh
 
 setenv XIA2CORE_ROOT ${XIA2_ROOT}/core
-setenv PATH ${PATH}:${XIA2_ROOT}/Applications
+setenv PATH ${XIA2_ROOT}/Applications:${PATH}
 
 setenv GFORTRAN_UNBUFFERED_PRECONNECTED 1 
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export XIA2CORE_ROOT=${XIA2_ROOT}/core
-export PATH=${PATH}:${XIA2_ROOT}/Applications
+export PATH=${XIA2_ROOT}/Applications:${PATH}
 export GFORTRAN_UNBUFFERED_PRECONNECTED=1
 
