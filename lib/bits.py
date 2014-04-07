59415908957a3df8881d635110bb982c5bf691d9
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-04-07 11:00:28 +0000

    return value with increment

--- lib/bits.py
+++ lib/bits.py
@@ -112,24 +112,24 @@ def remove_outliers(values, limit):
 from multiprocessing import Value, Lock
 
 class Counter(object):
-    def __init__(self, initval=0):
-        self.val = Value('i', initval)
-        self.lock = Lock()
+  def __init__(self, initval=0):
+    self.val = Value('i', initval)
+    self.lock = Lock()
 
-    def increment(self):
-        with self.lock:
-            self.val.value += 1
+  def increment(self):
+    with self.lock:
+      self.val.value += 1
+      return self.val.value
 
-    def value(self):
-        with self.lock:
-            return self.val.value
+  def value(self):
+    with self.lock:
+      return self.val.value
 
 _run_number = Counter(0)
 
 def _get_number():
   global _run_number
-  _run_number.increment()
-  return _run_number.value()
+  return _run_number.increment()
 
 ###### END MESSY CODE ######
 
