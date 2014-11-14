c54288dbab40a39a45b0bb26b002cf6bb30ed8c8
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-11-14 12:00:07 +0000

    add top level run_tests.py; will only work if xia2 is configured as a cctbx module

---
+++ run_tests.py
@@ -0,0 +1,16 @@
+from __future__ import division
+from libtbx import test_utils
+import libtbx.load_env
+
+tst_list = (
+    "$D/Test/Modules/Indexer/TstLabelitIndexer.py",
+    "$D/Test/Wrappers/Labelit/TstLabelitIndex.py",
+)
+
+def run () :
+  build_dir = libtbx.env.under_build("xia2")
+  dist_dir = libtbx.env.dist_path("xia2")
+  test_utils.run_tests(build_dir, dist_dir, tst_list)
+
+if (__name__ == "__main__"):
+  run()
