908af14c0a82971b0cc713f2be3c05a732bfdabc
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-11-26 10:35:56 +0000

    bug fix: also need to set the XIA2CORE_ROOT environment variable

--- command_line/xia2.new.py
+++ command_line/xia2.new.py
@@ -8,6 +8,7 @@ import libtbx.load_env
 xia2_root_dir = libtbx.env.find_in_repositories("xia2")
 sys.path.insert(0, xia2_root_dir)
 os.environ['XIA2_ROOT'] = xia2_root_dir
+os.environ['XIA2CORE_ROOT'] = os.path.join(xia2_root_dir, "core")
 
 from xia2.Applications import xia2
 
