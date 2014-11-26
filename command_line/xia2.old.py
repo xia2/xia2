617a229d0a0c38c9cc934fc93ebdc5f7af7a2e40
Richard Gildea <richard.gildea@diamond.ac.uk>
2014-11-26 17:47:19 +0000

    add xia2.old command: this is a thin cctbx wrapper around the conventional xia2 command

---
+++ command_line/xia2.old.py
@@ -0,0 +1,16 @@
+# LIBTBX_SET_DISPATCHER_NAME xia2.old
+
+import os
+import sys
+
+# Needed to make xia2 imports work correctly
+import libtbx.load_env
+xia2_root_dir = libtbx.env.find_in_repositories("xia2")
+sys.path.insert(0, xia2_root_dir)
+os.environ['XIA2_ROOT'] = xia2_root_dir
+os.environ['XIA2CORE_ROOT'] = os.path.join(xia2_root_dir, "core")
+
+from xia2.Applications import xia2
+
+if __name__ == '__main__':
+  xia2.run()
