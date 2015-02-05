a5f4b42ca550cec20816cc254cd4c533a51c45d9
Richard Gildea <richard.gildea@diamond.ac.uk>
2015-02-05 11:10:54 +0000

    bug fix: missing return statement (introduced by r4997)

--- Schema/__init__.py
+++ Schema/__init__.py
@@ -57,4 +57,5 @@ def load_imagesets(template, directory, id_image=None, image_range=None,
           image_range[0] - scan_image_range[0]:
           image_range[1] + 1 - scan_image_range[0]]]
         assert len(imagesets[0]) == image_range[1] - image_range[0] + 1, len(imagesets[0])
+        return imagesets
   return imageset_cache[full_template_path].values()
