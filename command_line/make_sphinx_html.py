65d5e481ec1c2bc6a8aa397249304138b5003e63
Richard Gildea <richard.gildea@diamond.ac.uk>
2015-02-06 14:08:07 +0000

    new command dev.xia2.make_sphinx_html

---
+++ command_line/make_sphinx_html.py
@@ -0,0 +1,18 @@
+# LIBTBX_SET_DISPATCHER_NAME dev.xia2.make_sphinx_html
+
+from __future__ import division
+from libtbx import easy_run
+import libtbx.load_env
+import os.path as op
+import shutil
+import os
+
+if (__name__ == "__main__") :
+  xia2_dir = libtbx.env.find_in_repositories("xia2")
+  assert (xia2_dir is not None)
+  dest_dir = op.join(xia2_dir, "html", "new")
+  if op.exists(dest_dir) :
+    shutil.rmtree(dest_dir)
+  os.chdir(op.join(xia2_dir, "doc", "sphinx"))
+  easy_run.call("make html")
+  shutil.move("build/html", dest_dir)
