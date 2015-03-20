027dd66a98a0b65e13f5d09a100ff2a2561dd5f6
Graeme Winter <graeme.winter@gmail.com>
2015-03-20 15:22:25 +0000

    mosflm is another background outlier algorithm method

--- Handlers/Phil.py
+++ Handlers/Phil.py
@@ -113,7 +113,7 @@ dials {
       .type = path
     profile_fitting = True
       .type = bool
-    background_outlier_algorithm = *null nsigma truncated normal tukey
+    background_outlier_algorithm = *null nsigma truncated normal tukey mosflm
       .type = choice
     use_threading = False
       .type = bool
