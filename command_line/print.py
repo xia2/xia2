efc4ea3eaf23eb46ba7591ab0d5694da408253e5
Richard Gildea <richard.gildea@diamond.ac.uk>
2015-03-20 16:39:40 +0000

    simple xia2.print command to test reading back xia2.json

---
+++ command_line/print.py
@@ -0,0 +1,37 @@
+
+import sys
+import os
+import math
+import time
+import exceptions
+import traceback
+
+# Needed to make xia2 imports work correctly
+import libtbx.load_env
+xia2_root_dir = libtbx.env.find_in_repositories("xia2")
+sys.path.insert(0, xia2_root_dir)
+os.environ['XIA2_ROOT'] = xia2_root_dir
+os.environ['XIA2CORE_ROOT'] = os.path.join(xia2_root_dir, "core")
+
+from Handlers.Streams import Chatter, Debug
+
+from Handlers.Files import cleanup
+from Handlers.Citations import Citations
+from Handlers.Environment import Environment, df
+
+from Applications.xia2 import get_command_line, write_citations, help
+
+
+# XML Marked up output for e-HTPX
+if not os.path.join(os.environ['XIA2_ROOT'], 'Interfaces') in sys.path:
+  sys.path.append(os.path.join(os.environ['XIA2_ROOT'], 'Interfaces'))
+
+def run():
+  assert os.path.exists('xia2.json')
+  from Schema.XProject import XProject
+  xinfo = XProject.from_json(filename='xia2.json')
+  Chatter.write(xinfo.get_output())
+  write_citations()
+
+if __name__ == '__main__':
+  run()
