0272725328cfa12e921aac97d0c1122585854a7a
Graeme Winter <graeme.winter@gmail.com>
2015-05-18 09:16:45 +0000

    Little command-line tool to make pretty pictures of abs corr surface

---
+++ command_line/aimless_absorption_map.py
@@ -0,0 +1,12 @@
+# LIBTBX_SET_DISPATCHER_NAME dev.xia2.aimless_absorption_map
+from __future__ import division
+
+def main(log, png):
+  from xia2.Toolkit.AimlessSurface import evaluate_1degree, scrape_coefficients
+  evaluate_1degree(scrape_coefficients(log), png)
+  return
+  
+
+if __name__ == '__main__':
+  import sys
+  main(sys.argv[1], sys.argv[2])
