8efc4c72af03554ffcae27d82a77d96c1bbd8d75
Graeme Winter <graeme.winter@gmail.com>
2015-06-16 06:58:10 +0000

    Do not want to use this reindex at the moment, breaks M/ISYM flags

--- Wrappers/CCP4/Reindex.py
+++ Wrappers/CCP4/Reindex.py
@@ -122,7 +122,7 @@ def Reindex(DriverType = None):
 
       Debug.write('Reindex... %s %s' % (self._spacegroup, self._operator))
 
-      if self._spacegroup and Flags.get_small_molecule():
+      if self._spacegroup and Flags.get_small_molecule() and False:
         if not self._operator or self._operator.replace(' ', '') == 'h,k,l':
           return self.cctbx_reindex()
 
