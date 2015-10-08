xia2/xia2

commit f1c1311e4f4648848aea606460d0db9bf7f2a44b

allow multiple reference geometries

Handlers/CommandLine.py
@@ -438,9 +438,11 @@ class _CommandLine(object):
       load_datablock(params.xia2.settings.input.json)
 
     if params.xia2.settings.input.reference_geometry is not None:
-      PhilIndex.update(
-        "xia2.settings.input.reference_geometry=%s" %os.path.abspath(
-          params.xia2.settings.input.reference_geometry))
+      reference_geometries = "\n".join(
+        ["xia2.settings.input.reference_geometry=%s" % os.path.abspath(g)
+          for g in params.xia2.settings.input.reference_geometry])
+      Debug.write(reference_geometries)
+      PhilIndex.update(reference_geometries)
       Debug.write("xia2.settings.trust_beam_centre=true")
       PhilIndex.update("xia2.settings.trust_beam_centre=true")
       params = PhilIndex.get_python_object()

Handlers/Phil.py
@@ -203,6 +203,7 @@ xia2.settings {
               "need to read all the image headers on start-up."
     reference_geometry = None
       .type = path
+      .multiple = True
       .help = "Experimental geometry from this datablock.json or "
               "experiments.json will override the geometry from the "
               "image headers."

Schema/__init__.py
@@ -81,25 +81,34 @@ def load_imagesets(template, directory, id_image=None, image_range=None,
 
 def update_with_reference_geometry(imagesets, reference_geometry):
   assert reference_geometry is not None
+  assert len(reference_geometry) >= 1
   from dxtbx.serialize import load
-  try:
-    experiments = load.experiment_list(
-      reference_geometry, check_format=False)
-    assert len(experiments.detectors()) == 1
-    assert len(experiments.beams()) == 1
-    reference_detector = experiments.detectors()[0]
-    reference_beam = experiments.beams()[0]
-  except Exception, e:
-    datablock = load.datablock(reference_geometry)
-    assert len(datablock) == 1
-    imageset = datablock[0].extract_imagesets()[0]
-    reference_detector = imageset.get_detector()
-    reference_beam = imageset.get_beam()
+
+  reference_components = []
+  for geometry in reference_geometry:
+    try:
+      experiments = load.experiment_list(geometry, check_format=False)
+      assert len(experiments.detectors()) == 1
+      assert len(experiments.beams()) == 1
+      reference_detector = experiments.detectors()[0]
+      reference_beam = experiments.beams()[0]
+    except Exception, e:
+      datablock = load.datablock(geometry)
+      assert len(datablock) == 1
+      imageset = datablock[0].extract_imagesets()[0]
+      reference_detector = imageset.get_detector()
+      reference_beam = imageset.get_beam()
+    reference_components.append({'detector': reference_detector, 'beam': reference_beam})
 
   for imageset in imagesets:
-    assert(reference_detector.is_similar_to(imageset.get_detector(),
-                                            fast_axis_tolerance=0.1,
-                                            slow_axis_tolerance=0.1,
-                                            origin_tolerance=10))
-    imageset.set_beam(reference_beam)
-    imageset.set_detector(reference_detector)
+    for reference_set in reference_components:
+      if reference_set['detector'].is_similar_to(imageset.get_detector(),
+                                          fast_axis_tolerance=0.1,
+                                          slow_axis_tolerance=0.1,
+                                          origin_tolerance=10):
+        break
+    else:
+      raise Exception("No appropriate reference geometry found")
+#   print "Appropriate set: ", reference_set
+    imageset.set_beam(reference_set['beam'])
+    imageset.set_detector(reference_set['detector'])

commit b2db39d24410d4ea811beb064eb390104dbf6fca

fix bug introduced by commit f1c1311e (adding .multiple to reference_geometry): this was setting trust_beam_centre=True in ALL cases

Handlers/CommandLine.py
@@ -437,7 +437,8 @@ class _CommandLine(object):
       assert os.path.isfile(params.xia2.settings.input.json)
       load_datablock(params.xia2.settings.input.json)
 
-    if params.xia2.settings.input.reference_geometry is not None:
+    reference_geometry = params.xia2.settings.input.reference_geometry
+    if reference_geometry is not None and len(reference_geometry) > 0:
       reference_geometries = "\n".join(
         ["xia2.settings.input.reference_geometry=%s" % os.path.abspath(g)
           for g in params.xia2.settings.input.reference_geometry])

Modules/Indexer/DialsIndexer.py
@@ -221,7 +221,8 @@ class DialsIndexer(Indexer):
     importer = self.Import()
     importer.set_image_range(self._indxr_images[0])
     importer.set_image_to_epoch(image_to_epoch)
-    if PhilIndex.params.xia2.settings.input.reference_geometry is not None:
+    reference_geometry = PhilIndex.params.xia2.settings.input.reference_geometry
+    if reference_geometry is not None and len(reference_geometry) > 0:
       if len(PhilIndex.params.xia2.settings.input.reference_geometry) > 1:
         from Schema import load_reference_geometries, find_relevant_reference_geometry
         geometry_file = find_relevant_reference_geometry(