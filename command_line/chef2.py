ab2e1047f600fb99da5eb818d42ce4fb4ff08b5e
rjgildea <richard.gildea@diamond.ac.uk>
2015-10-29 10:15:29 +0000

    add xia2.chef2 command

---
+++ command_line/chef2.py
@@ -0,0 +1,15 @@
+from __future__ import division
+import os
+import sys
+
+# Needed to make xia2 imports work correctly
+import libtbx.load_env
+xia2_root_dir = libtbx.env.find_in_repositories("xia2", optional=False)
+sys.path.insert(0, xia2_root_dir)
+os.environ['XIA2_ROOT'] = xia2_root_dir
+os.environ['XIA2CORE_ROOT'] = os.path.join(xia2_root_dir, "core")
+
+if __name__ == '__main__':
+
+  from Modules.PyChef2.PyChef import run
+  run(sys.argv[1:])
