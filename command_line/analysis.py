aa1785735d1e82cafd3db4defc6be98ffcaebd9c
rjgildea <richard.gildea@diamond.ac.uk>
2015-11-26 12:27:34 +0000

    embrace bootstrap for table styling

--- command_line/analysis.py
+++ command_line/analysis.py
@@ -78,6 +78,8 @@ def run(args):
 
   from xia2.lib.tabulate import tabulate
   merging_stats_table_html = tabulate(rows, headers, tablefmt='html')
+  merging_stats_table_html = merging_stats_table_html.replace(
+    '<table>', '<table class="table table-hover table-condensed">')
 
   unit_cell_params = intensities.unit_cell().parameters()
 
@@ -96,6 +98,8 @@ def run(args):
     ['CC1/2', '%.3f' %merging_stats.overall.cc_one_half],
   ]
   overall_stats_table_html = tabulate(rows, headers, tablefmt='html')
+  overall_stats_table_html = overall_stats_table_html.replace(
+    '<table>', '<table class="table table-hover table-condensed">')
 
   #headers = ['Crystal symmetry', '']
   #rows = [
@@ -423,53 +427,6 @@ body {
   margin-right: 5%%;
 }
 
-table {
-color: #333;
-font-family: Helvetica, Arial, sans-serif;
-/*width: 640px;*/
-border-collapse:
-collapse; border-spacing: 0;
-}
-
-td, th {
-padding-right: 10px;
-padding-left: 10px;
-border: 1px solid transparent; /* No more visible border */
-height: 30px;
-transition: all 0.3s;  /* Simple transition for hover effect */
-}
-
-th {
-background: #DFDFDF;  /* Darken header a bit */
-font-weight: bold;
-}
-
-td {
-background: #FAFAFA;
-/*text-align: center;*/
-}
-
-/* Cells in even rows (2,4,6...) are one color */
-tr:nth-child(even) td { background: #F1F1F1; }
-
-/* Cells in odd rows (1,3,5...) are another (excludes header cells)  */
-tr:nth-child(odd) td { background: #FEFEFE; }
-
-/*tr td:hover { background: #666; color: #FFF; } /* Hover cell effect! */
-
-
-.grid {
-  max-width: 1200px;
-}
-
-.grid-item {
-  float: left;
-  width: 600px;
-  height: 400px;
-  margin-bottom: 20px;
-}
-.grid-item--width2 { width: 1200px; }
-
 .plot {
   float: left;
   width: 600px;
@@ -495,7 +452,11 @@ tr:nth-child(odd) td { background: #FEFEFE; }
         </h4>
       </div>
       <div id="collapse1" class="panel-collapse collapse">
-        <div class="panel-body">%s</div>
+        <div class="panel-body">
+          <div class="table-responsive" style="width: 400px">
+            %s
+          </div>
+        </div>
         <!-- <div class="panel-footer"></div> -->
       </div>
     </div>
@@ -506,7 +467,11 @@ tr:nth-child(odd) td { background: #FEFEFE; }
         </h4>
       </div>
       <div id="collapse2" class="panel-collapse collapse">
-        <div class="panel-body">%s</div>
+        <div class="panel-body">
+          <div class="table-responsive">
+            %s
+          </div>
+        </div>
         <!-- <div class="panel-footer"></div> -->
       </div>
     </div>
