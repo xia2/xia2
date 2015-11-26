6542fdc70fcc8954f24ddf265c3f12c06243aeed
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2015-11-26 11:35:55 +0000

    Do not scale observations, show progress, fine histogram. Write results to json file instead of stdout.

--- command_line/overload.py
+++ command_line/overload.py
@@ -1,12 +1,17 @@
 from __future__ import division
+import binascii
+import json
+import sys
+import timeit
 
 def read_cbf_image(cbf_image):
   from cbflib_adaptbx import uncompress
-  import binascii
 
   start_tag = binascii.unhexlify('0c1a04d5')
 
-  data = open(cbf_image, 'rb').read()
+  with open(cbf_image, 'rb') as fh:
+    data = fh.read()
+
   data_offset = data.find(start_tag) + 4
   cbf_header = data[:data_offset - 4]
 
@@ -38,7 +43,6 @@ def get_overload(cbf_file):
         return int(record.split()[-2])
 
 def build_hist():
-  import sys
   from scitbx.array_family import flex
 
   if len(sys.argv) == 2 and sys.argv[1].endswith('.json'):
@@ -47,21 +51,45 @@ def build_hist():
     image_list = db.extract_imagesets()[0].paths()
   else:
     image_list = sys.argv[1:]
+  image_count = len(image_list)
 
-  thresh = 100
-  scale = thresh / get_overload(image_list[0])
+  # Faster, yet still less than ideal and wasting a lot of resources.
+  limit = get_overload(image_list[0])
+  binfactor = 5 # register up to 500% counts
+  histmax = (limit * binfactor) + 0.0
+  histbins = (limit * binfactor) + 1
+  hist = flex.histogram(flex.double(), data_min=0.0, data_max=histmax, n_slots=histbins)
 
-  hist = flex.histogram(flex.double(), data_min=0.0, data_max=(5.0*thresh),
-                        n_slots=500)
+  print "Processing %d images" % image_count
+  start = timeit.default_timer()
+  last_update = start
 
-  for image in image_list:
-    data = read_cbf_image(image)
-    scaled = scale * data.as_double()
-    tmp_hist = flex.histogram(scaled.as_1d(), data_min=0.0, data_max=(5.0*thresh),
-                              n_slots=500)
+#  image_maxima = [None] * image_count
+
+  for i in range(image_count):
+    data = read_cbf_image(image_list[i])
+    tmp_hist = flex.histogram(data.as_double().as_1d(), data_min=0.0, data_max=histmax, n_slots=histbins)
+#    image_max = histmax
+#    for b in reversed(tmp_hist.slots()):
+#      if b != 0:
+#        image_maxima[i] = int(image_max)
+#        break
+#      image_max -= 1
     hist.update(tmp_hist)
+    if timeit.default_timer() > (last_update + 3):
+      last_update = timeit.default_timer()
+      if sys.stdout.isatty():
+        sys.stdout.write('\033[A')
+      print 'Processed %d of %d images (%d seconds remain)    ' % (i+1, image_count, round((image_count - i) * (last_update - start) / (i+1)))
+#  print image_maxima
+  results = { 'scale_factor': 1 / limit,
+              'bin_count': histbins,
+              'bins': list(hist.slots()),
+              'image_files': image_list }
 
-  hist.show()
+  print "Writing results to overload.json"
+  with open('overload.json', 'w') as fh:
+    json.dump(results, fh)
 
 if __name__ == '__main__':
   build_hist()
