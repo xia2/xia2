f0f03e011cd5753947c13750c856416f96f44d3c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-01-27 17:09:39 +0000

    For now obtain a combined ESD by weighted averaging obtained ESDs from separate crystal objects

--- command_line/get_unit_cell_errors.py
+++ command_line/get_unit_cell_errors.py
@@ -1,10 +1,9 @@
 import sys
 import os
 import math
-import time
 import exceptions
 import traceback
-
+import re
 # Needed to make xia2 imports work correctly
 import libtbx.load_env
 xia2_root_dir = libtbx.env.find_in_repositories("xia2", optional=False)
@@ -16,10 +15,8 @@ from Handlers.Streams import Chatter, Debug
 
 from Handlers.Files import cleanup
 from Handlers.Citations import Citations
-from Handlers.Environment import Environment, df
+from Handlers.Environment import Environment
 from lib.bits import auto_logfiler
-
-from XIA2Version import Version
 
 # XML Marked up output for e-HTPX
 if not os.path.join(os.environ['XIA2_ROOT'], 'Interfaces') in sys.path:
@@ -28,9 +25,6 @@ if not os.path.join(os.environ['XIA2_ROOT'], 'Interfaces') in sys.path:
 from Applications.xia2setup import write_xinfo
 from Applications.xia2 import check, check_cctbx_version, check_environment
 from Applications.xia2 import get_command_line, write_citations, help
-from xia2.lib.tabulate import tabulate
-
-from scitbx.array_family import flex
 
 
 def get_unit_cell_errors(stop_after=None):
@@ -43,6 +37,7 @@ def get_unit_cell_errors(stop_after=None):
   from Wrappers.Dials.CombineExperiments import CombineExperiments
   from Wrappers.Dials.Reindex import Reindex
   from lib.bits import auto_logfiler
+  Citations.cite('dials')
 
   crystals = xinfo.get_crystals()
   for crystal_id, crystal in crystals.iteritems():
@@ -57,7 +52,6 @@ def get_unit_cell_errors(stop_after=None):
     epoch_to_integrated_intensities = {}
     epoch_to_sweep_name = {}
 
-    #epoch_to_si = {}
     # Aimless only
     epochs = scaler._sweep_handler.get_epochs()
 
@@ -68,38 +62,23 @@ def get_unit_cell_errors(stop_after=None):
 
     dials_combine.set_experimental_model(
       same_beam=True,
-      same_crystal=True,
+#     same_crystal=True,
+      same_crystal=False,
       same_detector=False,
       same_goniometer=False)
 
+    # Reindex each sweep to same setting
+    Chatter.write('Reindexing experiments')
+
     for epoch in epochs:
         si = scaler._sweep_handler.get_sweep_information(epoch)
-#        epoch_to_batches[epoch] = si.get_batches()
-#        epoch_to_integrated_intensities[epoch] = si.get_reflections()
-#        epoch_to_sweep_name[epoch] = si.get_sweep_name()
-
-# check CCP4ScalerA.py
-#~line 347 to get the integrater
         intgr = si.get_integrater()
-#        hklin = si.get_reflections()
         refiner = intgr.get_integrater_refiner()
 #        print intgr
-#        print hklin
 #        print refiner
 
-# go to DialsIntegrator.py
-# ~234
-# experiments filename, index filename
-# (poss DialsIndexer)
-#        _intgr_experiments_filename = refiner.get_refiner_payload("experiments.json")
-#        print _intgr_experiments_filename
         if reference_vectors is None:
           reference_vectors = refiner.get_refiner_payload("experiments.json")
-
-#        ## copy the data across
-#        from dxtbx.serialize import load
-#        experiments = load.experiment_list(_intgr_experiments_filename)
-#        experiment = experiments[0]
         _reflections_filename = refiner.get_refiner_payload("reflections.pickle")
         print _reflections_filename
 
@@ -113,16 +92,11 @@ def get_unit_cell_errors(stop_after=None):
 
         dials_combine.add_experiments(refiner.get_refiner_payload("experiments.json"))
         dials_combine.add_reflections(dials_reindex.get_reindexed_reflections_filename())
-#       Citations.cite('blend')
 
-
-# TstDialsRefiner.py
-# ..?
-# ~72
-
-# add multiple exps together
-#  
+    # joint refinement of n sweeps, but with separate A matrices,
+    # to obtain n unit cell uncertainties
     auto_logfiler(dials_combine)
+    Chatter.write("Combining experiments")
     dials_combine.run()
 
     from Wrappers.Dials.Refine import Refine
@@ -131,17 +105,62 @@ def get_unit_cell_errors(stop_after=None):
     dials_refine.set_indexed_filename(dials_combine.get_combined_reflections_filename())
     dials_refine.set_use_all_reflections(True)
     auto_logfiler(dials_refine)
+    Chatter.write("Running joint refinement")
     dials_refine.run()
 
     Chatter.write("")
+    match_esd_header = re.compile('Crystal ([0-9]+) refined cell parameters and estimated standard deviations')
+    match_esd = re.compile(' (a|b|c|alpha|beta|gamma): +([0-9]+\.[0-9]+) [^0-9]+([0-9]+\.[0-9]+)\)')
+
+    match_nref_header = re.compile('RMSDs by experiment:')
+    match_nref = re.compile('\| ([0-9]+) *\| ([0-9]+) *\|')
+
+    esd = {}
     with open('dials.refine.debug.log') as reflog:
       dials_log = reflog.readlines()
-      model_start = dials_log.index("Final refined crystal model:\n")
-      if model_start > 0:
-        Chatter.write("".join(dials_log[model_start:model_start+13]))
-      errors_start = dials_log.index("Refined cell parameters and estimated standard deviations:\n")
-      if errors_start > 0:
-        Chatter.write("".join(dials_log[errors_start:errors_start+7]))
+      for n in range(len(dials_log)):
+        result = match_esd_header.match(dials_log[n])
+        if result:
+          crystal_id = result.group(1)
+          Chatter.write("ESD %s" % crystal_id)
+          esd[crystal_id] = {}
+          for i in range(6):
+            result = match_esd.match(dials_log[n + i + 1])
+            if result:
+              Chatter.write("%s: %s (+/- %s)" % (result.group(1, 2, 3)))
+              esd[crystal_id][result.group(1)] = (float(result.group(2)), float(result.group(3)))
+          Chatter.write("")
+
+        if match_nref_header.match(dials_log[n]):
+          i = 5
+          result = match_nref.match(dials_log[n + i])
+          nref_total = 0
+          while result:
+            Chatter.write("Found %s reflections for experiment %s" % (result.group(2,1)))
+#            esd[result.group(1)]['nref'] = int(result.group(2))
+            nref_total = nref_total + int(result.group(2))
+            i = i + 1
+            result = match_nref.match(dials_log[n + i])
+          Chatter.write("Total number of reflections: %d" % nref_total)
+          Chatter.write("")
+
+    Chatter.write("Weighted estimated standard deviations:")
+    for value in ['a', 'b', 'c', 'alpha', 'beta', 'gamma']:
+      [x, sd] = zip(*[e[value] for e in esd.itervalues()])
+      if any([v == 0 for v in sd]):
+        # If any values with 0 variance present, average all values with 0 variance
+        est_value = sum([i for i, v in zip(x, sd) if v == 0])
+        est_value = est_value / sum([v == 0 for v in sd])
+        est_sigma = 0
+      else:
+        weights = [ 1 / (v * v) for v in sd ]
+        est_value = sum([ i * w for i, w in zip(x, weights) ])
+        est_value = est_value / sum(weights)
+        est_sigma = math.sqrt(1 / sum(weights))
+
+      print "%8s: %10.6f (+/- %f)" % (value, est_value, est_sigma)
+
+#   Chatter.write(str(esd))
   return
 
 def run():
