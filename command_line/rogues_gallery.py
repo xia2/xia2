b99de708a2cdc29b040b9a350ba3c47836e2c474
graeme-winter <graeme.winter@gmail.com>
2016-03-02 16:10:39 +0000

    Now have code which will find nearest matching reflection using annlib; seems to work; get the bb; now need to work out how to get the pixel data out

--- command_line/rogues_gallery.py
+++ command_line/rogues_gallery.py
@@ -21,11 +21,10 @@ def munch_rogues(rogues):
     tokens = record.split()
     if not tokens[-1] == '*':
       continue
-    x = float(tokens[15])
-    y = float(tokens[16])
+    x, y, z = map(float, tokens[15:18])
     b = int(tokens[6])
     h, k, l = map(int, tokens[3:6]) # don't forget these are probably reindexed
-    rogue_reflections.append((b, x, y, h, k, l))
+    rogue_reflections.append((b, x, y, z, h, k, l))
 
   return rogue_reflections
 
@@ -61,6 +60,49 @@ def reconstruct_rogues():
       intgr.get_integrated_experiments())
     reflections = pickle.load(open(intgr.get_integrated_reflections()))
     batched_reflections[si.get_batch_range()] = (experiments, reflections)
+
+  # - look up reflection in reflection list, get bounding box
+  # - pull pixels given from image set, flatten these, write out
+
+  from dials.array_family import flex
+  from annlib_ext import AnnAdaptor as ann_adaptor
+
+  reflections_run = { }
+  for run in batched_reflections:
+    reflections_run[run] = []
+
+  for rogue in rogue_reflections:
+    b = rogue[0]
+    for run in batched_reflections:
+      if b >= run[0] and b <= run[1]:
+        reflections_run[run].append(rogue)
+        break
+
+  for run in reflections_run:
+    experiment = batched_reflections[run][0]
+    reflections = batched_reflections[run][1]
+    rogues = reflections_run[run]
+    reference = flex.double()
+    scan = experiment.scans()[0]
+    images = experiment.imagesets()[0]
+    for xyz in reflections['xyzcal.px']:
+      reference.append(xyz[0])
+      reference.append(xyz[1])
+      reference.append(xyz[2])
+
+    search = flex.double()
+    for rogue in rogues:
+      search.append(rogue[1])
+      search.append(rogue[2])
+      search.append(scan.get_array_index_from_angle(rogue[3]))
+
+    ann = ann_adaptor(data=reference, dim=3, k=1)
+    ann.query(search)
+
+    for j, rogue in enumerate(rogues):
+      nn = reflections[ann.nn[j]]
+      print nn['bbox']
+      # FIXME now extract the pixel information
 
 if __name__ == '__main__':
   reconstruct_rogues()
