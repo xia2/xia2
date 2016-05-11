3bf0dab98495fd9340940ae3099902b1f99dffd4
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-05-11 12:26:30 +0100

    curl => urllib2

--- command_line/bugreport.py
+++ command_line/bugreport.py
@@ -1,17 +1,23 @@
 import base64
-from dials.util.procrunner import run_process
 import json
+import urllib2
 import xia2.XIA2Version
 
-token = base64.b64decode('eGlhMnN1cHBvcnQ6MWY5MzI4NWRhMDBhMGM3MDZkZjVkY2FiNWU0ZTY4YWNhMmJmYjIxNQ==')
+token = 'eGlhMnN1cHBvcnQ6MWY5MzI4NWRhMDBhMGM3MDZkZjVkY2FiNWU0ZTY4YWNhMmJmYjIxNQ=='
+
 report = { 'public': 'false', 'files': {} }
 report['description'] = '%s automated bug report' % xia2.XIA2Version.Version
 for filename in ['xia2.error', 'xia2-debug.txt', 'xia2.txt']:
   with open(filename, 'r') as fh:
     report['files'][filename] = { 'content': fh.read() }
 
-result = run_process(['curl', '-u', token, 'https://api.github.com/gists', '-d', '@-'], stdin = json.dumps(report))
-if result['exitcode'] == 0:
+post_data = json.dumps(report)
+url_request = urllib2.Request('https://api.github.com/gists', post_data)
+url_request.add_header("Authorization", "Basic %s" % token)
+
+socket = urllib2.urlopen(url_request)
+if socket.getcode() == 201:
   print "Bug report submitted"
 else:
-  print "Could not submit bug report"
+  print socket.info()
+  print "\nCould not submit bug report"
