db93d37bd64d6c9f8f538bb09143fb56a89fa50c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2016-06-29 15:50:57 +0100

    Add all citations to CIF

--- Interfaces/CIF/CIFHandler.py
+++ Interfaces/CIF/CIFHandler.py
@@ -11,28 +11,27 @@ from __future__ import division
 import datetime
 import iotbx.cif.model
 import xia2.XIA2Version
+import xia2.Handlers.Citations
 
 class _CIFHandler(object):
   def __init__(self):
     self._cif = iotbx.cif.model.cif()
     self._block = iotbx.cif.model.block()
     self._cif['xia2'] = self._block
-    self.collate_audit_information(self._block)
 
   def add_xcrystal(self, xcrystal):
 #   print "CIF: Adding crystal", xcrystal
     pass
 
   def write_cif(self):
+    self.collate_audit_information(self._block)
     with open('xia2.cif', 'w') as fh:
       self._cif.show(out=fh)
 
   def collate_audit_information(self, block):
     block["_audit_creation_method"] = xia2.XIA2Version.Version
     block["_audit_creation_date"] = datetime.date.today().isoformat()
-    block["_publ_section_references"] = '''
-Winter, G. (2010) Journal of Applied Crystallography 43
-'''
+    block["_publ_section_references"] = '\n'.join(xia2.Handlers.Citations.Citations.get_citations_acta())
 
 CIFHandler = _CIFHandler()
 
