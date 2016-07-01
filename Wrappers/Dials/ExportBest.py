224ce1c802e0d6972680208384c379e1d51942c5
rjgildea <richard.gildea@diamond.ac.uk>
2016-07-01 18:09:54 +0100

    wrapper for dials.export format=best

---
+++ Wrappers/Dials/ExportBest.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# ExportBest.py
+#
+#   Copyright (C) 2016 Diamond Light Source, Richard Gildea
+#
+#   This code is distributed under the BSD license, a copy of which is
+#   included in the root directory of this package.
+#
+# Export DIALS integration output in MTZ format.
+
+from __future__ import division
+
+from xia2.Handlers.Flags import Flags
+
+def ExportBest(DriverType = None):
+  '''A factory for ExportMtzWrapper classes.'''
+
+  from xia2.Driver.DriverFactory import DriverFactory
+  DriverInstance = DriverFactory.Driver(DriverType)
+
+  class ExportBestWrapper(DriverInstance.__class__):
+
+    def __init__(self):
+      DriverInstance.__class__.__init__(self)
+      self.set_executable('dials.export')
+
+      self._experiments_filename = None
+      self._reflections_filename = None
+      return
+
+    def set_experiments_filename(self, experiments_filename):
+      self._experiments_filename = experiments_filename
+      return
+
+    def get_experiments_filename(self):
+      return self._experiments_filename
+
+    def set_reflections_filename(self, reflections_filename):
+      self._reflections_filename = reflections_filename
+      return
+
+    def get_reflections_filename(self):
+      return self._reflections_filename
+
+    def run(self):
+      from xia2.Handlers.Streams import Debug
+      Debug.write('Running dials.export')
+
+      self.clear_command_line()
+      self.add_command_line('experiments=%s' % self._experiments_filename)
+      self.add_command_line('reflections=%s' % self._reflections_filename)
+      self.add_command_line('format=best')
+      self.start()
+      self.close_wait()
+      self.check_for_errors()
+
+      return
+
+  return ExportBestWrapper()
+
