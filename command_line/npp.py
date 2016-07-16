e5d45caad1efd538ac3c8849a30495ba8dbf214e
graeme-winter <graeme.winter@gmail.com>
2016-07-16 22:23:57 +0100

    Output hkl, i, v, I/sigI, gradient of NPP, gradient of same +/- 2 expected sigmas on percentiles of distribution

--- command_line/npp.py
+++ command_line/npp.py
@@ -1,25 +1,39 @@
 def npp(hklin):
   from iotbx.reflection_file_reader import any_reflection_file
   from xia2.Toolkit.NPP import npp_ify, mean_variance
+  from scitbx.array_family import flex
+  import math
   reader = any_reflection_file(hklin)
   mtz_object = reader.file_content()
   intensities = [ma for ma in reader.as_miller_arrays(merge_equivalents=False)
                  if ma.info().labels == ['I', 'SIGI']][0]
   indices = intensities.indices()
 
+  # merging: use external variance i.e. variances derived from SIGI column
   merger = intensities.merge_equivalents(use_internal_variance=False)
   mult = merger.redundancies().data()
   imean = merger.array()
   unique = imean.indices()
   iobs = imean.data()
+  # scale up variance to account for sqrt(multiplicity) effective scaling
   variobs = (imean.sigmas() ** 2) * mult.as_double()
 
   for hkl, i, v in zip(unique, iobs, variobs):
     sel = indices == hkl
     data = intensities.select(sel).data()
     _x, _y = npp_ify(data, input_mean_variance=(i,v))
-    for x, y in zip(_x, _y):
-      print x, y
+
+    # perform linreg on (i) all data and (ii) subset between +/- 2 sigma
+
+    sel = (flex.abs(_x) < 2)
+    _x_ = _x.select(sel)
+    _y_ = _y.select(sel)
+
+    fit_all = flex.linear_regression(_x, _y)
+    fit_cen = flex.linear_regression(_x_, _y_)
+
+    print '%3d %3d %3d' % hkl, '%.2f %.2f %.2f' % (i, v, i/math.sqrt(v)), \
+      '%.2f %.2f' % (fit_all.slope(), fit_cen.slope())
 
 if __name__ == '__main__':
   import sys
