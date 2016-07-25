d96b859769b3cca1bd7700efebb50a9a6015fad3
graeme-winter <graeme.winter@gmail.com>
2016-07-25 16:28:57 +0100

    If profile_fitting false use only summation integration; fixes #43

--- Modules/Scaler/CCP4ScalerA.py
+++ Modules/Scaler/CCP4ScalerA.py
@@ -781,7 +781,12 @@ class CCP4ScalerA(Scaler):
 
     sc = self._updated_aimless()
     sc.set_hklin(self._prepared_reflections)
-    sc.set_intensities(PhilIndex.params.ccp4.aimless.intensities)
+
+    # if profile fitting off use summation intensities
+    if PhilIndex.params.xia2.settings.integration.profile_fitting:
+      sc.set_intensities(PhilIndex.params.ccp4.aimless.intensities)
+    else:
+      sc.set_intensities('summation')
 
     sc.set_chef_unmerged(True)
 
