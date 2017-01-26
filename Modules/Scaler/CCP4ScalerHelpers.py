b27bc67522cb5b086b648309f29c38dbf5a77015
rjgildea <richard.gildea@diamond.ac.uk>
2017-01-26 16:51:48 +0000

    putative bug fix for reseting refiners when combined with multi-sweep indexing

--- Modules/Scaler/CCP4ScalerHelpers.py
+++ Modules/Scaler/CCP4ScalerHelpers.py
@@ -373,10 +373,9 @@ class CCP4ScalerHelper(object):
       Debug.write(
           'No solution found: assuming lattice from refiner')
 
-    # now pass the correct lattice conclusion across the whole set
-    for j, refiner in enumerate(refiners[1:]):
-      Debug.write('Asserting lattice %s for %d' % (correct_lattice, j+1))
-      refiner.set_refiner_asserted_lattice(correct_lattice)
+    if need_to_return:
+      for refiner in refiners[1:]:
+        refiner.refiner_reset()
 
     if rerun_pointless:
       pointless.set_correct_lattice(correct_lattice)
