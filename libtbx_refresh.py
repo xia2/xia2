fb851b7e89b039944a7d6c51662ba7a251b759d2
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-10-22 00:19:53 +0100

    add library dependencies

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -1,5 +1,9 @@
-from __future__ import absolute_import, division
+from __future__ import absolute_import, division, print_function
 
 # implicitly update the .gitversion file
 from xia2.XIA2Version import Version
-print Version
+print(Version)
+
+import libtbx.pkg_utils
+libtbx.pkg_utils.require('mock', '>=2.0')
+libtbx.pkg_utils.require('pytest', '>=2')
