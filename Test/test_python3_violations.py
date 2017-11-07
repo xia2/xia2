847574d34e03f769c2ff753fa93a0d1df2a8a2d0
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2017-11-07 15:22:34 +0000

    Enable Jenkins Python3 monster for xia2

---
+++ Test/test_python3_violations.py
@@ -0,0 +1,9 @@
+def test_find_python3_violations():
+  import xia2
+  import pytest
+  import dials.test.python3_regression as py3test
+  result = py3test.find_new_python3_incompatible_code(xia2)
+  if result is None:
+    pytest.skip('No python3 interpreter available')
+  elif result:
+    pytest.fail(result)
