6b2d15f2eb41fbef9e4ac64c4693a4c1d9a41f0a
graeme-winter <graeme.winter@gmail.com>
2018-06-08 08:21:18 +0100

    Painful, much boiler plate

--- command_line/table1.py
+++ command_line/table1.py
@@ -1,12 +1,18 @@
 from __future__ import division, print_function
 
-def table1_tex(merging_stats):
+def table1_tex(crystal_params, merging_stats):
   # based on table1 from
   #
   # http://journals.iucr.org/d/issues/2018/02/00/di5011/index.html
+
+  assert len(crystal_params) == len(merging_stats)
   
   # first iterate through and work out how many columns we will be needing
   columns = [len(ms) for ms in merging_stats]
+
+  if max(columns) > 1:
+    raise RuntimeError(':TODO: make this work for multiwavelength data sets')
+  
   ncols = sum(columns)
 
   print('\\begin{tabular}{%s}' % ('l' * (ncols + 1)))
@@ -19,8 +25,25 @@ def table1_tex(merging_stats):
 
   print(' & '.join(name_str) + ' \\\\')
   print('Crystal parameters' + ' & ' * ncols + '\\\\')
+  print('Space group & ' + 
+          ' & '.join([cp['space_group'] for cp in crystal_params]) + ' \\\\')
+  print('Unit-cell parameters (\\AA) & ' + ' & '.join(
+          ['$a=%.5f b=%.5f c=%.5f \\alpha=%.5f \\beta=%.5f \\gamma=%.5f$'
+             % tuple(cp['cell']) for cp in crystal_params]) + ' \\\\')
+  print('Data statistics' + ' & ' * ncols + '\\\\')
+
+  # resolution ranges, shells
+
+  resolution_str = ['Resolution range (\\AA)']
   
-  print('\\end{tabular}')
+  for ms in merging_stats:
+    for name in ms:
+      low = ms[name]['Low resolution limit']
+      high = ms[name]['High resolution limit']
+      resolution_str.append('%.2f-%.2f (%.2f-%.2f)' %
+                              (low[0], high[0], low[2], high[2]))
+  
+  print(' & '.join(resolution_str) + ' \\\\')
   
 
 def table1():
@@ -39,12 +62,21 @@ def table1():
   # the image headers :TODO:
 
   merging_stats = []
+  crystal_params = []
     
   for j in jsons:
     for x in j['_crystals']:
-      merging_stats.append(j['_crystals'][x]['_scaler']['_scalr_statistics'])
+      s = j['_crystals'][x]['_scaler']
+      crystal_param = {
+        'space_group':s['_scalr_likely_spacegroups'][0],
+        'cell':s['_scalr_cell'],
+        'cell_esd':s['_scalr_cell_esd']
+        }
+          
+      merging_stats.append(s['_scalr_statistics'])
+      crystal_params.append(crystal_param)
 
-  table1_tex(merging_stats)
+  table1_tex(crystal_params, merging_stats)
       
 if __name__ == '__main__':
   table1()
