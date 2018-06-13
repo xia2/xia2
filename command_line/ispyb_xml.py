70e2f7d59c1c71ea4d76a67123f6da44cdf55cc7
rjgildea <richard.gildea@diamond.ac.uk>
2018-06-13 10:29:56 +0100

    xia2.ispyb_xml: include pipeline name in processingPrograms field

--- command_line/ispyb_xml.py
+++ command_line/ispyb_xml.py
@@ -1,23 +1,27 @@
 from __future__ import absolute_import, division, print_function
 
+import os
 import libtbx.load_env
+import iotbx.phil
+from xia2.Interfaces.ISPyB.ISPyBXmlHandler import ISPyBXmlHandler
+from xia2.Schema.XProject import XProject
 
 def ispyb_xml(xml_out):
-  from xia2.Interfaces.ISPyB.ISPyBXmlHandler import ISPyBXmlHandler
-  import os
   assert os.path.exists('xia2.json')
   assert os.path.exists('xia2.txt')
+  assert os.path.exists('xia2-working.phil')
   command_line = ''
   for record in open('xia2.txt'):
     if record.startswith('Command line:'):
       command_line = record.replace('Command line:', '').strip()
-  from xia2.Schema.XProject import XProject
+  with open('xia2-working.phil', 'rb') as f:
+    working_phil = iotbx.phil.parse(f.read())
   xinfo = XProject.from_json(filename='xia2.json')
   crystals = xinfo.get_crystals()
   assert len(crystals) == 1
   crystal = next(crystals.itervalues())
   ISPyBXmlHandler.add_xcrystal(crystal)
-  ISPyBXmlHandler.write_xml(xml_out, command_line)
+  ISPyBXmlHandler.write_xml(xml_out, command_line, working_phil=working_phil)
 
 if __name__ == '__main__':
   import sys
