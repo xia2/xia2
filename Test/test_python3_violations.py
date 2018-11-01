271d03a50bea4079c0013875013403fc696bd988
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-11-01 09:59:58 +0000

    import code from libtbx instead

--- Test/test_python3_violations.py
+++ Test/test_python3_violations.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 def test_find_python3_violations():
   import xia2
   import pytest
-  import dials.test.python3_regression as py3test
+  import libtbx.test_utils.python3_regression as py3test
   result = py3test.find_new_python3_incompatible_code(xia2)
   if result is None:
     pytest.skip('No python3 interpreter available')
