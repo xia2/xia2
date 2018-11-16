e10bb8d86867c4ab8815903a816ffe1d2694bc46
James Beilsten-Edmands <jbeilstenedmands@gmail.com>
2018-11-16 15:58:48 +0000

    Add regression test for dials-full pipeline with small-molecule data

--- Test/regression/test_small_molecule.py
+++ Test/regression/test_small_molecule.py
@@ -25,6 +25,20 @@ def test_dials(regression_data, run_in_tmpdir, ccp4):
   )
   assert success, issues
 
+@pytest.mark.regression
+def test_dials_full(regression_data, run_in_tmpdir, ccp4):
+    command_line = [
+        'xia2', 'pipeline=dials-full', 'nproc=2',
+        'small_molecule=True', 'read_all_image_headers=False', 'trust_beam_centre=True',
+        regression_data('small_molecule_example').strpath,
+    ]
+    result = procrunner.run(command_line)
+    success, issues = xia2.Test.regression.check_result(
+        'small_molecule.dials_full', result, run_in_tmpdir, ccp4, expected_data_files=[
+    'AUTOMATIC_DEFAULT_scaled.mtz', 'AUTOMATIC_DEFAULT_scaled_unmerged.mtz']
+    )
+    assert success, issues
+
 
 @pytest.mark.regression
 def test_xds(regression_data, run_in_tmpdir, ccp4, xds):
