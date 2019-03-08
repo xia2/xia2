1fda8f7f1087ba328c3a5f54b75c26436317d4d5
rjgildea <richard.gildea@diamond.ac.uk>
2019-03-08 17:08:11 +0000

    use run_in_tmpdir fixture

--- Test/regression/test_multiplex.py
+++ Test/regression/test_multiplex.py
@@ -9,25 +9,25 @@ expected_data_files = [
 ]
 
 
-def test_proteinase_k(regression_test, ccp4, dials_data, tmpdir):
+def test_proteinase_k(regression_test, ccp4, dials_data, run_in_tmpdir):
     data_dir = dials_data("multi_crystal_proteinase_k")
     expts = sorted(f.strpath for f in data_dir.listdir("experiments*.json"))
     refls = sorted(f.strpath for f in data_dir.listdir("reflections*.pickle"))
     command_line = ["xia2.multiplex"] + expts + refls
     print(" ".join(command_line))
-    result = procrunner.run(command_line, working_directory=tmpdir.strpath)
+    result = procrunner.run(command_line)
     assert not result["exitcode"]
     for f in expected_data_files:
-        assert tmpdir.join(f).check(file=1), "expected file %s missing" % f
+        assert run_in_tmpdir.join(f).check(file=1), "expected file %s missing" % f
 
 
-def test_proteinase_k_dose(regression_test, ccp4, dials_data, tmpdir):
+def test_proteinase_k_dose(regression_test, ccp4, dials_data, run_in_tmpdir):
     data_dir = dials_data("multi_crystal_proteinase_k")
     expts = sorted(f.strpath for f in data_dir.listdir("experiments*.json"))
     refls = sorted(f.strpath for f in data_dir.listdir("reflections*.pickle"))
     command_line = ["xia2.multiplex", "dose=1,20"] + expts + refls
     print(" ".join(command_line))
-    result = procrunner.run(command_line, working_directory=tmpdir.strpath)
+    result = procrunner.run(command_line)
     assert not result["exitcode"]
     for f in expected_data_files:
-        assert tmpdir.join(f).check(file=1), "expected file %s missing" % f
+        assert run_in_tmpdir.join(f).check(file=1), "expected file %s missing" % f
