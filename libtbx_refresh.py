d68eb28e20ed0670b4e7f9c0c5227dba95b3b901
Nicholas Devenish <ndevenish@gmail.com>
2019-03-12 16:53:46 +0000

    Move python dependencies to libtbx_config

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -1,13 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
-import libtbx.pkg_utils
 from xia2.XIA2Version import Version
 
 # the import implicitly updates the .gitversion file
 print(Version)
-
-libtbx.pkg_utils.require("mock", ">=2.0")
-libtbx.pkg_utils.require("pytest", ">=3.1")
-libtbx.pkg_utils.require("Jinja2")
-libtbx.pkg_utils.require("procrunner")
-libtbx.pkg_utils.require("tabulate")
