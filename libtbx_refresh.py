5f4e5bc834f7bb12c5859bde0647be3c49d96296
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-05-24 22:01:13 +0100

    Enable precommitbx nagging
    cf dials/dials#786

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, division, print_function
 
+import dials.precommitbx.nagger
 from xia2.XIA2Version import Version
+
+dials.precommitbx.nagger.nag()
 
 # the import implicitly updates the .gitversion file
 print(Version)
