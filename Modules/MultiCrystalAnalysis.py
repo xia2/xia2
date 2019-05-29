cb0155af49e70defc47b2dd58d613d2076cbe123
rjgildea <richard.gildea@diamond.ac.uk>
2019-05-29 11:40:02 +0100

    move plotting code to separate file; add test

--- Modules/MultiCrystalAnalysis.py
+++ Modules/MultiCrystalAnalysis.py
@@ -18,12 +18,6 @@ from libtbx import phil
 logger = logging.getLogger(__name__)
 
 
-def flex_double_as_string(flex_array, n_digits=None):
-    if n_digits is not None:
-        flex_array = flex_array.round(n_digits)
-    return list(flex_array.as_string())
-
-
 class multi_crystal_analysis(xia2_report_base):
     def __init__(self, params, experiments=None, reflections=None, data_manager=None):
         super(multi_crystal_analysis, self).__init__(params)
@@ -165,7 +159,9 @@ class multi_crystal_analysis(xia2_report_base):
         # panel_distances = panel_distances_from_experiments(experiments)
 
         d = OrderedDict()
-        d.update(self._plot_uc_histograms(uc_params))
+        from xia2.Modules.MultiCrystal.plots import plot_uc_histograms
+
+        d.update(plot_uc_histograms(uc_params))
         # self._plot_uc_vs_detector_distance(uc_params, panel_distances, outliers, params.steps_per_angstrom)
         # self._plot_number_of_crystals(experiments)
 
@@ -182,98 +178,6 @@ class multi_crystal_analysis(xia2_report_base):
             xtitle="Dataset",
             ytitle="Distance (Å^2)",
         )
-
-        return d
-
-    @staticmethod
-    def _plot_uc_histograms(uc_params):
-
-        a, b, c = (flex_double_as_string(p, n_digits=4) for p in uc_params[:3])
-        d = OrderedDict()
-
-        d["uc_scatter"] = {
-            "data": [
-                {
-                    "x": a,
-                    "y": b,
-                    "type": "scatter",
-                    "mode": "markers",
-                    "name": "a vs. b",
-                    "xaxis": "x",
-                    "yaxis": "y",
-                },
-                {
-                    "x": b,
-                    "y": c,
-                    "type": "scatter",
-                    "mode": "markers",
-                    "name": "b vs. c",
-                    "xaxis": "x2",
-                    "yaxis": "y2",
-                },
-                {
-                    "x": c,
-                    "y": a,
-                    "type": "scatter",
-                    "mode": "markers",
-                    "name": "c vs. a",
-                    "xaxis": "x3",
-                    "yaxis": "y3",
-                },
-            ],
-            "layout": {
-                "grid": {"rows": 1, "columns": 3, "pattern": "independent"},
-                "title": "Distribution of unit cell parameters",
-                "showlegend": False,
-                "xaxis": {"title": "a (Å)"},
-                "yaxis": {"title": "b (Å)"},
-                "xaxis2": {"title": "b (Å)"},
-                "yaxis2": {"title": "c (Å)"},
-                "xaxis3": {"title": "c (Å)"},
-                "yaxis3": {"title": "a (Å)"},
-            },
-        }
-
-        d["uc_hist"] = {
-            "data": [
-                {
-                    "x": a,
-                    "type": "histogram",
-                    "connectgaps": False,
-                    "name": "uc_hist_a",
-                    "nbins": "auto",
-                    "xaxis": "x",
-                    "yaxis": "y",
-                },
-                {
-                    "x": b,
-                    "type": "histogram",
-                    "connectgaps": False,
-                    "name": "uc_hist_b",
-                    "nbins": "auto",
-                    "xaxis": "x2",
-                    "yaxis": "y",
-                },
-                {
-                    "x": c,
-                    "type": "histogram",
-                    "connectgaps": False,
-                    "name": "uc_hist_c",
-                    "nbins": "auto",
-                    "xaxis": "x3",
-                    "yaxis": "y",
-                },
-            ],
-            "layout": {
-                "grid": {"rows": 1, "columns": 3, "subplots": [["xy", "x2y", "x3y"]]},
-                "title": "Histogram of unit cell parameters",
-                "showlegend": False,
-                "xaxis": {"title": "a (Å)"},
-                "yaxis": {"title": "Frequency"},
-                "xaxis2": {"title": "b (Å)"},
-                "xaxis3": {"title": "c (Å)"},
-            },
-        }
 
         return d
 
