72c2b754712e721122078eec0a03e4a30b767224
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-10-31 12:37:03 +0000

    Remove broken sort-of-sort dictionary function
    Since the lattice_manager __init__() call depends on an undefined
    function in OtherCell() the assumption is that this code path has not
    been used in years. Therefore rip it out entirely.

--- Schema/XCrystal.py
+++ Schema/XCrystal.py
@@ -16,13 +16,6 @@
 # "average" value and also handling lattice changes during the data
 # reduction.
 #
-# This latter function is delegated to a lower level object, the
-# lattice manager which is contained in this module.
-#
-# This depends on:
-#
-# DPA/Wrappers/CCP4/Othercell
-#
 # FIXME 05/SEP/06 question - do I want to maintain a link to the unit cells
 #                 of am I better off just handling the possible lattices and
 #                 treating the unit cells as a separate problem? Maintaining
@@ -66,7 +59,6 @@
 from __future__ import absolute_import, division, print_function
 
 import collections
-import math
 import os
 
 # Generation of Crystallographic Information Files (CIF/mmCIF)
@@ -78,65 +70,6 @@ from xia2.Handlers.Streams import banner
 from xia2.Handlers.Syminfo import Syminfo
 from xia2.lib.NMolLib import compute_nmol, compute_solvent
 from xia2.Modules.Scaler.ScalerFactory import Scaler
-from xia2.Wrappers.CCP4.Othercell import Othercell
-
-
-def sort_o_dict(dict, metric):
-    """A generic sorter for dictionaries - will return the keys in
-    the correct order for sorting by the input metric."""
-    result = []
-    jiffy = []
-
-    class sort_o_thing(object):
-        def __init__(self, tag, guff):
-            self.tag = tag
-            for key in guff.keys():
-                setattr(self, key, guff[key])
-
-        def __cmp__(self, other):
-            return getattr(self, metric) < getattr(other, metric)
-
-    for key in dict.keys():
-        jiffy.append(sort_o_thing(key, dict[key]))
-
-    jiffy.sort()
-
-    for j in jiffy:
-        result.append(j.tag)
-
-    return result
-
-
-class _lattice_manager(object):
-    """A class to manage lattice representations."""
-
-    def __init__(self, index_lattice, index_cell):
-        """Initialise the whole system from the original indexing
-        results."""
-
-        self._allowed_lattices = {}
-        self._allowed_lattice_order = []
-
-        o = Othercell()
-        o.set_cell(index_cell)
-        o.set_lattice(index_lattice)
-
-        o.generate()
-
-        self._allowed_lattices = o.get_possible_lattices()
-        self._allowed_lattice_order = sort_o_dict(self._allowed_lattices, "number")
-        self._allowed_lattice_order.reverse()
-
-    def get_lattice(self):
-        return self._allowed_lattices[self._allowed_lattice_order[0]]
-
-    def kill_lattice(self):
-        # remove the top one from the list
-
-        if len(self._allowed_lattice_order) == 1:
-            raise RuntimeError("out of lattices")
-
-        self._allowed_lattice_order = self._allowed_lattice_order[1:]
 
 
 class _aa_sequence(object):
@@ -298,7 +231,6 @@ class XCrystal(object):
     and also amino acid sequence, heavy atom information."""
 
     def __init__(self, name, project):
-
         self._name = name
 
         # separate out the anomalous pairs or merge them together?
@@ -316,12 +248,9 @@ class XCrystal(object):
         self._ha_info = {}
         self._wavelengths = {}
         self._samples = {}
-        self._lattice_manager = None
 
         # hooks to dangle command interfaces from
-
         self._scaler = None
-
         self._refiner = None
 
         # things to store input reflections which are used to define
@@ -414,7 +343,6 @@ class XCrystal(object):
                 for k_, v_ in v.iteritems():
                     v[k_] = _ha_info.from_dict(v_)
             setattr(return_obj, k, v)
-        sweep_dict = {}
         for sample in return_obj._samples.values():
             for i, sname in enumerate(sample._sweeps):
                 found_sweep = False
@@ -880,61 +808,6 @@ class XCrystal(object):
             result.extend(self._wavelengths[wavelength].get_all_image_names())
         return result
 
-    def set_lattice(self, lattice, cell):
-        """Configure the cell - if it is already set, then manage this
-        carefully..."""
-
-        # FIXME this should also verify that the cell for the provided
-        # lattice exactly matches the limitations provided in IUCR
-        # tables A.
-
-        if self._lattice_manager:
-            self._update_lattice(lattice, cell)
-        else:
-            self._lattice_manager = _lattice_manager(lattice, cell)
-
-    def _update_lattice(self, lattice, cell):
-        """Inspect the available lattices and see if this matches
-        one of them..."""
-
-        # FIXME need to think in here in terms of the lattice
-        # being higher than the current one...
-        # though that shouldn't happen, because if this is the
-        # next processing, this should have taken the top
-        # lattice supplied earler as input...
-
-        while lattice != self._lattice_manager.get_lattice()["lattice"]:
-            self._lattice_manager.kill_lattice()
-
-        # this should now point to the correct lattice class...
-        # check that the unit cell matches reasonably well...
-
-        cell_orig = self._lattice_manager.get_lattice()["cell"]
-
-        dist = 0.0
-
-        for j in range(6):
-            dist += math.fabs(cell_orig[j] - cell[j])
-
-        # allow average of 1 degree, 1 angstrom
-        if dist > 6.0:
-            raise RuntimeError(
-                "new lattice incompatible: %s vs. %s"
-                % (
-                    "[%6.2f, %6.2f, %6.2f, %6.2f, %6.2f, %6.2f]" % tuple(cell),
-                    "[%6.2f, %6.2f, %6.2f, %6.2f, %6.2f, %6.2f]" % tuple(cell_orig),
-                )
-            )
-
-        # if we reach here we're satisfied that the new lattice matches...
-        # FIXME write out some messages here to Chatter.
-
-    def get_lattice(self):
-        if self._lattice_manager:
-            return self._lattice_manager.get_lattice()
-
-        return None
-
     def set_anomalous(self, anomalous=True):
         self._anomalous = anomalous
 
@@ -1033,26 +906,3 @@ class XCrystal(object):
             scaler.as_json(
                 filename=os.path.join(scaler.get_working_directory(), "xia2.json")
             )
-
-
-if __name__ == "__main__":
-    # lm = _lattice_manager('aP', (43.62, 52.27, 116.4, 103, 100.7, 90.03))
-    # _print_lattice(lm.get_lattice())
-    # lm.kill_lattice()
-    # _print_lattice(lm.get_lattice())
-
-    xc = XCrystal("DEMO", None)
-
-    # this should configure with all possible lattices, though
-    # I think going through an explicit "init lattices" would help...
-    xc.set_lattice("aP", (43.62, 52.27, 116.4, 103, 100.7, 90.03))
-    _print_lattice(xc.get_lattice())
-
-    # this should "drop" the lattice by one - the idea here is
-    # that this is the output from e.g. pointless updating the lattice
-    # used for processing
-    xc.set_lattice("mC", (228.70, 43.62, 52.27, 90.00, 103.20, 90.00))
-    _print_lattice(xc.get_lattice())
-
-    # this should raise an exception - the unit cell is not compatible
-    xc.set_lattice("mC", (221.0, 44.0, 57.0, 90.0, 106.0, 90.0))
