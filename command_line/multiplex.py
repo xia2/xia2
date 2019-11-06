xia2/xia2

commit 83ce8c4609ba8c1e035522c487e1b2ea5f43c8a6

Perform systematic absence analysis in multiplex
Run dials.symmetry in systematic-absences-only mode after scaling to determine
full space group in xia2.multiplex.
Set laue_group= to skip Laue group determination by dials.cosym.
Set space_group= to skip both Laue group determination by dials.cosym and
systematic absences analysis by dials.symmetry.
Eliminate use of procrunner, instead calling command line run() method directly.
Eliminate Sorry.
Fixes #355

command_line/multiplex.py
@@ -3,9 +3,9 @@ from __future__ import absolute_import, division, print_function
 
 import logging
 import random
+import sys
 
 import iotbx.phil
-from dials.util import Sorry
 
 import xia2.Handlers.Streams
 from dials.array_family import flex
@@ -39,7 +39,7 @@ output {
 )
 
 
-def run():
+def run(args):
     usage = (
         "xia2.multiplex [options] [param.phil] "
         "models1.expt models2.expt observations1.refl "
@@ -57,7 +57,7 @@ def run():
     )
 
     # Parse the command line
-    params, options = parser.parse_args(show_diff_phil=False)
+    params, options = parser.parse_args(args=args, show_diff_phil=False)
 
     # Configure the logging
     xia2.Handlers.Streams.streams_off()
@@ -85,7 +85,7 @@ def run():
     try:
         assert len(params.input.reflections) == len(params.input.experiments)
     except AssertionError:
-        raise Sorry(
+        raise sys.exit(
             "The number of input reflections files does not match the "
             "number of input experiments"
         )
@@ -113,7 +113,11 @@ def run():
         for identifier in params.identifiers:
             identifiers.extend(identifier.split(","))
         params.identifiers = identifiers
-    ScaleAndMerge.MultiCrystalScale(experiments, reflections_all, params)
+
+    try:
+        scaled = ScaleAndMerge.MultiCrystalScale(experiments, reflections_all, params)
+    except ValueError as e:
+        sys.exit(str(e))
 
 
 if __name__ == "__main__":

Modules/MultiCrystal/ScaleAndMerge.py
@@ -82,8 +82,14 @@ symmetry {
   }
   le_page_max_delta = 5
     .type = float(value_min=0)
+  laue_group = None
+    .type = space_group
+    .help = "Specify the Laue group. If None, then the Laue group will be determined "
+            "by dials.cosym."
   space_group = None
     .type = space_group
+    .help = "Specify the space group. If None, then the dials.symmetry will perform "
+            "analysis of systematically absent reflections to determine the space group."
 }
 
 resolution
@@ -305,6 +311,8 @@ class MultiCrystalScale(object):
         self._data_manager = DataManager(experiments, reflections)
 
         self._params = params
+        if all([params.symmetry.laue_group, params.symmetry.space_group]):
+            raise ValueError("Can not specify both laue_group and space_group")
 
         if self._params.nproc is Auto:
             self._params.nproc = get_number_cpus()
@@ -366,13 +374,13 @@ class MultiCrystalScale(object):
         if self._params.symmetry.resolve_indexing_ambiguity:
             self.cosym()
 
-        self.decide_space_group()
-
         self._individual_report_dicts = OrderedDict()
         self._comparison_graphs = OrderedDict()
 
         self._scaled = Scale(self._data_manager, self._params)
         self._record_individual_report(self._scaled.report(), "All data")
+
+        self.decide_space_group()
 
         self._data_manager.export_experiments("multiplex.expt")
         self._data_manager.export_reflections("multiplex.refl")
@@ -605,6 +613,8 @@ class MultiCrystalScale(object):
         cosym.add_reflections_file(reflections_filename)
         if self._params.symmetry.space_group is not None:
             cosym.set_space_group(self._params.symmetry.space_group.group())
+        if self._params.symmetry.laue_group is not None:
+            cosym.set_space_group(self._params.symmetry.laue_group.group())
         cosym.run()
         self._cosym_analysis = cosym.get_cosym_analysis()
         self._experiments_filename = cosym.get_reindexed_experiments()
@@ -616,14 +626,16 @@ class MultiCrystalScale(object):
             self._reflections_filename
         )
 
-        if self._params.symmetry.space_group is None:
+        if not any(
+            [self._params.symmetry.space_group, self._params.symmetry.laue_group]
+        ):
             best_solution = cosym.get_best_solution()
             best_space_group = sgtbx.space_group(
                 str(best_solution["patterson_group"])
             ).build_derived_acentric_group()
-            self._params.symmetry.space_group = best_space_group.info()
+            self._params.symmetry.laue_group = best_space_group.info()
             logger.info(
-                "Space group determined by dials.cosym: %s" % best_space_group.info()
+                "Laue group determined by dials.cosym: %s" % best_space_group.info()
             )
 
     def decide_space_group(self):
@@ -663,10 +675,8 @@ class MultiCrystalScale(object):
         symmetry.set_tolerance(
             relative_length_tolerance=None, absolute_angle_tolerance=None
         )
+        symmetry.set_mode_absences_only()
         symmetry.decide_pointgroup()
-        space_group = sgtbx.space_group_info(
-            symbol=str(symmetry.get_pointgroup())
-        ).group()
 
         self._data_manager.experiments = load.experiment_list(
             self._experiments_filename, check_format=False
@@ -674,6 +684,7 @@ class MultiCrystalScale(object):
         self._data_manager.reflections = flex.reflection_table.from_file(
             self._reflections_filename
         )
+        space_group = self._data_manager.experiments[0].crystal.get_space_group()
 
         logger.info("Space group determined by dials.symmetry: %s" % space_group.info())
 

Test/regression/test_multiplex.py
@@ -1,37 +1,78 @@
 from __future__ import absolute_import, division, print_function
 
-import procrunner
 import pytest
 
-expected_data_files = ["scaled.mtz", "scaled_unmerged.mtz", "xia2.multiplex.html"]
+from dxtbx.serialize import load
+from xia2.command_line.multiplex import run
+
+expected_data_files = [
+    "multiplex.expt",
+    "multiplex.refl",
+    "scaled.mtz",
+    "scaled_unmerged.mtz",
+    "xia2.multiplex.html",
+]
 
 
-def test_proteinase_k(regression_test, ccp4, dials_data, run_in_tmpdir):
+def test_proteinase_k(regression_test, ccp4, dials_data, tmpdir):
     data_dir = dials_data("multi_crystal_proteinase_k")
     expts = sorted(f.strpath for f in data_dir.listdir("experiments*.json"))
     refls = sorted(f.strpath for f in data_dir.listdir("reflections*.pickle"))
-    command_line = ["xia2.multiplex"] + expts + refls
-    print(" ".join(command_line))
-    result = procrunner.run(command_line)
-    assert not result["exitcode"]
+    with tmpdir.as_cwd():
+        run(expts + refls)
     for f in expected_data_files:
-        assert run_in_tmpdir.join(f).check(file=1), "expected file %s missing" % f
+        assert tmpdir.join(f).check(file=1), "expected file %s missing" % f
+    multiplex_expts = load.experiment_list(
+        tmpdir.join("multiplex.expt").strpath, check_format=False
+    )
+    for expt in multiplex_expts:
+        assert expt.crystal.get_space_group().type().lookup_symbol() == "P 41 21 2"
 
 
-@pytest.mark.parametrize("space_group", [None, "P422"])
+@pytest.mark.parametrize(
+    "laue_group,space_group", [("P422", None), (None, "P422"), (None, "P43212")]
+)
 def test_proteinase_k_dose(
-    space_group, regression_test, ccp4, dials_data, run_in_tmpdir
+    laue_group, space_group, regression_test, ccp4, dials_data, tmpdir
 ):
     data_dir = dials_data("multi_crystal_proteinase_k")
     expts = sorted(f.strpath for f in data_dir.listdir("experiments*.json"))
     refls = sorted(f.strpath for f in data_dir.listdir("reflections*.pickle"))
-    command_line = (
-        ["xia2.multiplex", "dose=1,20", "symmetry.space_group=%s" % space_group]
+    command_line_args = (
+        [
+            "dose=1,20",
+            "symmetry.laue_group=%s" % laue_group,
+            "symmetry.space_group=%s" % space_group,
+        ]
         + expts
         + refls
     )
-    print(" ".join(command_line))
-    result = procrunner.run(command_line)
-    assert not result["exitcode"]
+    with tmpdir.as_cwd():
+        run(command_line_args)
     for f in expected_data_files:
-        assert run_in_tmpdir.join(f).check(file=1), "expected file %s missing" % f
+        assert tmpdir.join(f).check(file=1), "expected file %s missing" % f
+    multiplex_expts = load.experiment_list(
+        tmpdir.join("multiplex.expt").strpath, check_format=False
+    )
+    for expt in multiplex_expts:
+        if space_group is None:
+            assert expt.crystal.get_space_group().type().lookup_symbol() == "P 41 21 2"
+        else:
+            assert (
+                expt.crystal.get_space_group().type().lookup_symbol().replace(" ", "")
+                == space_group
+            )
+
+
+def test_proteinase_k_laue_group_space_group_raises_error(
+    regression_test, ccp4, dials_data, tmpdir
+):
+    data_dir = dials_data("multi_crystal_proteinase_k")
+    expts = sorted(f.strpath for f in data_dir.listdir("experiments*.json"))
+    refls = sorted(f.strpath for f in data_dir.listdir("reflections*.pickle"))
+    command_line_args = (
+        ["symmetry.laue_group=P422", "symmetry.space_group=P41212"] + expts + refls
+    )
+    with tmpdir.as_cwd():
+        with pytest.raises(SystemExit):
+            run(command_line_args)

commit ea531293b50bf33f2995efd9e6f4bd47d3323c95

Fix bug introduced by 83ce8c46

command_line/multiplex.py
@@ -115,10 +115,10 @@ def run(args):
         params.identifiers = identifiers
 
     try:
-        scaled = ScaleAndMerge.MultiCrystalScale(experiments, reflections_all, params)
+        ScaleAndMerge.MultiCrystalScale(experiments, reflections_all, params)
     except ValueError as e:
         sys.exit(str(e))
 
 
 if __name__ == "__main__":
-    run()
+    run(sys.argv[1:])