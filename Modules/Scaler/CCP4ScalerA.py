e97a82e989c45362c94b7224a31fc6d3095b7c49
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-11-12 08:27:54 +0000

    Fix 'variable redefined' errors

--- Modules/Scaler/CCP4ScalerA.py
+++ Modules/Scaler/CCP4ScalerA.py
@@ -592,7 +592,7 @@ class CCP4ScalerA(Scaler):
                 "Probably twinned, pointgroups: %s"
                 % " ".join(p.replace(" ", "") for p in list(pointgroup_set))
             )
-            numbers = [Syminfo.spacegroup_name_to_number(s) for s in pointgroup_set]
+            numbers = (Syminfo.spacegroup_name_to_number(ps) for ps in pointgroup_set)
             overall_pointgroup = Syminfo.spacegroup_number_to_name(min(numbers))
             self._scalr_input_pointgroup = overall_pointgroup
 
