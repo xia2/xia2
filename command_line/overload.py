bf98d1b9e7ee6b76d74328522ba06733cf6c84b0
Graeme Winter <graeme.winter@gmail.com>
2020-02-13 12:20:12 +0000

    Remove hard coded CBF assumptions

--- command_line/overload.py
+++ command_line/overload.py
@@ -95,56 +95,58 @@ def build_hist(nproc=1):
     if len(sys.argv) >= 2 and sys.argv[1].startswith("nproc="):
         nproc = int(sys.argv[1][6:])
         sys.argv = sys.argv[1:]
-    if len(sys.argv) == 2 and sys.argv[1].endswith((".expt", ".json")):
-        from dxtbx.model.experiment_list import ExperimentListFactory
 
-        experiments = ExperimentListFactory.from_json_file(sys.argv[1])
-        image_list = experiments.imagesets()[0].paths()
-    else:
-        image_list = sys.argv[1:]
-    image_count = len(image_list)
+    from dxtbx.model.experiment_list import ExperimentListFactory
 
-    # Faster, yet still less than ideal and wasting a lot of resources.
-    limit = get_overload(image_list[0])
-    binfactor = 5  # register up to 500% counts
-    histmax = (limit * binfactor) + 0.0
-    histbins = int(limit * binfactor) + 1
-    use_python_counter = histbins > 90000000  # empirically determined
+    experiments = ExperimentListFactory.from_args(sys.argv[1:])
+    if len(experiments) == 0:
+        experiments = ExperimentListFactory.from_filenames(sys.argv[1:])
 
-    print(
-        "Processing %d images in %d processes using %s\n"
-        % (
-            image_count,
-            nproc,
-            "python Counter" if use_python_counter else "flex arrays",
-        )
-    )
+    for experiment in experiments:
+        imageset = experiment.imageset
+        limit = experiment.detector[0].get_trusted_range()[1]
+        n0, n1 = experiment.scan.get_image_range()
+        image_count = n1 - n0 + 1
 
-    def process_image(process):
-        last_update = start = timeit.default_timer()
+        binfactor = 5  # register up to 500% counts
+        histmax = (limit * binfactor) + 0.0
+        histbins = int(limit * binfactor) + 1
+        use_python_counter = histbins > 90000000  # empirically determined
 
-        i = process
-        if use_python_counter:
-            local_hist = Counter()
-        else:
-            local_hist = flex.histogram(
-                flex.double(), data_min=0.0, data_max=histmax, n_slots=histbins
+        print(
+            "Processing %d images in %d processes using %s\n"
+            % (
+                image_count,
+                nproc,
+                "python Counter" if use_python_counter else "flex arrays",
             )
+        )
 
-        max_images = image_count // nproc
-        if process >= image_count % nproc:
-            max_images += 1
-        while i < image_count:
-            data = read_cbf_image(image_list[i])
-            if not use_python_counter:
-                data = flex.histogram(
-                    data.as_double().as_1d(),
-                    data_min=0.0,
-                    data_max=histmax,
-                    n_slots=histbins,
+        def process_image(process):
+            last_update = start = timeit.default_timer()
+
+            i = process
+            if use_python_counter:
+                local_hist = Counter()
+            else:
+                local_hist = flex.histogram(
+                    flex.double(), data_min=0.0, data_max=histmax, n_slots=histbins
                 )
-            local_hist.update(data)
-            i = i + nproc
+
+            max_images = image_count // nproc
+            if process >= image_count % nproc:
+                max_images += 1
+            while i < image_count:
+                data = imageset.get_raw_data(i)[0]
+                if not use_python_counter:
+                    data = flex.histogram(
+                        data.as_double().as_1d(),
+                        data_min=0.0,
+                        data_max=histmax,
+                        n_slots=histbins,
+                    )
+                local_hist.update(data)
+                i = i + nproc
             if process == 0:
                 if timeit.default_timer() > (last_update + 3):
                     last_update = timeit.default_timer()
@@ -157,37 +159,37 @@ def build_hist(nproc=1):
                             round((image_count - i) * (last_update - start) / (i + 1)),
                         )
                     )
-        return local_hist
+            return local_hist
 
-    results = easy_mp.parallel_map(
-        func=process_image,
-        iterable=range(nproc),
-        processes=nproc,
-        preserve_exception_message=True,
-    )
+        results = easy_mp.parallel_map(
+            func=process_image,
+            iterable=range(nproc),
+            processes=nproc,
+            preserve_exception_message=True,
+        )
 
-    print("Merging results")
-    result_hist = None
-    for hist in results:
-        if result_hist is None:
-            result_hist = hist
-        else:
-            result_hist.update(hist)
+        print("Merging results")
+        result_hist = None
+        for hist in results:
+            if result_hist is None:
+                result_hist = hist
+            else:
+                result_hist.update(hist)
 
-    if not use_python_counter:
-        # reformat histogram into dictionary
-        result = list(result_hist.slots())
-        result_hist = {b: count for b, count in enumerate(result) if count > 0}
+        if not use_python_counter:
+            # reformat histogram into dictionary
+            result = list(result_hist.slots())
+            result_hist = {b: count for b, count in enumerate(result) if count > 0}
 
-    results = {
-        "scale_factor": 1 / limit,
-        "overload_limit": limit,
-        "counts": result_hist,
-    }
+        results = {
+            "scale_factor": 1 / limit,
+            "overload_limit": limit,
+            "counts": result_hist,
+        }
 
-    print("Writing results to overload.json")
-    with open("overload.json", "w") as fh:
-        json.dump(results, fh, indent=1, sort_keys=True)
+        print("Writing results to overload.json")
+        with open("overload.json", "w") as fh:
+            json.dump(results, fh, indent=1, sort_keys=True)
 
 
 if __name__ == "__main__":
