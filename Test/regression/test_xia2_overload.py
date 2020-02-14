9b14b68660f99f809d42e8185d012731c04c32b0
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2020-02-14 11:10:16 +0000

    Do black

--- Test/regression/test_xia2_overload.py
+++ Test/regression/test_xia2_overload.py
@@ -6,6 +6,6 @@ import procrunner
 def test(dials_data, tmpdir):
     images = dials_data("centroid_test_data").listdir("centroid*.cbf")
 
-    result = procrunner.run(["xia2.overload",] + images, working_directory=tmpdir,)
+    result = procrunner.run(["xia2.overload"] + images, working_directory=tmpdir)
     assert not result.returncode and not result.stderr
     assert tmpdir.join("overload.json").check()
