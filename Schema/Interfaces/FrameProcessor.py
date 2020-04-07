e1518d240cbb9759ca95ce71020e923a874457ea
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2020-04-07 10:48:06 +0100

    Remove redundant object.__init__() call

--- Schema/Interfaces/FrameProcessor.py
+++ Schema/Interfaces/FrameProcessor.py
@@ -35,8 +35,6 @@ class FrameProcessor(object):
     diffraction frames."""
 
     def __init__(self, image=None):
-        super(FrameProcessor, self).__init__()
-
         self._fp_template = None
         self._fp_directory = None
 
