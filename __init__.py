12d94e35a8d9586c1f3f6749e4e050dbfcef5e21
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-05-11 10:16:44 +0100

    no longer required after merge of cctbx/cctbx_project#324

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
-import os
 import sys
 import warnings
 
@@ -10,17 +9,3 @@ if sys.version_info.major == 2:
         "For more information on Python 2.7 support please go to https://github.com/dials/dials/issues/1175.",
         DeprecationWarning,
     )
-
-# Invert FPE trap defaults, https://github.com/cctbx/cctbx_project/pull/324
-if "boost.python" in sys.modules:
-    import boost.python
-
-    boost.python.ext.trap_exceptions(
-        bool(os.getenv("BOOST_ADAPTBX_TRAP_FPE")),
-        bool(os.getenv("BOOST_ADAPTBX_TRAP_INVALID")),
-        bool(os.getenv("BOOST_ADAPTBX_TRAP_OVERFLOW")),
-    )
-elif not os.getenv("BOOST_ADAPTBX_TRAP_FPE") and not os.getenv(
-    "BOOST_ADAPTBX_TRAP_OVERFLOW"
-):
-    os.environ["BOOST_ADAPTBX_FPE_DEFAULT"] = "1"
