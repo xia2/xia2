5390a3a37938c17086249877a0f098af25b8483c
Graeme Winter <graeme.winter@gmail.com>
2020-05-14 14:16:23 +0100

    Tear pages from diary

--- Driver/DriverFactory.py
+++ Driver/DriverFactory.py
@@ -1,10 +1,3 @@
-# DriverFactory.py
-#
-#   Copyright (C) 2006 CCLRC, Graeme Winter
-#
-#   This code is distributed under the BSD license, a copy of which is
-#   included in the root directory of this package.
-
 from __future__ import absolute_import, division, print_function
 
 import os
