e7169b938e2d83d76649896bee9c052685fa10ea
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-05-20 23:18:30 +0100

    Guard against empty bins in deltacchalf analysis

--- Modules/DeltaCcHalf.py
+++ Modules/DeltaCcHalf.py
@@ -127,9 +127,9 @@ class DeltaCcHalf:
             )
         else:
             cc_bins = intensities.cc_one_half(use_binning=True, return_n_refl=True)
+        bin_data = [b for b in cc_bins if b is not None]
         cc_i = flex.mean_weighted(
-            flex.double(b[0] for b in cc_bins.data[1:-1]),
-            flex.double(b[1] for b in cc_bins.data[1:-1]),
+            flex.double(b[0] for b in bin_data), flex.double(b[1] for b in bin_data),
         )
         return self.cc_overall - cc_i
 
