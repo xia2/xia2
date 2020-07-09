8b0f5fa4e0bc9737915279f3198e5793c4ba8922
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2020-07-09 15:16:41 +0100

    Fix logic to find set of unique instrument models
    Also update a deficient test.  The dodgy logic and test were introduced
    in #485.

--- Test/Schema/test_Schema.py
+++ Test/Schema/test_Schema.py
@@ -2,7 +2,7 @@ import mock
 import sys
 
 from dxtbx.model import ExperimentList
-from xia2.Schema import load_imagesets, load_reference_geometries
+from xia2.Schema import load_imagesets, load_reference_geometries, compare_geometries
 
 
 def test_load_imageset(dials_data, tmp_path):
@@ -27,14 +27,18 @@ def test_load_reference_geometries(dials_data):
 
     Test the function that finds the set of unique instrument models from a list
     of experiment list files.
+
+    There are eight input instrument models, of which only two are unique.
     """
     files = ["scaled_20_25.expt", "scaled_30.expt", "scaled_35.expt"]
     files = [(dials_data("l_cysteine_4_sweeps_scaled") / f).strpath for f in files]
+    files.append((dials_data("l_cysteine_dials_output") / "indexed.expt").strpath)
 
-    # Check that there are four input instrument models, of which only two are unique.
-    assert (
-        sum(len(ExperimentList.from_file(f, check_format=False)) for f in files) == 4
-    ), "Expected to find four experiments, one for each sweep."
-    assert (
-        len(load_reference_geometries(files)) == 2
-    ), "Expected to find two unique instrument models."
+    num_input = sum(len(ExperimentList.from_file(f, check_format=False)) for f in files)
+    assert num_input == 8, "Expected to find eight experiments, one for each sweep."
+
+    unique_geometries = load_reference_geometries(files)
+    assert len(unique_geometries) == 2, "Expected to find two unique instrument models."
+
+    detectors = (geom["detector"] for geom in unique_geometries)
+    assert not compare_geometries(*detectors), "Unique detectors cannot be equivalent."
