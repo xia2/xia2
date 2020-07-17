7de651b54c59ea16e95184e1d1197d5ce007d3e9
Graeme Winter <graeme.winter@gmail.com>
2020-07-17 07:16:07 +0100

    Revert "Match rename of resolutionizer"
    This reverts commit d33f60e363a8adc2c350b22a7105403d84eb40f6.
    
    Has more instances where this needs looking at => branch && PR

--- Handlers/Phil.py
+++ Handlers/Phil.py
@@ -773,7 +773,7 @@ xia2.settings
       .type = float(value_min=0.0)
       .help = "High resolution cutoff."
       .short_caption = "High resolution cutoff"
-    include scope dials.util.resolution_analysis.phil_str
+    include scope dials.util.resolutionizer.phil_str
   }
   unify_setting = False
     .type = bool
