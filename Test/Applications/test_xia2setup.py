70bec6011e6df21bb468219f304fd7325d81ee0d
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-07-24 09:44:59 +0100

    Add unroll write_xinfo test
    Much singleton hacks :(

--- Test/Applications/test_xia2setup.py
+++ Test/Applications/test_xia2setup.py
@@ -21,10 +21,14 @@ def test_write_xinfo_split_sweep(dials_data, tmp_path, monkeypatch):
             "read_all_image_headers=False",
         ],
     ):
-        # Force reload of CommandLine singleton object with the parameters above
+        # Force reload of various singleton objects with the parameters above
         import xia2.Handlers.CommandLine
+        import xia2.Applications.xia2setup
+        import xia2.Schema
 
         reload(xia2.Handlers.CommandLine)
+        reload(xia2.Applications.xia2setup)
+        xia2.Schema.imageset_cache = xia2.Schema._ImagesetCache()
 
         template = xia2.Handlers.CommandLine.CommandLine.get_template()
         directories = [os.path.split(p) for p in template]
@@ -37,5 +41,40 @@ def test_write_xinfo_split_sweep(dials_data, tmp_path, monkeypatch):
         assert xinfo.exists()
         x = XInfo(xinfo)
         assert len(x.get_crystals()["DEFAULT"]["sweeps"]) == 2
-        x.get_crystals()["DEFAULT"]["sweeps"]["SWEEP1"]["start_end"] == [1, 22]
-        x.get_crystals()["DEFAULT"]["sweeps"]["SWEEP2"]["start_end"] == [23, 45]
+        assert x.get_crystals()["DEFAULT"]["sweeps"]["SWEEP1"]["start_end"] == [1, 22]
+        assert x.get_crystals()["DEFAULT"]["sweeps"]["SWEEP2"]["start_end"] == [23, 45]
+
+
+def test_write_xinfo_unroll(dials_data, tmp_path):
+    # This test partially exercises the fix to https://github.com/xia2/xia2/issues/498 with a different syntax
+    xinfo = tmp_path.joinpath("unroll.xinfo")
+
+    with mock.patch.object(
+        sys,
+        "argv",
+        [
+            f"image={dials_data('insulin').join('insulin_1_001.img:1:45:15')}",
+            "read_all_image_headers=False",
+        ],
+    ):
+        # Force reload of various singleton objects with the parameters above
+        import xia2.Handlers.CommandLine
+        import xia2.Applications.xia2setup
+        import xia2.Schema
+
+        reload(xia2.Handlers.CommandLine)
+        reload(xia2.Applications.xia2setup)
+        xia2.Schema.imageset_cache = xia2.Schema._ImagesetCache()
+
+        template = xia2.Handlers.CommandLine.CommandLine.get_template()
+        directories = [os.path.split(p) for p in template]
+
+        xia2.Applications.xia2setup.write_xinfo(
+            xinfo, directories, template=template,
+        )
+        assert xinfo.exists()
+        x = XInfo(xinfo)
+        assert len(x.get_crystals()["DEFAULT"]["sweeps"]) == 3
+        assert x.get_crystals()["DEFAULT"]["sweeps"]["SWEEP1"]["start_end"] == [1, 15]
+        assert x.get_crystals()["DEFAULT"]["sweeps"]["SWEEP2"]["start_end"] == [16, 30]
+        assert x.get_crystals()["DEFAULT"]["sweeps"]["SWEEP3"]["start_end"] == [31, 45]
