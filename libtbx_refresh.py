5dccbf2c69ed59092e9984ba563c406c656a6836
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-10-20 15:05:01 +0100

    Set up a setup.py for xia2
    including the new versioning logic using bump2version and
    importlib_metadata.

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -1,7 +1,39 @@
 import dials.precommitbx.nagger
-from xia2.XIA2Version import Version
 
 dials.precommitbx.nagger.nag()
 
-# the import implicitly updates the .gitversion file
-print(Version)
+
+def _install_xia2_setup():
+    """Install xia2 as a regular/editable python package"""
+    import subprocess
+    import sys
+
+    import libtbx.load_env
+
+    # Call pip
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "--no-deps",
+            "-e",
+            libtbx.env.dist_path("xia2"),
+        ],
+        check=True,
+    )
+
+
+def _show_xia2_version():
+    try:
+        from xia2.XIA2Version import Version
+
+        # the import implicitly updates the .gitversion file
+        print(Version)
+    except ModuleNotFoundError:
+        print("Can't tell xia2 version")
+
+
+_install_xia2_setup()
+_show_xia2_version()
--- setup.py
+++ setup.py
@@ -1,17 +1,51 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import sys
+import os
+import subprocess
 from setuptools import setup, find_packages
 
-requirements = ["dials-data", "Jinja2", "procrunner", "tabulate"]
+# Version number is determined either by git revision (which takes precendence)
+# or a static version number which is updated by bump2version
+__version_tag__ = "3.3.dev"
 
-setup_requirements = []
-needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
-if needs_pytest:
-    setup_requirements.append("pytest-runner")
 
-test_requirements = ["mock", "pytest"]
+def get_git_revision():
+    """Try to obtain the current git revision number"""
+    xia2_root_path = os.path.split(os.path.realpath(__file__))[0]
+
+    if not os.path.exists(os.path.join(xia2_root_path, ".git")):
+        return None
+
+    try:
+        result = subprocess.run(
+            ("git", "describe", "--long"),
+            check=True,
+            cwd=xia2_root_path,
+            encoding="latin-1",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+        )
+        version = result.stdout.rstrip()
+    except Exception:
+        return None
+    if version.startswith("v"):
+        version = version[1:].replace(".0-", ".")
+
+    try:
+        result = subprocess.run(
+            ("git", "describe", "--contains", "--all", "HEAD"),
+            check=True,
+            cwd=xia2_root_path,
+            encoding="latin-1",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+        )
+        branch = result.stdout.rstrip()
+        if branch != "" and branch != "master" and not branch.endswith("/master"):
+            version = version + "-" + branch
+    except Exception:
+        pass
+
+    return version
+
 
 setup(
     author="Diamond Light Source",
@@ -24,23 +58,39 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
     description="An expert system for automated reduction of X-Ray diffraction data from macromolecular crystals",
-    install_requires=requirements,
-    license="BSD license",
+    install_requires=[
+        "dials-data>=2.0",
+        "Jinja2",
+        "procrunner",
+        "tabulate",
+        'importlib_metadata;python_version<"3.8"',
+    ],
+    license="BSD-3-Clause",
     include_package_data=True,
     keywords="xia2",
     name="xia2",
-    packages=find_packages(),
-    package_dir={"xia2": "../xia2"},
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    project_urls={
+        "Changelog": "https://github.com/xia2/xia2/blob/master/CHANGELOG.rst",
+        "Issue Tracker": "https://github.com/xia2/xia2/issues",
+    },
+    python_requires=">=3.6",
     data_files=[
         ("xia2", ["XIA2Version.py", "__init__.py", "libtbx_refresh.py", "conftest.py"])
     ],
-    setup_requires=setup_requirements,
     test_suite="tests",
-    tests_require=test_requirements,
+    tests_require=[
+        "pytest>=3.1",
+        "pytest-mock",
+    ],
     url="https://github.com/xia2/xia2",
-    version="0.6.476",
+    version=get_git_revision() or __version_tag__,
     zip_safe=False,
 )
