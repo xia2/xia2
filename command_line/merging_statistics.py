88dde5ccb0245f3d7c2e8fa3d8384ebfafbc21a7
Richard Gildea <richard.gildea@diamond.ac.uk>
2020-11-03 09:44:34 +0000

    Improved error handling in xia2.merging_statistics
    * Cleanly call sys.exit if --help specified
    * Identify all input files (and any remaining unhandled args) before
    attempting to calculate any merging statistics

--- command_line/merging_statistics.py
+++ command_line/merging_statistics.py
@@ -131,16 +131,22 @@ def table1_tex(merging_stats):
 def run(args):
     if len(args) == 0 or "-h" in args or "--help" in args:
         master_params.show()
+        sys.exit()
     interp = master_params.command_line_argument_interpreter(args)
     phil_scope, unhandled = interp.process_and_fetch(
         args, custom_processor="collect_remaining"
     )
     params = phil_scope.extract()
-    merging_stats = []
+    input_files = []
 
     for arg in unhandled:
-        assert os.path.isfile(arg)
-        merging_stats.append(merging_statistics.run([arg], master_params=phil_scope))
+        if not os.path.isfile(arg):
+            print(f"{arg} is not a file or interpretable as a parameter")
+            sys.exit(1)
+        input_files.append(arg)
+    merging_stats = [
+        (merging_statistics.run([f], master_params=phil_scope)) for f in input_files
+    ]
     if params.latex:
         table1_tex(merging_stats)
 
