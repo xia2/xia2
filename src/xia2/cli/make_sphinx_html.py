f83a0df0c9f5d988ce724115f1f5109baaf6f201
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2020-12-09 10:28:42 +0000

    Use pathlib and fix up package paths for src-layout
    cf. #528

--- src/xia2/cli/make_sphinx_html.py
+++ src/xia2/cli/make_sphinx_html.py
@@ -1,23 +1,26 @@
-import os
 import shutil
 
+import pathlib
 import procrunner
 import xia2
 
 
 def run():
-    xia2_dir = os.path.dirname(xia2.__file__)
-    dest_dir = os.path.join(xia2_dir, "html")
-    if os.path.exists(dest_dir):
+    xia2_init_file = pathlib.Path(xia2.__file__)
+    xia2_dir = xia2_init_file.parent.parent.parent
+    if xia2_dir.name != "xia2":
+        exit(f"{xia2_dir} does not appear to be the xia2 directory")
+    dest_dir = xia2_dir / "html"
+    sphinx_dir = xia2_dir / "doc" / "sphinx"
+    if dest_dir.is_dir():
         shutil.rmtree(dest_dir)
-    os.chdir(os.path.join(xia2_dir, "doc", "sphinx"))
-    result = procrunner.run(["make", "clean"])
-    assert result["exitcode"] == 0, (
-        "make clean failed with exit code %d" % result["exitcode"]
-    )
-    result = procrunner.run(["make", "html"])
-    assert result["exitcode"] == 0, (
-        "make html failed with exit code %d" % result["exitcode"]
-    )
+    result = procrunner.run(["make", "clean"], working_directory=sphinx_dir)
+    if result.returncode:
+        exit(f"make clean failed with exit code {result.returncode}")
+
+    result = procrunner.run(["make", "html"], working_directory=sphinx_dir)
+    if result.returncode:
+        exit(f"make html failed with exit code {result.returncode}")
+
     print("Moving HTML pages to", dest_dir)
-    shutil.move("build/html", dest_dir)
+    shutil.move(sphinx_dir / "build" / "html", dest_dir)
