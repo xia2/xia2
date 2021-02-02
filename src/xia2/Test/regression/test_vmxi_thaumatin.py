96b2cee169b89d967d8e88ae5970fcfd219433e6
Richard Gildea <rjgildea@users.noreply.github.com>
2021-02-02 17:46:20 +0000

    Fix missing SENSOR_THICKNESS= for EIGER (#564)
    Bug introduced by #292
    
    See also cctbx/dxtbx#296
    
    * add end-to-end xia2 vmxi_thaumatin eiger test
    
    * Different fallback PILATUS/EIGER sensor thicknesses

---
+++ src/xia2/Test/regression/test_vmxi_thaumatin.py
@@ -0,0 +1,27 @@
+import procrunner
+import pytest
+
+import xia2.Test.regression
+
+
+@pytest.mark.parametrize("pipeline", ["dials", "3dii"])
+def test_xia2(pipeline, regression_test, dials_data, tmpdir, ccp4):
+    master_h5 = dials_data("vmxi_thaumatin") / "image_15799_master.h5:1:20"
+    command_line = [
+        "xia2",
+        f"pipeline={pipeline}",
+        "nproc=1",
+        "trust_beam_centre=True",
+        "read_all_image_headers=False",
+        "space_group=P41212",
+        f"image={master_h5}",
+    ]
+    result = procrunner.run(command_line, working_directory=tmpdir)
+    success, issues = xia2.Test.regression.check_result(
+        f"vxmi_thaumatin.{pipeline}",
+        result,
+        tmpdir,
+        ccp4,
+        expected_space_group="P41212",
+    )
+    assert success, issues
