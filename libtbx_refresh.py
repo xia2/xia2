2abb25e51fe69870494e1c8eec0c196e6ace56f0
Markus Gerstel <2102431+Anthchirp@users.noreply.github.com>
2021-02-10 11:06:09 +0000

    Deliberately break the 'master' branch (#561)
    in preparation to move to 'main'.
    Closes #557

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -2,6 +2,25 @@ import dials.precommitbx.nagger
 
 dials.precommitbx.nagger.nag()
 
+import libtbx.load_env
+
+exit(
+    ("=" * 80)
+    + """
+
+Your xia2 repository is still tracking 'master',
+but the main xia2 branch has been renamed to 'main'.
+
+Please go into your xia2 repository at %s and run the following commands:
+  git branch -m master main
+  git fetch origin
+  git branch -u origin/main main
+
+For more information please see https://github.com/xia2/xia2/issues/557
+"""
+    % libtbx.env.dist_path("xia2")
+)
+
 
 def _install_xia2_setup():
     """Install xia2 as a regular/editable python package"""
