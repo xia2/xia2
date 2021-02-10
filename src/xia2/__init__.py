c9b9dbacd6c99dd3add5a23b6f819eb5b009091e
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2021-02-10 11:10:37 +0000

    Unbreak 'main' branch (part 2 of #561)
    This reverts commit 2abb25e51fe69870494e1c8eec0c196e6ace56f0.

--- src/xia2/__init__.py
+++ src/xia2/__init__.py
@@ -2,24 +2,3 @@ import sys
 
 if sys.version_info.major == 2:
     sys.exit("Python 2 is no longer supported")
-
-import pathlib
-
-_xia2 = pathlib.Path(__file__).parents[2]
-
-exit(
-    ("=" * 80)
-    + """
-
-Your xia2 repository is still tracking 'master',
-but the main xia2 branch has been renamed to 'main'.
-
-Please go into your xia2 repository at %s and run the following commands:
-  git branch -m master main
-  git fetch origin
-  git branch -u origin/main main
-
-For more information please see https://github.com/xia2/xia2/issues/557
-"""
-    % _xia2
-)
