4bf71a61b6c59ee42843a18dc778e7735df9c4ab
David Waterman <dagewa@users.noreply.github.com>
2022-01-14 11:49:27 +0000

    Add and update PHIL captions (#644)
    so that they can be read by the i2 GUI.
    
    Co-authored-by: Richard Gildea <rjgildea@users.noreply.github.com>

--- src/xia2/Modules/MultiCrystal/__init__.py
+++ src/xia2/Modules/MultiCrystal/__init__.py
@@ -27,10 +27,13 @@ master_phil_scope = iotbx.phil.parse(
     """\
 unit_cell = None
   .type = unit_cell
+  .short_caption = "Unit cell"
 n_bins = 20
   .type = int(value_min=1)
+  .short_caption = "Number of bins"
 d_min = None
   .type = float(value_min=0)
+  .short_caption = "High resolution cutoff"
 %s
 """
     % batch_phil_scope
