32337c53ae78383883a8cb43e7e5af61c33930e1
David Waterman <dagewa@users.noreply.github.com>
2022-01-27 17:19:00 +0000

    Fix for xia2/xds hanging when run from ccp4i2 (#651)
    xia2 can hang in CCP4i2 during the pointless version check.
    This does not affect direct interactive use of xia2.
    
    Fix by ensuring all procrunner calls do not pass stdin on to
    subprocesses. For the technical background please see PR #651.
    
    Co-authored-by: Markus Gerstel <markus.gerstel@diamond.ac.uk>

--- src/xia2/cli/make_sphinx_html.py
+++ src/xia2/cli/make_sphinx_html.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import pathlib
 import shutil
+import subprocess
 
 import procrunner
 
@@ -17,13 +18,17 @@ def run():
     sphinx_dir = xia2_dir / "doc" / "sphinx"
     if dest_dir.is_dir():
         shutil.rmtree(dest_dir)
-    result = procrunner.run(["make", "clean"], working_directory=sphinx_dir)
+    result = procrunner.run(
+        ["make", "clean"], working_directory=sphinx_dir, stdin=subprocess.DEVNULL
+    )
     if result.returncode:
         exit(f"make clean failed with exit code {result.returncode}")
 
-    result = procrunner.run(["make", "html"], working_directory=sphinx_dir)
+    result = procrunner.run(
+        ["make", "html"], working_directory=sphinx_dir, stdin=subprocess.DEVNULL
+    )
     if result.returncode:
         exit(f"make html failed with exit code {result.returncode}")
 
-    print("Moving HTML pages to", dest_dir)
+    print(f"Moving HTML pages to {dest_dir}")
     shutil.move(sphinx_dir / "build" / "html", dest_dir)
