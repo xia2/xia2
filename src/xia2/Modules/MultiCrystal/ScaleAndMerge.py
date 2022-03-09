66931fe7fd61dc85b66d81e50ae2fc9ae8855e65
David Waterman <dagewa@users.noreply.github.com>
2022-03-09 11:39:21 +0000

    Multiplex: Allow processing imported mtz when imageset is absent (#641)
    Experiments imported by dials.import_mtz might not have an imageset. In
    that case, don't fail when trying to access that object.
    
    Co-authored-by: Richard Gildea <rjgildea@users.noreply.github.com>

--- src/xia2/Modules/MultiCrystal/ScaleAndMerge.py
+++ src/xia2/Modules/MultiCrystal/ScaleAndMerge.py
@@ -285,8 +285,9 @@ class DataManager:
 
         for i, expt in enumerate(self._experiments):
             expt.scan.set_batch_offset(i * 10**n)
-            # This may be a different scan instance ¯\_(ツ)_/¯
-            expt.imageset.get_scan().set_batch_offset(expt.scan.get_batch_offset())
+            if expt.imageset:
+                # This may be a different scan instance ¯\_(ツ)_/¯
+                expt.imageset.get_scan().set_batch_offset(expt.scan.get_batch_offset())
             logger.debug(
                 f"{expt.scan.get_batch_offset()} {expt.scan.get_batch_range()}"
             )
