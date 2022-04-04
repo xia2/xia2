2d0baeb6ba3df1918dd4d23dcd30ba7ee9b5e7e8
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2022-04-04 10:38:22 +0100

    pathlib

--- tests/Applications/test_check_hdf5_master_files.py
+++ tests/Applications/test_check_hdf5_master_files.py
@@ -5,12 +5,12 @@ import pytest
 from xia2.Applications.xia2_main import check_hdf5_master_files
 
 
-def test_check_hdf5_master_files(dials_data):
+def test_check_hdf5_master_files_works_on_master_file(dials_data):
+    master = dials_data("vmxi_thaumatin", pathlib=True) / "image_15799_master.h5"
+    check_hdf5_master_files([master])
 
-    master = dials_data("vmxi_thaumatin").join("image_15799_master.h5").strpath
-    data = dials_data("vmxi_thaumatin").join("image_15799_data_000001.h5").strpath
 
+def test_check_hdf5_master_files_fails_on_data_file(dials_data):
+    data = dials_data("vmxi_thaumatin", pathlib=True) / "image_15799_data_000001.h5"
     with pytest.raises(SystemExit):
         check_hdf5_master_files([data])
-
-    check_hdf5_master_files([master])
