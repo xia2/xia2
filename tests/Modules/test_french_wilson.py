6711a509595da0925d3eefe3c8e1c7c001652d31
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2022-04-04 10:41:23 +0100

    pathlib

--- tests/Modules/test_french_wilson.py
+++ tests/Modules/test_french_wilson.py
@@ -1,18 +1,22 @@
 from __future__ import annotations
 
+import os
+
 from iotbx.reflection_file_reader import any_reflection_file
 
 import xia2.Modules.CctbxFrenchWilson
 
 
-def test_french_wilson(dials_data, tmpdir):
-    scaled_mtz = dials_data("x4wide_processed").join("AUTOMATIC_DEFAULT_scaled.mtz")
-    truncated_mtz = tmpdir.join("truncate.mtz")
-    xia2.Modules.CctbxFrenchWilson.do_french_wilson(
-        scaled_mtz.strpath, truncated_mtz.strpath, anomalous=True
+def test_french_wilson(dials_data, tmp_path):
+    scaled_mtz = (
+        dials_data("x4wide_processed", pathlib=True) / "AUTOMATIC_DEFAULT_scaled.mtz"
     )
-    assert truncated_mtz.check()
-    result = any_reflection_file(truncated_mtz.strpath)
+    truncated_mtz = tmp_path / "truncate.mtz"
+    xia2.Modules.CctbxFrenchWilson.do_french_wilson(
+        scaled_mtz, truncated_mtz, anomalous=True
+    )
+    assert truncated_mtz.is_file()
+    result = any_reflection_file(os.fspath(truncated_mtz))
     assert result.file_type() == "ccp4_mtz"
     all_labels = [
         ma.info().labels for ma in result.as_miller_arrays(merge_equivalents=False)
