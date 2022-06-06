af40f34efacdbefc6433221acc44e981136c8242
Nicholas Devenish <ndevenish@gmail.com>
2022-06-06 14:25:59 +0100

    Move deprecation backwards

--- src/xia2/Modules/Analysis/__init__.py
+++ src/xia2/Modules/Analysis/__init__.py
@@ -22,7 +22,7 @@ d_min = None
   .type = float(value_min=0)
 d_max = None
   .type = float(value_min=0)
-resolution_bins = None  # Deprecated, remove after version 3.9.
+resolution_bins = None  # Deprecated, remove after DIALS 3.11.
   .type = int
 anomalous = False
   .type = bool
