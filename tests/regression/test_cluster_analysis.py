9054d0bafe87dc46e3cdba41830c965e9f5b924a
Amy Thompson <gdk85799@ws435.diamond.ac.uk>
2023-05-31 13:47:32 +0100

    added test cluster data

--- tests/regression/test_cluster_analysis.py
+++ tests/regression/test_cluster_analysis.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
 import os
-
-# import pathlib
 import subprocess
 
 import pytest
@@ -13,14 +11,77 @@ from xia2.Modules.MultiCrystal import ClusterInfo
 from xia2.Modules.MultiCrystalAnalysis import MultiCrystalAnalysis
 
 # Setup Test Clusters
-test_1 = [
+# Test 1 - test rejects staircasing
+# Test 2 - test only looks for ones with none in common AND min height threshold
+# test 3 - checks min cluster size - rejects pairs where it's too small
+# test 4 - test only outputs defined number of clusters
+data_1 = [
     ClusterInfo(1, [1, 2, 3], 5, 100, [1, 1, 1, 90, 90, 90], 0.3),
     ClusterInfo(2, [1, 2, 3, 4, 5], 5, 100, [1, 1, 1, 90, 90, 90], 0.35),
 ]
 params_1 = scope_extract("test", "test", "test")
-params_1.__inject__("max_cluster_height_difference", 0.05)
+params_1.__inject__("max_cluster_height_difference", 0.1)
 params_1.__inject__("max_output_clusters", 10)
 params_1.__inject__("min_cluster_size", 2)
+
+data_2 = [
+    ClusterInfo(1, [1, 2, 3], 5, 100, [1, 1, 1, 90, 90, 90], 0.32),
+    ClusterInfo(2, [4, 5], 5, 100, [1, 1, 1, 90, 90, 90], 0.33),
+    ClusterInfo(3, [1, 2, 3, 4, 5], 5, 100, [1, 1, 1, 90, 90, 90], 0.35),
+]
+params_2 = scope_extract("test", "test", "test")
+params_2.__inject__("max_cluster_height_difference", 0.0005)
+params_2.__inject__("max_output_clusters", 10)
+params_2.__inject__("min_cluster_size", 2)
+
+params_2_5 = scope_extract("test", "test", "test")
+params_2_5.__inject__("max_cluster_height_difference", 0.5)
+params_2_5.__inject__("max_output_clusters", 10)
+params_2_5.__inject__("min_cluster_size", 2)
+
+data_3 = [
+    ClusterInfo(1, [1, 2], 5, 100, [1, 1, 1, 90, 90, 90], 0.32),
+    ClusterInfo(2, [4, 5], 5, 100, [1, 1, 1, 90, 90, 90], 0.33),
+    ClusterInfo(3, [1, 2, 3, 4, 5], 5, 100, [1, 1, 1, 90, 90, 90], 0.35),
+    ClusterInfo(4, [1, 2, 3, 4, 5, 6, 7], 5, 100, [1, 1, 1, 90, 90, 90], 0.37),
+    ClusterInfo(5, [8, 9, 10], 5, 100, [1, 1, 1, 90, 90, 90], 0.375),
+    ClusterInfo(
+        6, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5, 100, [1, 1, 1, 90, 90, 90], 0.39
+    ),
+]
+params_3 = scope_extract("test", "test", "test")
+params_3.__inject__("max_cluster_height_difference", 0.5)
+params_3.__inject__("max_output_clusters", 10)
+params_3.__inject__("min_cluster_size", 3)
+
+data_4 = [
+    ClusterInfo(1, [1, 2, 3], 5, 100, [1, 1, 1, 90, 90, 90], 0.32),
+    ClusterInfo(2, [4, 5], 5, 100, [1, 1, 1, 90, 90, 90], 0.33),
+    ClusterInfo(3, [6, 7, 8, 9], 5, 100, [1, 1, 1, 90, 90, 90], 0.34),
+    ClusterInfo(4, [10, 11, 12, 13], 5, 100, [1, 1, 1, 90, 90, 90], 0.35),
+    ClusterInfo(
+        5,
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+        5,
+        100,
+        [1, 1, 1, 90, 90, 90],
+        0.36,
+    ),
+    ClusterInfo(6, [14, 15, 16, 17], 5, 100, [1, 1, 1, 90, 90, 90], 0.37),
+    ClusterInfo(7, [18, 19, 20, 21], 5, 100, [1, 1, 1, 90, 90, 90], 0.375),
+    ClusterInfo(
+        8,
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
+        5,
+        100,
+        [1, 1, 1, 90, 90, 90],
+        0.39,
+    ),
+]
+params_4 = scope_extract("test", "test", "test")
+params_4.__inject__("max_cluster_height_difference", 0.5)
+params_4.__inject__("max_output_clusters", 2)
+params_4.__inject__("min_cluster_size", 2)
 
 
 def test_serial_data(dials_data, tmp_path):
@@ -90,7 +151,16 @@ def check_output(main_dir):
     assert (main_dir / "xia2.cluster_analysis.html").is_file()
 
 
-@pytest.mark.parametrize("clusters,params,expected", [(test_1, params_1, [])])
+@pytest.mark.parametrize(
+    "clusters,params,expected",
+    [
+        (data_1, params_1, []),
+        (data_2, params_2, []),
+        (data_2, params_2_5, ["cluster_2", "cluster_1"]),
+        (data_3, params_3, ["cluster_4", "cluster_5"]),
+        (data_4, params_4, ["cluster_5", "cluster_7", "cluster_6"]),
+    ],
+)
 def test_interesting_cluster_algorithm(clusters, params, expected):
     (
         file_data,
