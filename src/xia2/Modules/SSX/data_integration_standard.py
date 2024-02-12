5a2154f93d4107a5f7c496b164c5c25867747273
David Waterman <dgwaterman@gmail.com>
2024-02-12 14:21:55 +0000

    Fix incorrect handling of Windows drive names.
    The previous method only worked if the user was trying to do an import
    with a :start:end slice at the end. If there was no slice, only a drive
    letter, then the old method failed with
    
    Error: /path/to/image.h5:start:end
    
    With this change, extract the drive letter part of the path first
    before splitting into tokens.

--- src/xia2/Modules/SSX/data_integration_standard.py
+++ src/xia2/Modules/SSX/data_integration_standard.py
@@ -289,14 +289,12 @@ def _handle_slices(images_or_templates, path_type="image"):
     import_command = []
     for obj in images_or_templates:
         # here we only care about ':' which are later than C:\
-        if ":" in obj:
-            tokens = obj.split(":")
-            # cope with windows drives i.e. C:\data\blah\thing_0001.cbf:1:100
-            if len(tokens[0]) == 1:
-                tokens = [f"{tokens[0]}:{tokens[1]}"] + tokens[2:]
+        drive, tail = os.path.splitdrive(obj)
+        if ":" in tail:
+            tokens = tail.split(":")
             if len(tokens) != 3:
                 raise RuntimeError("/path/to/image.h5:start:end")
-            dataset = tokens[0]
+            dataset = drive + tokens[0]
             starts.append(int(tokens[1]))
             ends.append(int(tokens[2]))
             import_command.append(
