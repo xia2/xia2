3e3dc42777ca891ec1fd8eac3fb52ba5c4ae8179
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2024-07-30 09:14:47 +0100

    Fix crash in report generation if no reflections in a resolution bin (#801)
    Catch error if empty bins, and reduce n_bins

--- src/xia2/Modules/Report/__init__.py
+++ src/xia2/Modules/Report/__init__.py
@@ -237,22 +237,34 @@ class Report:
         return d
 
     def resolution_plots_and_stats(self):
-        self.merging_stats = merging_statistics.dataset_statistics(
-            self.intensities,
-            n_bins=self.n_bins,
-            cc_one_half_significance_level=self.params.cc_half_significance_level,
-            eliminate_sys_absent=self.params.eliminate_sys_absent,
-            use_internal_variance=self.params.use_internal_variance,
-            assert_is_not_unique_set_under_symmetry=False,
-        )
+        self.merging_stats = None
+        n_bins = self.n_bins
+        while self.merging_stats is None:
+            try:
+                self.merging_stats = merging_statistics.dataset_statistics(
+                    self.intensities,
+                    n_bins=n_bins,
+                    cc_one_half_significance_level=self.params.cc_half_significance_level,
+                    eliminate_sys_absent=self.params.eliminate_sys_absent,
+                    use_internal_variance=self.params.use_internal_variance,
+                    assert_is_not_unique_set_under_symmetry=False,
+                )
+            except merging_statistics.StatisticsError:
+                # Too few reflections for too many bins. Reduce number of bins and try again.
+                n_bins = n_bins - 3
+                if n_bins > 5:
+                    continue
+                else:
+                    raise
 
         intensities_anom = self.intensities.as_anomalous_array()
         intensities_anom = intensities_anom.map_to_asu().customized_copy(
             info=self.intensities.info()
         )
+
         self.merging_stats_anom = merging_statistics.dataset_statistics(
             intensities_anom,
-            n_bins=self.n_bins,
+            n_bins=n_bins,
             anomalous=True,
             cc_one_half_significance_level=self.params.cc_half_significance_level,
             eliminate_sys_absent=self.params.eliminate_sys_absent,
