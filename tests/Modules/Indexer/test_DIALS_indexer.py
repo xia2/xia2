a59d8c5d9d8b045d79f3f1ff93dd14b11276dc58
David Waterman <dgwaterman@gmail.com>
2025-08-05 11:38:19 +0100

    Update expected results following RBS changes

--- tests/Modules/Indexer/test_DIALS_indexer.py
+++ tests/Modules/Indexer/test_DIALS_indexer.py
@@ -37,16 +37,16 @@ def _exercise_dials_indexer(dials_data, tmp_path):
     indexer.index()
 
     assert indexer.get_indexer_cell() == pytest.approx(
-        (42.20, 42.20, 39.68, 90, 90, 90), rel=1e-3
+        (42.43, 42.43, 39.90, 90, 90, 90), rel=1e-3
     )
     solution = indexer.get_solution()
-    assert solution["rmsd"] == pytest.approx(0.08208, abs=1e-3)
-    assert solution["metric"] == pytest.approx(0.34599, abs=5e-3)
+    assert solution["rmsd"] == pytest.approx(0.08343, abs=1e-3)
+    assert solution["metric"] == pytest.approx(0.346, abs=5e-3)
     assert solution["number"] == 9
     assert solution["lattice"] == "tP"
 
     beam_centre = indexer.get_indexer_beam_centre()
-    assert beam_centre == pytest.approx((219.8788, 212.6103), abs=1e-3)
+    assert beam_centre == pytest.approx((219.8976, 212.5918), abs=1e-3)
     print(indexer.get_indexer_experiment_list()[0].crystal)
     print(indexer.get_indexer_experiment_list()[0].detector)
 
